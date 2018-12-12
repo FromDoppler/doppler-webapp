7f71d1e66f63728162fa9cc8d9164b69116ac1b1
fcoronelmakingsense <fcoronel@makingsense.com>
2018-12-12 16:56:27 -0300

    [no ticket] Add service method to test cors in doppler

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -3,11 +3,31 @@ import React, { Component } from 'react';
 import './Nav.css';
 
 export default class HeaderNav extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: {},
+    };
+  }
+
+  componentWillMount() {
+    fetch('http://localhost:52191/Reports/Reports/GetUserData', {
+      mode: 'cors',
+      credentials: 'include',
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ data: data.user });
+      });
+  }
+
   render() {
     return (
       <nav>
         <ul>
-          <li />
+          <li>{this.state.data.Email}</li>
         </ul>
       </nav>
     );
