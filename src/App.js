eca535aec44be636c5770323414f6a429a51f65b
cbernat <cbernat@makingsense.com>
2019-01-07 11:41:31 -0300

    [no-ticket] fix: test pr builder

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <HeaderNav />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload. Test Pr builder.
           </p>
           <a
             className="App-link"
