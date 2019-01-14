e0a745d327b5ebb76eaa0a2835e502a9826ca514
fcoronelmakingsense <fcoronel@makingsense.com>
2019-01-14 16:42:14 -0300

    [No ticket] Add jwt initial configuration

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -3,34 +3,11 @@ import React, { Component } from 'react';
 import './Nav.css';
 
 export default class HeaderNav extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: {},
-    };
-  }
-
-  componentWillMount() {
-    fetch(process.env.REACT_APP_API_URL + '/Reports/Reports/GetUserData', {
-      mode: 'cors',
-      credentials: 'include',
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        this.setState({ data: data.user });
-      })
-      .catch((error) => {
-        window.location.href = process.env.REACT_APP_API_URL + '/SignIn/index';
-      });
-  }
-
   render() {
     return (
       <nav>
         <ul>
-          <li>{this.state.data.Email}</li>
+          <li />
         </ul>
       </nav>
     );
