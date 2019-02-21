a869f485d422bf9798bcd81f4fb39676a01f8e76
fcoronelmakingsense <fcoronel@makingsense.com>
2019-02-21 15:45:45 -0300

    [feat] Integrate ui system header in webapp

--- src/components/Header/HeaderNav/HeaderNav.js
+++ src/components/Header/HeaderNav/HeaderNav.js
@@ -1,54 +1,33 @@
 import React from 'react';
 
-class HeaderNav extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.hoverMenu = this.hoverMenu.bind(this);
-  }
-
-  hoverMenu(hasSubNav) {
-    if (Array.isArray(hasSubNav) && hasSubNav.length) {
-      this.props.toggleHeaderOpen(true);
-    } else {
-      this.props.toggleHeaderOpen(false);
-    }
-  }
-
-  render() {
-    const navItems = this.props.nav;
-    return (
-      <nav className="nav-left-main">
-        <div className="menu-main--container">
-          <ul className="menu-main">
-            {navItems.map((item, index) => (
-              <li
-                className="submenu-item"
-                key={index}
-                onMouseEnter={this.hoverMenu.bind(this, item.subNav)}
-                onMouseLeave={this.hoverMenu.bind(this)}
-              >
-                <a className={item.isSelected ? 'active' : ''} href={item.url}>
-                  {item.title}
-                </a>
-                {item.subNav ? (
-                  <ul className="sub-menu">
-                    {item.subNav.map((itemSubNav, index) => (
-                      <li key={index}>
-                        <a className={itemSubNav.isSelected ? 'active' : ''} href={itemSubNav.url}>
-                          {itemSubNav.title}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                ) : null}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </nav>
-    );
-  }
-}
+const HeaderNav = (props) => {
+  const navItems = props.nav;
+  return (
+    <nav className="nav-left-main">
+      <div className="menu-main--container">
+        <ul className="menu-main">
+          {navItems.map((item, index) => (
+            <li className="submenu-item" key={index}>
+              <a className={item.isSelected ? 'active' : ''} href={item.url}>
+                {item.title}
+              </a>
+              {item.subNav ? (
+                <ul className="sub-menu">
+                  {item.subNav.map((itemSubNav, index) => (
+                    <li key={index}>
+                      <a className={itemSubNav.isSelected ? 'active' : ''} href={itemSubNav.url}>
+                        {itemSubNav.title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              ) : null}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default HeaderNav;
--- src/components/Header/HeaderUserMenu/HeaderUserMenu.js
+++ src/components/Header/HeaderUserMenu/HeaderUserMenu.js
@@ -1,87 +1,69 @@
 import React from 'react';
 
-class HeaderUserMenu extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isMenuOpen: false,
-    };
-    this.toggleMenu = this.toggleMenu.bind(this);
-  }
-
-  toggleMenu() {
-    this.setState({ isMenuOpen: !this.state.isMenuOpen });
-  }
-
-  render() {
-    const user = this.props.user;
-    return (
-      <div>
-        <span id="user-menu--open" onClick={this.toggleMenu}>
-          <span style={{ background: user.avatar.color }} className="user-avatar">
+const HeaderUserMenu = (props) => {
+  const user = props.user;
+  return (
+    <div>
+      <span id="user-menu--open">
+        <span style={{ background: user.avatar.color }} className="user-avatar">
+          {user.avatar.text}
+        </span>
+      </span>
+      <div class="user-menu">
+        <header>
+          <span className="user-avatar--menu" style={{ background: user.avatar.color }}>
             {user.avatar.text}
           </span>
-        </span>
-        <div className={this.state.isMenuOpen ? 'user-menu open' : 'user-menu'}>
-          <header>
-            <span className="user-avatar--menu" style={{ background: user.avatar.color }}>
-              {user.avatar.text}
-            </span>
-            <p>
-              <span className="name">{user.fullname}</span>
-              <span className="email">{user.email}</span>
-            </p>
-          </header>
-          <div className="user-plan--container">
-            <div className="user-plan--type">
-              {user.plan.isSubscribers === 'true' || user.plan.isMonthlyByEmail === 'true' ? (
-                <p className="user-plan--monthly-text">
-                  <span>{user.plan.planName}</span> |{' '}
-                  <strong>
-                    {user.plan.maxSubscribers} {user.plan.itemDescription}
-                  </strong>
-                </p>
-              ) : (
-                ''
-              )}
-              <p>
-                <strong>{user.plan.remainingCredits}</strong>
-                {user.plan.description}
+          <p>
+            <span className="name">{user.fullname}</span>
+            <span className="email">{user.email}</span>
+          </p>
+        </header>
+        <div className="user-plan--container">
+          <div className="user-plan--type">
+            {user.plan.isSubscribers === 'true' || user.plan.isMonthlyByEmail === 'true' ? (
+              <p className="user-plan--monthly-text">
+                <span>{user.plan.planName}</span> |{' '}
+                <strong>
+                  {user.plan.maxSubscribers} {user.plan.itemDescription}
+                </strong>
               </p>
-            </div>
-            {user.clientManager &&
-            user.plan.buttonUrl &&
-            user.plan.pendingFreeUpgrade !== 'true' ? (
-              <a className="buy-plan" href={user.plan.buttonUrl}>
-                {user.plan.buttonText}
-              </a>
             ) : (
               ''
             )}
-            {!user.clientManager &&
-            user.plan.buttonUrl &&
-            user.plan.pendingFreeUpgrade !== 'true' ? (
-              <button className="buy-plan">{user.plan.buttonText}</button>
-            ) : (
-              ''
-            )}
-            {!user.clientManager && !user.plan.buttonUrl ? (
-              <button className="buy-plan">{user.plan.buttonText}</button>
-            ) : (
-              ''
-            )}
+            <p>
+              <strong>{user.plan.remainingCredits}</strong>
+              {user.plan.description}
+            </p>
           </div>
-          <ul className="options-user">
-            {user.nav.map((item, index) => (
-              <li key={index}>
-                <a href={item.url}>{item.title}</a>
-              </li>
-            ))}
-          </ul>
+          {user.clientManager && user.plan.buttonUrl && user.plan.pendingFreeUpgrade !== 'true' ? (
+            <a className="buy-plan" href={user.plan.buttonUrl}>
+              {user.plan.buttonText}
+            </a>
+          ) : (
+            ''
+          )}
+          {!user.clientManager && user.plan.buttonUrl && user.plan.pendingFreeUpgrade !== 'true' ? (
+            <button className="buy-plan">{user.plan.buttonText}</button>
+          ) : (
+            ''
+          )}
+          {!user.clientManager && !user.plan.buttonUrl ? (
+            <button className="buy-plan">{user.plan.buttonText}</button>
+          ) : (
+            ''
+          )}
         </div>
+        <ul className="options-user">
+          {user.nav.map((item, index) => (
+            <li key={index}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default HeaderUserMenu;
