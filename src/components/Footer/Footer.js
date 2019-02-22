d62cdd29efbb6c628074634b0b86ea12839dcc70
fcoronelmakingsense <fcoronel@makingsense.com>
2019-02-22 16:39:18 -0300

    feat: integrate doppler ui system footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,20 +2,18 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 const Footer = () => (
-  <footer>
-    <div>
-      <p>
-        <span>
-          <FormattedMessage id="footer.iso" />
-        </span>
-        <span>
-          &copy; {new Date().getFullYear()} Doppler LLC.{' '}
-          <FormattedMessage id="footer.allRightReserved" />.{' '}
-          <a href="https://fromdoppler.com/privacidad">
-            <FormattedMessage id="footer.privacy" />
-          </a>
-        </span>
-      </p>
+  <footer className="footer-main">
+    <div className="footer-wrapper">
+      <span>
+        <FormattedMessage id="footer.iso" />
+      </span>
+      <span>
+        &copy; {new Date().getFullYear()} Doppler LLC.{' '}
+        <FormattedMessage id="footer.allRightReserved" />.{' '}
+        <a className="link--default" href="https://fromdoppler.com/privacidad">
+          <FormattedMessage id="footer.privacy" />
+        </a>
+      </span>
     </div>
   </footer>
 );
