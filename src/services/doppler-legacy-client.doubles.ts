0449b8e8eb54474d6891245ad029c97428ff3762
Andrés Moschini <andresmoschini@gmail.com>
2019-03-07 16:32:23 -0300

    refactor(session): include headers data in session object

--- src/services/doppler-legacy-client.doubles.ts
+++ src/services/doppler-legacy-client.doubles.ts
@@ -1,11 +1,19 @@
-import { DopplerLegacyClient } from './doppler-legacy-client';
+import { DopplerLegacyClient, mapHeaderDataJson } from './doppler-legacy-client';
+import headerDataJson from '../headerData.json';
 
 export class HardcodedDopplerLegacyClient implements DopplerLegacyClient {
+  public constructor(public readonly email = 'hardcoded@email.com') {}
+
   public async getUserData() {
+    const { user, nav, alert } = mapHeaderDataJson(headerDataJson);
+
     return {
       user: {
-        email: 'hardcoded@email.com',
+        ...user,
+        email: this.email,
       },
+      nav: nav,
+      alert,
     };
   }
 }
