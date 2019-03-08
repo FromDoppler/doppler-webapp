b480c007747ac7ce145d0ad9c262718d5c1ff3f9
Andrés Moschini <andresmoschini@gmail.com>
2019-03-08 16:07:31 -0300

    chore(localization): add a DopplerIntlProvider's double to simplify some tests

---
+++ src/DopplerIntlProvider.double-with-ids-as-values.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { addLocaleData, IntlProvider } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import messages_en from './i18n/en.json';
+import { flattenMessages } from './utils';
+
+addLocaleData([...en]);
+
+const messages = Object.keys(flattenMessages(messages_en)).reduce(
+  (accumulator, currentValue) => ({ ...accumulator, [currentValue]: currentValue }),
+  {},
+);
+
+export default ({ children }) => (
+  <IntlProvider locale="en" messages={messages}>
+    {children}
+  </IntlProvider>
+);
