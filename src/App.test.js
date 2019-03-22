74fc245416ffe677b959ae7d27b89ffe9fe4b68d
Andrés Moschini <andresmoschini@gmail.com>
2019-03-22 15:07:47 -0300

    test: add new test to verify that authenticated user is redirected to reports when path does not exist

--- src/App.test.js
+++ src/App.test.js
@@ -352,6 +352,57 @@ describe('App component', () => {
 
         const { getByText, container } = render(
           <AppServicesProvider forcedServices={dependencies}>
+            <Router initialEntries={['/this/route/does/not/exist?param1=value1#hash']}>
+              <RouterInspector target={currentRouteState} />
+              <App locale="en" />
+            </Router>
+          </AppServicesProvider>,
+        );
+
+        expect(currentRouteState.location.pathname).toEqual('/this/route/does/not/exist');
+        expect(currentRouteState.location.search).toEqual('?param1=value1');
+        expect(currentRouteState.location.hash).toEqual('#hash');
+        getByText('Loading...');
+
+        // Act
+        dependencies.sessionManager.updateAppSession({
+          status: 'authenticated',
+          userData: {
+            user: {
+              lang: 'es',
+              avatar: {},
+              plan: {},
+              nav: [],
+            },
+            nav: [],
+          },
+        });
+
+        // Assert
+        expect(currentRouteState.location.pathname).toEqual('/reports');
+        expect(currentRouteState.location.search).toEqual('');
+        expect(currentRouteState.location.hash).toEqual('');
+        expect(currentRouteState.location.state).toBeUndefined();
+        expect(currentRouteState.history.length).toEqual(1);
+        expect(currentRouteState.history.action).toEqual('REPLACE');
+        const headerEl = container.querySelector('.header-main');
+        expect(headerEl).not.toBeNull();
+        const menuEl = container.querySelector('.menu-main');
+        expect(menuEl).not.toBeNull();
+        const footerEl = container.querySelector('.footer-main');
+        expect(footerEl).not.toBeNull();
+      });
+
+      it('should be redirected to /reports when route does not exists', () => {
+        const dependencies = {
+          RedirectToLogin: RedirectToInternalLogin,
+          sessionManager: createDoubleSessionManager(),
+        };
+
+        const currentRouteState = {};
+
+        const { getByText, container } = render(
+          <AppServicesProvider forcedServices={dependencies}>
             <Router initialEntries={['/reports?param1=value1#hash']}>
               <RouterInspector target={currentRouteState} />
               <App locale="en" />
