e5b91c402262fec07d06c4353da2ccfe2bad4503
Andrés Moschini <andresmoschini@gmail.com>
2019-03-22 12:46:12 -0300

    test: add new test to verify redirect to internal login

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import { render, cleanup, wait } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import App from './App';
 import { AppServicesProvider } from './services/pure-di';
+import { RedirectToInternalLogin } from './components/RedirectToLogin';
 import { MemoryRouter as Router, withRouter } from 'react-router-dom';
 
 function createDoubleSessionManager() {
@@ -17,6 +18,13 @@ function createDoubleSessionManager() {
   };
   return double;
 }
+
+const RouterInspector = withRouter(({ match, location, history, target }) => {
+  target.match = match;
+  target.location = location;
+  target.history = history;
+  return null;
+});
 
 describe('App component', () => {
   afterEach(cleanup);
@@ -208,6 +216,50 @@ describe('App component', () => {
         expect(dependencies.window.location.href).toEqual(
           'http://legacyUrl.localhost/SignIn/index?redirect=http://webapp.localhost/path1/path2/#/reports',
         );
+      });
+
+      it('should be redirected to Internal Login after open /reports (when using RedirectToInternalLogin)', () => {
+        const dependencies = {
+          RedirectToLogin: RedirectToInternalLogin,
+          sessionManager: createDoubleSessionManager(),
+        };
+
+        const currentRouteState = {};
+
+        const { getByText, container } = render(
+          <AppServicesProvider forcedServices={dependencies}>
+            <Router initialEntries={['/reports?param1=value1#hash']}>
+              <RouterInspector target={currentRouteState} />
+              <App locale="en" />
+            </Router>
+          </AppServicesProvider>,
+        );
+
+        expect(currentRouteState.location.pathname).toEqual('/reports');
+        expect(currentRouteState.location.search).toEqual('?param1=value1');
+        expect(currentRouteState.location.hash).toEqual('#hash');
+        getByText('Loading...');
+
+        // Act
+        dependencies.sessionManager.updateAppSession({
+          status: 'not-authenticated',
+        });
+
+        // Assert
+        expect(currentRouteState.location.pathname).toEqual('/login');
+        expect(currentRouteState.location.state).toBeDefined();
+        expect(currentRouteState.location.state.from).toBeDefined();
+        expect(currentRouteState.location.state.from.pathname).toEqual('/reports');
+        expect(currentRouteState.location.state.from.search).toEqual('?param1=value1');
+        expect(currentRouteState.location.state.from.hash).toEqual('#hash');
+        expect(currentRouteState.history.length).toEqual(1); // because the URL has been replaced in the redirect
+        expect(currentRouteState.history.action).toEqual('REPLACE');
+        const headerEl = container.querySelector('.header-main');
+        expect(headerEl).toBeNull();
+        const menuEl = container.querySelector('.menu-main');
+        expect(menuEl).toBeNull();
+        const footerEl = container.querySelector('.footer-main');
+        expect(footerEl).toBeNull();
       });
     });
   });
