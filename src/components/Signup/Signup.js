59d12d15979f94c2f58be83560438cc331bd85d6
Andrés Moschini <andresmoschini@gmail.com>
2019-03-25 20:56:33 -0300

    chore: prepare indentation for formik

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -18,81 +18,81 @@ export default injectIntl(function({ intl }) {
         </header>
         <h5>{_('signup.sign_up')}</h5>
         <p className="content-subtitle">{_('signup.sign_up_sub')}</p>
-        <form className="signup-form">
-          <fieldset>
-            <ul className="field-group">
-              <li className="field-item field-item--50">
-                <label htmlFor="name">{_('signup.label_firstname')}</label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder={_('signup.placeholder_firstname')}
-                />
-              </li>
-              <li className="field-item field-item--50">
-                <label htmlFor="lastname">{_('signup.label_lastname')}</label>
-                <input
-                  type="text"
-                  name="lastname"
-                  id="lastname"
-                  placeholder={_('signup.placeholder_lastname')}
-                />
-              </li>
-              <li className="field-item error">
-                <label htmlFor="phone">{_('signup.label_phone')}</label>
-                <input
-                  type="tel"
-                  name="phone"
-                  id="phone"
-                  placeholder={_('signup.placeholder_phone')}
-                />
-              </li>
-            </ul>
-          </fieldset>
-          <fieldset>
-            <ul className="field-group">
-              <li className="field-item error">
-                <label htmlFor="email">{_('signup.label_email')}</label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder={_('signup.placeholder_email')}
-                />
-              </li>
-              <li className="field-item">
-                <label htmlFor="password">{_('signup.label_password')}</label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder={_('signup.placeholder_password')}
-                />
-              </li>
-            </ul>
-          </fieldset>
-          <fieldset>
-            <ul className="field-group">
-              <li className="field-item field-item__checkbox error">
-                <input type="checkbox" name="accept_privacy_policies" />
-                <span className="checkmark" />
-                <label htmlFor="accept_privacy_policy">
-                  {' '}
-                  <FormattedHTMLMessage id="signup.privacy_policy_consent_HTML" />
-                </label>
-              </li>
-              <li className="field-item field-item__checkbox">
-                <input type="checkbox" name="accept_promotions" />
-                <span className="checkmark" />
-                <label htmlFor="accept_promotions">{_('signup.promotions_consent')}</label>
-              </li>
-            </ul>
-            <button type="button" className="dp-button button--round button-medium primary-green">
-              {_('signup.button_signup')}
-            </button>
-          </fieldset>
-        </form>
+            <form className="signup-form">
+              <fieldset>
+                <ul className="field-group">
+                  <li className="field-item field-item--50">
+                    <label htmlFor="name">{_('signup.label_firstname')}</label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      placeholder={_('signup.placeholder_firstname')}
+                    />
+                  </li>
+                  <li className="field-item field-item--50">
+                    <label htmlFor="lastname">{_('signup.label_lastname')}</label>
+                    <input
+                      type="text"
+                      name="lastname"
+                      id="lastname"
+                      placeholder={_('signup.placeholder_lastname')}
+                    />
+                  </li>
+                  <li className="field-item error">
+                    <label htmlFor="phone">{_('signup.label_phone')}</label>
+                    <input
+                      type="tel"
+                      name="phone"
+                      id="phone"
+                      placeholder={_('signup.placeholder_phone')}
+                    />
+                  </li>
+                </ul>
+              </fieldset>
+              <fieldset>
+                <ul className="field-group">
+                  <li className="field-item error">
+                    <label htmlFor="email">{_('signup.label_email')}</label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder={_('signup.placeholder_email')}
+                    />
+                  </li>
+                  <li className="field-item">
+                    <label htmlFor="password">{_('signup.label_password')}</label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder={_('signup.placeholder_password')}
+                    />
+                  </li>
+                </ul>
+              </fieldset>
+              <fieldset>
+                <ul className="field-group">
+                  <li className="field-item field-item__checkbox error">
+                    <input type="checkbox" name="accept_privacy_policies" />
+                    <span className="checkmark" />
+                    <label htmlFor="accept_privacy_policy">
+                      {' '}
+                      <FormattedHTMLMessage id="signup.privacy_policy_consent_HTML" />
+                    </label>
+                  </li>
+                  <li className="field-item field-item__checkbox">
+                    <input type="checkbox" name="accept_promotions" />
+                    <span className="checkmark" />
+                    <label htmlFor="accept_promotions">{_('signup.promotions_consent')}</label>
+                  </li>
+                </ul>
+                <button type="button" className="dp-button button--round button-medium primary-green">
+                  {_('signup.button_signup')}
+                </button>
+              </fieldset>
+            </form>
         <div className="content-legal">
           <FormattedHTMLMessage id="signup.legal_HTML" />
         </div>
