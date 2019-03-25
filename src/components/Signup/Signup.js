ba3566e8a79a3b64c914a35973cb14b60f3b3477
Andrés Moschini <andresmoschini@gmail.com>
2019-03-25 20:58:11 -0300

    chore: add formik with basic validation

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,9 +1,75 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FormattedHTMLMessage, injectIntl } from 'react-intl';
+import { timeout } from '../../utils';
+import { Formik, Form, Field } from 'formik';
+
+const fieldNames = {
+  firstname: 'firstname',
+  lastname: 'lastname',
+  phone: 'phone',
+  email: 'email',
+  password: 'password',
+  accept_privacy_policies: 'accept_privacy_policies',
+  accept_promotions: 'accept_promotions',
+};
+
+/** Prepare empty values for all fields
+ * It is required because in another way, the fields are not marked as touched.
+ */
+const getFormInitialValues = () =>
+  Object.keys(fieldNames).reduce(
+    (accumulator, currentValue) => ({ ...accumulator, [currentValue]: '' }),
+    {},
+  );
 
 export default injectIntl(function({ intl }) {
   const _ = (id, values) => intl.formatMessage({ id: id }, values);
+
+  const validate = (values) => {
+    const errors = {};
+    if (!values[fieldNames.firstname]) {
+      errors[fieldNames.firstname] = _('validation_messages.error_required_field');
+    }
+
+    if (!values[fieldNames.lastname]) {
+      errors[fieldNames.lastname] = _('validation_messages.error_required_field');
+    }
+
+    if (!values[fieldNames.phone]) {
+      errors[fieldNames.phone] = _('validation_messages.error_required_field');
+    } else {
+      // TODO: validate phone
+    }
+
+    if (!values[fieldNames.email]) {
+      errors[fieldNames.email] = _('validation_messages.error_required_field');
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values[fieldNames.email])) {
+      // TODO: review if this is our desired validation
+      errors[fieldNames.email] = _('validation_messages.error_invalid_email_address');
+    }
+
+    if (!values[fieldNames.password]) {
+      // TODO: I think that password validation has a different format
+      errors[fieldNames.password] = _('validation_messages.error_required_field');
+    } else {
+      // TODO: validate password
+    }
+
+    if (!values[fieldNames.accept_privacy_policies]) {
+      // TODO: show the right message
+      errors[fieldNames.accept_privacy_policies] = _('validation_messages.error_required_field');
+    }
+
+    return errors;
+  };
+
+  const onSubmit = async (values, { setSubmitting }) => {
+    // TODO: implement it
+    await timeout(1500);
+    setSubmitting(false);
+  };
+
   return (
     <main className="panel-wrapper">
       <article className="main-panel">
@@ -18,81 +84,166 @@ export default injectIntl(function({ intl }) {
         </header>
         <h5>{_('signup.sign_up')}</h5>
         <p className="content-subtitle">{_('signup.sign_up_sub')}</p>
-            <form className="signup-form">
+        <Formik initialValues={getFormInitialValues()} validate={validate} onSubmit={onSubmit}>
+          {({ errors, touched }) => (
+            <Form className="signup-form">
               <fieldset>
                 <ul className="field-group">
-                  <li className="field-item field-item--50">
-                    <label htmlFor="name">{_('signup.label_firstname')}</label>
-                    <input
+                  <li
+                    className={
+                      'field-item field-item--50' +
+                      (touched[fieldNames.firstname] && errors[fieldNames.firstname]
+                        ? ' error'
+                        : '')
+                    }
+                  >
+                    <label htmlFor={fieldNames.firstname}>{_('signup.label_firstname')}</label>
+                    <Field
                       type="text"
-                      name="name"
-                      id="name"
+                      name={fieldNames.firstname}
+                      id={fieldNames.firstname}
                       placeholder={_('signup.placeholder_firstname')}
                     />
+                    {touched[fieldNames.firstname] && errors[fieldNames.firstname] ? (
+                      <div className="wrapper-errors">
+                        <p className="error-message">{errors[fieldNames.firstname]}</p>
+                      </div>
+                    ) : null}
                   </li>
-                  <li className="field-item field-item--50">
-                    <label htmlFor="lastname">{_('signup.label_lastname')}</label>
-                    <input
+
+                  <li
+                    className={
+                      'field-item field-item--50' +
+                      (touched[fieldNames.lastname] && errors[fieldNames.lastname] ? ' error' : '')
+                    }
+                  >
+                    <label htmlFor={fieldNames.lastname}>{_('signup.label_lastname')}</label>
+                    <Field
                       type="text"
-                      name="lastname"
-                      id="lastname"
+                      name={fieldNames.lastname}
+                      id={fieldNames.lastname}
                       placeholder={_('signup.placeholder_lastname')}
                     />
+                    {touched[fieldNames.lastname] && errors[fieldNames.lastname] ? (
+                      <div className="wrapper-errors">
+                        <p className="error-message">{errors[fieldNames.lastname]}</p>
+                      </div>
+                    ) : null}
                   </li>
-                  <li className="field-item error">
-                    <label htmlFor="phone">{_('signup.label_phone')}</label>
-                    <input
+                  <li
+                    className={
+                      'field-item' +
+                      (touched[fieldNames.phone] && errors[fieldNames.phone] ? ' error' : '')
+                    }
+                  >
+                    <label htmlFor={fieldNames.phone}>{_('signup.label_phone')}</label>
+                    <Field
                       type="tel"
-                      name="phone"
-                      id="phone"
+                      name={fieldNames.phone}
+                      id={fieldNames.phone}
                       placeholder={_('signup.placeholder_phone')}
                     />
+                    {touched[fieldNames.phone] && errors[fieldNames.phone] ? (
+                      <div className="wrapper-errors">
+                        <p className="error-message">{errors[fieldNames.phone]}</p>
+                      </div>
+                    ) : null}
                   </li>
                 </ul>
               </fieldset>
               <fieldset>
                 <ul className="field-group">
-                  <li className="field-item error">
-                    <label htmlFor="email">{_('signup.label_email')}</label>
-                    <input
+                  <li
+                    className={
+                      'field-item' +
+                      (touched[fieldNames.email] && errors[fieldNames.email] ? ' error' : '')
+                    }
+                  >
+                    <label htmlFor={fieldNames.email}>{_('signup.label_email')}</label>
+                    <Field
                       type="email"
-                      name="email"
-                      id="email"
+                      name={fieldNames.email}
+                      id={fieldNames.email}
                       placeholder={_('signup.placeholder_email')}
                     />
+                    {touched[fieldNames.email] && errors[fieldNames.email] ? (
+                      <div className="wrapper-errors">
+                        <p className="error-message">{errors[fieldNames.email]}</p>
+                      </div>
+                    ) : null}
                   </li>
-                  <li className="field-item">
-                    <label htmlFor="password">{_('signup.label_password')}</label>
-                    <input
+                  <li
+                    className={
+                      'field-item' +
+                      (touched[fieldNames.password] && errors[fieldNames.password] ? ' error' : '')
+                    }
+                  >
+                    <label htmlFor={fieldNames.password}>{_('signup.label_password')}</label>
+                    <Field
                       type="password"
-                      name="password"
-                      id="password"
+                      name={fieldNames.password}
+                      id={fieldNames.password}
                       placeholder={_('signup.placeholder_password')}
                     />
+                    {touched[fieldNames.password] && errors[fieldNames.password] ? (
+                      <div className="wrapper-errors">
+                        <p className="error-message">{errors[fieldNames.password]}</p>
+                      </div>
+                    ) : null}
                   </li>
                 </ul>
               </fieldset>
               <fieldset>
                 <ul className="field-group">
-                  <li className="field-item field-item__checkbox error">
-                    <input type="checkbox" name="accept_privacy_policies" />
+                  <li
+                    className={
+                      'field-item field-item__checkbox' +
+                      (touched[fieldNames.accept_privacy_policies] &&
+                      errors[fieldNames.accept_privacy_policies]
+                        ? ' error'
+                        : '')
+                    }
+                  >
+                    <Field
+                      type="checkbox"
+                      name={fieldNames.accept_privacy_policies}
+                      id={fieldNames.accept_privacy_policies}
+                    />
                     <span className="checkmark" />
-                    <label htmlFor="accept_privacy_policy">
+                    <label htmlFor={fieldNames.accept_privacy_policies}>
                       {' '}
                       <FormattedHTMLMessage id="signup.privacy_policy_consent_HTML" />
                     </label>
+                    {touched[fieldNames.password] && errors[fieldNames.accept_privacy_policies] ? (
+                      <div className="wrapper-errors">
+                        <p className="error-message">
+                          {errors[fieldNames.accept_privacy_policies]}
+                        </p>
+                      </div>
+                    ) : null}
                   </li>
                   <li className="field-item field-item__checkbox">
-                    <input type="checkbox" name="accept_promotions" />
+                    <Field
+                      type="checkbox"
+                      name={fieldNames.accept_promotions}
+                      id={fieldNames.accept_promotions}
+                    />
                     <span className="checkmark" />
-                    <label htmlFor="accept_promotions">{_('signup.promotions_consent')}</label>
+                    <label htmlFor={fieldNames.accept_promotions}>
+                      {_('signup.promotions_consent')}
+                    </label>
                   </li>
                 </ul>
-                <button type="button" className="dp-button button--round button-medium primary-green">
+                <button
+                  type="submit"
+                  className="dp-button button--round button-medium primary-green"
+                >
                   {_('signup.button_signup')}
                 </button>
               </fieldset>
-            </form>
+            </Form>
+          )}
+        </Formik>
         <div className="content-legal">
           <FormattedHTMLMessage id="signup.legal_HTML" />
         </div>
