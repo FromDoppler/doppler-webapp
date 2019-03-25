e57a89287f87453c5e2cc9788d333fb33fc706d3
Andrés Moschini <andresmoschini@gmail.com>
2019-03-25 15:45:00 -0300

    chore: add translations and other adaptations to signup form

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,173 +1,124 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FormattedHTMLMessage, injectIntl } from 'react-intl';
 
-export default function() {
+export default injectIntl(function({ intl }) {
+  const _ = (id, values) => intl.formatMessage({ id: id }, values);
   return (
     <main className="panel-wrapper">
       <article className="main-panel">
         <header>
           <img src="img/doppler-logo.svg" className="logo-doppler" alt="Doppler" />
           <small className="content-signin">
-            ¿Ya tienes una cuenta?{' '}
-            <a href="/" className="link-green" target="_blank">
-              INGRESA
-            </a>
+            {_('signup.do_you_already_have_an_account')}{' '}
+            <Link to="/login" className="link-green uppercase">
+              {_('signup.log_in')}
+            </Link>
           </small>
         </header>
-        <h5>Regístrate</h5>
-        <p className="content-subtitle">Crea una cuenta GRATIS hasta 500 Suscriptores.</p>
+        <h5>{_('signup.sign_up')}</h5>
+        <p className="content-subtitle">{_('signup.sign_up_sub')}</p>
         <form className="signup-form">
           <fieldset>
-            <legend>Datos personales</legend>
             <ul className="field-group">
-              <li>
-                <ul className="field-group">
-                  <li className="field-item field-item--50 error">
-                    <label htmlFor="name">Nombre:</label>
-                    <input type="name" name="name" id="name" placeholder="Nombre" />
-                    <div className="wrapper-errors bounceIn">
-                      <p className="error-message">¡Ouch! Este campo está vacío</p>
-                    </div>
-                  </li>
-                  <li className="field-item field-item--50 error">
-                    <label htmlFor="lastname">Apellido:</label>
-                    <input type="lastname" name="name" id="lastname" placeholder="Apellido" />
-                    <div className="wrapper-errors bounceIn">
-                      <p className="error-message">¡Ouch! Este campo está vacío</p>
-                    </div>
-                  </li>
-                </ul>
+              <li className="field-item field-item--50">
+                <label htmlFor="name">{_('signup.label_firstname')}</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder={_('signup.placeholder_firstname')}
+                />
+              </li>
+              <li className="field-item field-item--50">
+                <label htmlFor="lastname">{_('signup.label_lastname')}</label>
+                <input
+                  type="text"
+                  name="lastname"
+                  id="lastname"
+                  placeholder={_('signup.placeholder_lastname')}
+                />
               </li>
               <li className="field-item error">
-                <label htmlFor="phone">Teléfono:</label>
-                <input type="tel" name="phone" id="phone" placeholder="9 11 2345-6789" />
-                <div className="wrapper-errors bounceIn">
-                  <p className="error-message">¡Ouch! Este campo está vacío</p>
-                </div>
+                <label htmlFor="phone">{_('signup.label_phone')}</label>
+                <input
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                  placeholder={_('signup.placeholder_phone')}
+                />
               </li>
             </ul>
           </fieldset>
           <fieldset>
             <ul className="field-group">
-              <legend>Usuario y contraseña</legend>
               <li className="field-item error">
-                <label htmlFor="email">Email:</label>
+                <label htmlFor="email">{_('signup.label_email')}</label>
                 <input
                   type="email"
                   name="email"
                   id="email"
-                  placeholder="Tu Email será tu nombre de usuario"
+                  placeholder={_('signup.placeholder_email')}
                 />
-                <div className="wrapper-errors bounceIn">
-                  <p className="error-message">¡Ouch! Este campo está vacío</p>
-                </div>
               </li>
               <li className="field-item">
-                <label htmlFor="password">Contraseña:</label>
+                <label htmlFor="password">{_('signup.label_password')}</label>
                 <input
                   type="password"
                   name="password"
                   id="password"
-                  placeholder="Escribe tu clave secreta"
+                  placeholder={_('signup.placeholder_password')}
                 />
-                <div className="wrapper-password">
-                  <p className="password-message">
-                    <span className="waiting-message">8 caracteres como mínimo</span>
-                    <span className="waiting-message">Un dígito</span>
-                  </p>
-                  <p className="password-message">
-                    <span className="lack-message">8 caracteres como mínimo</span>
-                    <span className="waiting-message">Un dígito</span>
-                  </p>
-                  <p className="password-message">
-                    <span className="complete-message">8 caracteres como mínimo</span>
-                    <span className="lack-message">Un dígito</span>
-                  </p>
-                  <p className="password-message">
-                    <span className="secure-message">¡Tu contraseña es segura!</span>
-                  </p>
-                </div>
               </li>
             </ul>
           </fieldset>
           <fieldset>
-            <legend>Política de privacidad y promociones</legend>
             <ul className="field-group">
               <li className="field-item field-item__checkbox error">
-                <input type="checkbox" name="acepto_politicas" />
+                <input type="checkbox" name="accept_privacy_policies" />
                 <span className="checkmark" />
-                <label htmlFor="acepto_politicas">
+                <label htmlFor="accept_privacy_policy">
                   {' '}
-                  Acepto la{' '}
-                  <a className="link-green" href="/">
-                    Política de Privacidad
-                  </a>{' '}
-                  de Doppler.
+                  <FormattedHTMLMessage id="signup.privacy_policy_consent_HTML" />
                 </label>
-                <div className="wrapper-errors bounceIn">
-                  <p className="error-message">¡Ouch! Este campo está vacío</p>
-                </div>
               </li>
               <li className="field-item field-item__checkbox">
-                <input type="checkbox" name="acepto_promociones" />
+                <input type="checkbox" name="accept_promotions" />
                 <span className="checkmark" />
-                <label htmlFor="acepto_promociones">
-                  Acepto recibir las promociones de Doppler y sus aliados.
-                </label>
+                <label htmlFor="accept_promotions">{_('signup.promotions_consent')}</label>
               </li>
             </ul>
             <button type="button" className="dp-button button--round button-medium primary-green">
-              Crear cuenta
+              {_('signup.button_signup')}
             </button>
           </fieldset>
         </form>
         <div className="content-legal">
-          <p>
-            Doppler te informa que los datos de carácter personal que nos proporciones al rellenar
-            el presente formulario serán tratados por Making Sense LLC (Doppler) como responsable de
-            esta web.
-          </p>
-          <p>
-            <strong>Finalidad:</strong> Darte de alta en nuestra plataforma y brindarte los
-            servicios que nos requieras.
-          </p>
-          <p>
-            <strong>Legitimación:</strong> Consentimiento del interesado.
-          </p>
-          <p>
-            <strong>Destinatarios:</strong> Tus datos serán guardados por Doppler, Zoho como CRM,
-            Digital Ocean, Cogeco Peer1 y Rackspace como empresas de hosting.
-          </p>
-          <p>
-            <strong>Información adicional:</strong> En la{' '}
-            <a href="/" className="link-green" target="_blank">
-              Política de Privacidad
-            </a>{' '}
-            de Doppler encontrarás información adicional sobre la recopilación y el uso de su
-            información personal por parte de Doppler, incluida información sobre acceso,
-            conservación, rectificación, eliminación, seguridad, transferencias transfronterizas y
-            otros temas.
-          </p>
+          <FormattedHTMLMessage id="signup.legal_HTML" />
         </div>
         <p className="content-promotion">
-          Pst! si tienes un código promocional podrás ingresarlo al confirmar tu cuenta.{' '}
-          <a href="/" className="link-green" target="_blank">
-            HELP
+          {' '}
+          {_('signup.promotion_code_reminder')}{' '}
+          <a
+            href={_('signup.promotion_code_help_url')}
+            className="link-green uppercase"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {_('common.help')}
           </a>
         </p>
         <footer>
-          <small>© 2019 Doppler LLC. Todos los derechos reservados.</small>
+          <small>{_('signup.copyright', { year: 2019 })}</small>
         </footer>
       </article>
       <section className="feature-panel">
         <article className="feature-content">
-          <h6>Editor de emails</h6>
-          <h3>Crea Emails en minutos y accede a nuestra Galería de Plantillas</h3>
-          <p>
-            Nuestras Plantillas para Email son totalmente Responsive y fácilmente editables desde
-            nuestro Editor HTML.
-          </p>
+          <h6>{_('feature_panel.email_editor')}</h6>
+          <h3>{_('feature_panel.email_editor_description')}</h3>
+          <p>{_('feature_panel.email_editor_remarks')}</p>
         </article>
       </section>
     </main>
   );
-}
+});
