fc8f199cd2847439016f853c032e31dd8e13f60f
Andrés Moschini <andresmoschini@gmail.com>
2019-03-26 11:00:15 -0300

    test(i18n): add test to ensure consistency between language files

---
+++ src/i18n/language-files.test.js
@@ -0,0 +1,25 @@
+import 'jest';
+import messages_es from './es.json';
+import messages_en from './en.json';
+import { flattenMessages } from './utils';
+
+const flattenEs = flattenMessages(messages_es);
+const flattenEn = flattenMessages(messages_en);
+const unsortedKeysEn = Object.keys(flattenEn);
+const unsortedKeysEs = Object.keys(flattenEs);
+const sortedKeysEn = unsortedKeysEn.sort();
+const sortedKeysEs = unsortedKeysEs.sort();
+
+describe('language files', () => {
+  it('should have the same number of keys', () => {
+    expect(flattenEn.length).toEqual(flattenEs.length);
+  });
+
+  it('should have the same keys in any order', () => {
+    expect(sortedKeysEn).toEqual(sortedKeysEs);
+  });
+
+  it('should have the keys in the same order', () => {
+    expect(unsortedKeysEn).toEqual(unsortedKeysEs);
+  });
+});
