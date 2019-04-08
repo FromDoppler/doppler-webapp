96b4dfd64f995e1da4c36349bd094f5ad277088b
Andrés Moschini <andresmoschini@gmail.com>
2019-04-08 17:15:28 -0300

    fix: fix Login page refresh refactoring this page as function in place of class

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { injectIntl } from 'react-intl';
 import { timeout } from '../../utils';
@@ -19,35 +19,23 @@ const getFormInitialValues = () =>
     {},
   );
 
-class Login extends React.Component {
-  constructor({ intl }) {
-    super();
+const Login = ({ intl, location }) => {
+  const [redirectAfterLogin, setRedirectAfterLogin] = useState(false);
+  const _ = (id, values) => intl.formatMessage({ id: id }, values);
 
-    this.state = {
-      redirectAfterLogin: false,
-    };
-
-    this.intl = intl;
-    this._ = this._.bind(this);
-    this.validate = this.validate.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  _ = (id, values) => this.intl.formatMessage({ id: id }, values);
-
-  validate = (values) => {
+  const validate = (values) => {
     const errors = {};
 
     if (!values[fieldNames.user]) {
-      errors[fieldNames.user] = this._('validation_messages.error_required_field');
+      errors[fieldNames.user] = _('validation_messages.error_required_field');
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values[fieldNames.user])) {
       // TODO: review if this is our desired validation
-      errors[fieldNames.user] = this._('validation_messages.error_invalid_email_address');
+      errors[fieldNames.user] = _('validation_messages.error_invalid_email_address');
     }
 
     if (!values[fieldNames.password]) {
       // TODO: I think that password validation has a different format
-      errors[fieldNames.password] = this._('validation_messages.error_required_field');
+      errors[fieldNames.password] = _('validation_messages.error_required_field');
     } else {
       // TODO: validate password
     }
@@ -55,109 +43,94 @@ class Login extends React.Component {
     return errors;
   };
 
-  onSubmit = async (values, { setSubmitting }) => {
+  const onSubmit = async (values, { setSubmitting }) => {
     // TODO: implement login submit
     await timeout(1500);
     setSubmitting(false);
-
-    this.setState({ redirectAfterLogin: true });
+    setRedirectAfterLogin(true);
   };
 
-  render() {
-    //TODO Add Doppler app redirect
-    const { from } = this.props.location.state
-      ? this.props.location.state
-      : { from: { pathname: '/' } };
+  if (redirectAfterLogin) {
+    //TODO: Also allow redirect to doppler legacy URLs
+    const from = (location.state && location.state.from) || { pathname: '/' };
+    return <Redirect to={from} />;
+  }
 
-    const { redirectAfterLogin } = this.state;
-
-    if (redirectAfterLogin) {
-      return <Redirect to={from} />;
-    }
-
-    return (
-      <main className="panel-wrapper">
-        <article className="main-panel">
-          <header>
-            <h1 className="logo-doppler-new">Doppler</h1>
-            <div className="language-selector">
-              {this.intl.locale === 'es' ? (
-                <div className="option" id="spanish-selector">
-                  <span>
-                    <strong>ES</strong>
-                  </span>
-                  <Link to="?lang=en" className="option--item">
-                    EN
-                  </Link>
-                </div>
-              ) : (
-                <div className="option" id="spanish-selector">
-                  <span>
-                    <strong>EN</strong>
-                  </span>
-                  <Link to="?lang=es" className="option--item">
-                    ES
-                  </Link>
-                </div>
-              )}
-            </div>
-          </header>
-          <h5>{this._('login.enter_doppler')}</h5>
-          <p className="content-subtitle">{this._('login.enter_doppler_sub')}</p>
-          <p className="content-subtitle">
-            {this._('login.you_want_create_account')}{' '}
-            <Link to="/signup" className="uppercase">
-              {this._('login.signup')}
-            </Link>
-          </p>
-          <Formik
-            initialValues={getFormInitialValues()}
-            validate={this.validate}
-            onSubmit={this.onSubmit}
-          >
-            <Form className="login-form">
-              <fieldset>
-                <FieldGroup>
-                  <InputFieldItem
-                    fieldName={fieldNames.user}
-                    label={this._('login.label_user')}
-                    type="email"
-                    placeholder={this._('signup.placeholder_email')}
-                  />
-                  <PasswordFieldItem
-                    fieldName={fieldNames.password}
-                    label={this._('signup.label_password')}
-                    placeholder={this._('signup.placeholder_password')}
-                  />
-                </FieldGroup>
-              </fieldset>
-              <fieldset>
-                <button
-                  type="submit"
-                  className="dp-button button--round button-medium primary-green"
-                >
-                  {this._('login.button_login')}
-                </button>
-                {/*
+  return (
+    <main className="panel-wrapper">
+      <article className="main-panel">
+        <header>
+          <h1 className="logo-doppler-new">Doppler</h1>
+          <div className="language-selector">
+            {intl.locale === 'es' ? (
+              <div className="option" id="spanish-selector">
+                <span>
+                  <strong>ES</strong>
+                </span>
+                <Link to="?lang=en" className="option--item">
+                  EN
+                </Link>
+              </div>
+            ) : (
+              <div className="option" id="spanish-selector">
+                <span>
+                  <strong>EN</strong>
+                </span>
+                <Link to="?lang=es" className="option--item">
+                  ES
+                </Link>
+              </div>
+            )}
+          </div>
+        </header>
+        <h5>{_('login.enter_doppler')}</h5>
+        <p className="content-subtitle">{_('login.enter_doppler_sub')}</p>
+        <p className="content-subtitle">
+          {_('login.you_want_create_account')}{' '}
+          <Link to="/signup" className="uppercase">
+            {_('login.signup')}
+          </Link>
+        </p>
+        <Formik initialValues={getFormInitialValues()} validate={validate} onSubmit={onSubmit}>
+          <Form className="login-form">
+            <fieldset>
+              <FieldGroup>
+                <InputFieldItem
+                  fieldName={fieldNames.user}
+                  label={_('login.label_user')}
+                  type="email"
+                  placeholder={_('signup.placeholder_email')}
+                />
+                <PasswordFieldItem
+                  fieldName={fieldNames.password}
+                  label={_('signup.label_password')}
+                  placeholder={_('signup.placeholder_password')}
+                />
+              </FieldGroup>
+            </fieldset>
+            <fieldset>
+              <button type="submit" className="dp-button button--round button-medium primary-green">
+                {_('login.button_login')}
+              </button>
+              {/*
                 // TODO: implement forgot password
                 */}
-              </fieldset>
-            </Form>
-          </Formik>
-          <footer>
-            <small>{this._('signup.copyright', { year: 2019 })}</small>
-          </footer>
+            </fieldset>
+          </Form>
+        </Formik>
+        <footer>
+          <small>{_('signup.copyright', { year: 2019 })}</small>
+        </footer>
+      </article>
+      <section className="feature-panel">
+        <article className="feature-content">
+          <h6>{_('feature_panel.email_editor')}</h6>
+          <h3>{_('feature_panel.email_editor_description')}</h3>
+          <p>{_('feature_panel.email_editor_remarks')}</p>
         </article>
-        <section className="feature-panel">
-          <article className="feature-content">
-            <h6>{this._('feature_panel.email_editor')}</h6>
-            <h3>{this._('feature_panel.email_editor_description')}</h3>
-            <p>{this._('feature_panel.email_editor_remarks')}</p>
-          </article>
-        </section>
-      </main>
-    );
-  }
-}
+      </section>
+    </main>
+  );
+};
 
 export default injectIntl(Login);
