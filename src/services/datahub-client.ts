fa5955e278fecf1d8f74a2379fb57c6eb833cda4
Andrés Moschini <andresmoschini@gmail.com>
2019-04-10 18:44:30 -0300

    feat: consume real Datahub API in datahub-client

--- src/services/datahub-client.ts
+++ src/services/datahub-client.ts
@@ -1,17 +1,15 @@
-import { AxiosInstance, AxiosStatic } from 'axios';
+import { AxiosInstance, AxiosStatic, AxiosPromise } from 'axios';
 import { RefObject } from 'react';
-import { DatahubConnectionData } from './app-session';
+import { AppSession, DatahubConnectionData } from './app-session';
 
 export interface DomainEntry {
-  id: number;
   name: string;
-  verified_date: Date;
+  verified_date: Date | null;
 }
 
 export type emailFilterOptions = 'with_email' | 'without_email' | null;
 
 export interface DatahubClient {
-  // See: https://github.com/MakingSense/Customer-Data-Hub/blob/0f28f0906b22198622f71867e1cdaf00abd41af1/apisec/swaggerDef.js#L74
   getAccountDomains(): Promise<DomainEntry[]>;
   getVisitsByPeriod(query: {
     domainName: number;
@@ -24,54 +22,10 @@ export interface DatahubClient {
   }): Promise<{ name: string; totalVisits: number }[]>;
 }
 
-const fakeData = [
-  {
-    id: 1,
-    name: 'www.fromdoppler.com',
-    verified_date: new Date('2017-12-17'),
-    pages: [],
-  },
-  {
-    id: 2,
-    name: 'www.makingsense.com',
-    verified_date: new Date('2010-12-17'),
-    pages: [],
-  },
-  {
-    id: 3,
-    name: 'www.google.com',
-    verified_date: new Date('2017-12-17'),
-    pages: [],
-  },
-];
-
-const fakePagesData = [
-  {
-    name: 'https://www.fromdoppler.com/email-marketing',
-    totalVisits: 10122,
-  },
-  {
-    name: 'https://www.fromdoppler.com/precios',
-    totalVisits: 9000,
-  },
-  {
-    name: 'https://www.fromdoppler.com/login',
-    totalVisits: 5001,
-  },
-  {
-    name: 'https://www.fromdoppler.com/productos',
-    totalVisits: 3800,
-  },
-  {
-    name: 'https://www.fromdoppler.com/servicios',
-    totalVisits: 1023,
-  },
-];
-
 export class HttpDatahubClient implements DatahubClient {
   private readonly axios: AxiosInstance;
   private readonly baseUrl: string;
-  private readonly connectionDataRef: RefObject<DatahubConnectionData>;
+  private readonly connectionDataRef: RefObject<AppSession>;
 
   constructor({
     axiosStatic,
@@ -80,20 +34,46 @@ export class HttpDatahubClient implements DatahubClient {
   }: {
     axiosStatic: AxiosStatic;
     baseUrl: string;
-    connectionDataRef: RefObject<DatahubConnectionData>;
+    connectionDataRef: RefObject<AppSession>;
   }) {
     this.baseUrl = baseUrl;
     this.axios = axiosStatic.create({
-      baseURL: baseUrl,
-      withCredentials: true,
+      baseURL: this.baseUrl,
     });
     this.connectionDataRef = connectionDataRef;
   }
 
-  // TODO: implement this class
+  private getDatahubConnectionData(): DatahubConnectionData {
+    const connectionData = this.connectionDataRef.current;
+    if (
+      !connectionData ||
+      connectionData.status !== 'authenticated' ||
+      !connectionData.jwtToken ||
+      !connectionData.datahubCustomerId
+    ) {
+      throw new Error('DataHub connection data is not available');
+    }
+    return connectionData;
+  }
+
+  private customerGet<T>(subUrl: string, params?: any): AxiosPromise<T> {
+    const { datahubCustomerId: customerId, jwtToken } = this.getDatahubConnectionData();
+    return this.axios.request<T>({
+      method: 'GET',
+      url: `/cdhapi/customers/${customerId}/${subUrl}`,
+      params: params,
+      headers: { Authorization: `Bearer ${jwtToken}` },
+    });
+  }
+
   public async getAccountDomains(): Promise<DomainEntry[]> {
-    console.log('getAccountDomains');
-    return fakeData.map((x) => ({ id: x.id, name: x.name, verified_date: x.verified_date }));
+    const response = await this.customerGet<{ items: any[] }>('domains');
+    return response.data.items
+      .filter((x) => x.enabled)
+      .map((x) => ({
+        name: x.domainName,
+        verified_date: (x.lastNavigationEventTime && new Date(x.lastNavigationEventTime)) || null,
+      }));
   }
 
   public async getVisitsByPeriod({
@@ -105,9 +85,14 @@ export class HttpDatahubClient implements DatahubClient {
     dateFrom: Date;
     emailFilter: emailFilterOptions;
   }): Promise<number> {
-    console.log('getVisitsByPeriod', { domainName, dateFrom, emailFilter });
-    const visits = Math.round(Math.random() * (100 - 1) + 1);
-    return visits;
+    const response = await this.customerGet<{ visitors_quantity: number }>(
+      `domains/${domainName}/visitors/quantity`,
+      {
+        startDate: dateFrom.toISOString(),
+        emailFilterBy: emailFilter,
+      },
+    );
+    return response.data.visitors_quantity;
   }
 
   public async getPagesRankingByPeriod({
@@ -117,7 +102,12 @@ export class HttpDatahubClient implements DatahubClient {
     domainName: number;
     dateFrom: Date;
   }): Promise<{ name: string; totalVisits: number }[]> {
-    console.log('getPagesRankingByPeriod', { domainName, dateFrom });
-    return fakePagesData.map((x) => ({ name: x.name, totalVisits: x.totalVisits }));
+    const response = await this.customerGet<{ items: { page: string; count: number }[] }>(
+      `domains/${domainName}/events/summarized-by-page`,
+      {
+        startDate: dateFrom.toISOString(),
+      },
+    );
+    return response.data.items.map((x) => ({ name: x.page, totalVisits: x.count }));
   }
 }
