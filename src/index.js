16ecca1423ff447e403e60a8cb3a66cd492bd7c2
Andrés Moschini <andresmoschini@gmail.com>
2019-04-10 19:09:23 -0300

    chore: use double Datahub client in development environment

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { HashRouter as Router } from 'react-router-dom';
 
 // Only used in development environment, it does not affect production build
 import { HardcodedDopplerLegacyClient } from './services/doppler-legacy-client.doubles';
+import { HardcodedDatahubClient } from './services/datahub-client.doubles';
 
 // TODO: this hardcoded data will depend by the app language
 const locale = navigator.language.toLowerCase().split(/[_-]+/)[0] || 'en';
@@ -18,6 +19,7 @@ const forcedServices =
   process.env.NODE_ENV === 'development'
     ? {
         dopplerLegacyClient: new HardcodedDopplerLegacyClient(),
+        datahubClient: new HardcodedDatahubClient(),
         RedirectToLogin: RedirectToInternalLogin,
       }
     : {};
