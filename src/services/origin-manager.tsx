26c3c1c8c436b567b671d98eee3710dd89767f44
Andrés Moschini <andresmoschini@gmail.com>
2019-04-18 20:14:37 -0300

    chore: refactor cache for origin stored in localStorage

--- src/services/origin-manager.tsx
+++ src/services/origin-manager.tsx
@@ -6,7 +6,20 @@ import queryString from 'query-string';
 const DopplerOriginLocalStorageKey = 'dopplerOrigin.value';
 const DopplerOriginDateLocalStorageKey = 'dopplerOrigin.date';
 
-let originIsInLocalStorage: boolean | null = null;
+let originCache:
+  | { readonly stored: true; readonly value: string; readonly unknown?: false }
+  | { readonly stored: false; readonly value?: undefined; readonly unknown?: undefined }
+  | { readonly unknown: true } = {
+  unknown: true,
+};
+
+function ensureOriginCache(localStorage: Storage) {
+  if (originCache.unknown) {
+    const value = localStorage.getItem(DopplerOriginLocalStorageKey);
+    originCache = value ? { stored: true, value: value } : { stored: false };
+  }
+  return originCache;
+}
 
 function _OriginCatcher({
   dependencies: { localStorage },
@@ -17,21 +30,20 @@ function _OriginCatcher({
 }) {
   const output = <></>;
   const parsedQuery = (location && location.search && queryString.parse(location.search)) || null;
-  const originFromUrl = parsedQuery && (parsedQuery['origin'] || parsedQuery['Origin']);
+  const originFromUrl =
+    parsedQuery && ((parsedQuery['origin'] || parsedQuery['Origin']) as string | undefined);
 
   if (!originFromUrl) {
     return output;
   }
 
   // Optimization to avoid too much local storage usage
-  if (originIsInLocalStorage === null) {
-    originIsInLocalStorage = !!localStorage.getItem(DopplerOriginLocalStorageKey);
-  }
+  const cache = ensureOriginCache(localStorage);
 
-  if (!originIsInLocalStorage) {
-    localStorage.setItem(DopplerOriginLocalStorageKey, originFromUrl as string);
+  if (!cache.stored) {
+    localStorage.setItem(DopplerOriginLocalStorageKey, originFromUrl);
     localStorage.setItem(DopplerOriginDateLocalStorageKey, new Date().toUTCString());
-    originIsInLocalStorage = true;
+    originCache = { stored: true, value: originFromUrl };
   }
 
   return output;
