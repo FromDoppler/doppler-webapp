0ee723a80568ba5d0222e8033eacab28cfe6e246
Andrés Moschini <andresmoschini@gmail.com>
2019-04-20 12:15:31 -0300

    chore: add a redirect component that keeps querystring

---
+++ src/components/RedirectWithQuery.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+function parse(path) {
+  const parsed = /^([^?]*)(\?(.+))?$/.exec(path);
+  const pathname = parsed[1];
+  const search = parsed[3];
+  return { pathname, search };
+}
+
+const RedirectWithQuery = ({ exact, from, to }) => (
+  <Route
+    exact={exact}
+    path={from}
+    component={({ location: { search } }) => {
+      const parsedTo = parse(to);
+      const parsedCurrent = parse(search);
+      const newSearch =
+        parsedTo.search && parsedCurrent.search
+          ? '?' + parsedTo.search + '&' + parsedCurrent.search
+          : parsedTo.search
+          ? '?' + parsedTo.search
+          : parsedCurrent.search
+          ? '?' + parsedCurrent.search
+          : '';
+      return (
+        <Redirect
+          to={{
+            pathname: parsedTo.pathname,
+            search: newSearch,
+          }}
+        />
+      );
+    }}
+  />
+);
+
+export default RedirectWithQuery;
