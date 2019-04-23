8d78d3b0114a25001d2b900a83347a8af5b43e9b
gbaamonde <gusvodesign@gmail.com>
2019-04-23 18:21:33 -0300

    feat: when a free account enters reports we should show you a text and a link to upgrade

--- src/components/DatahubRequired/DatahubRequired.js
+++ src/components/DatahubRequired/DatahubRequired.js
@@ -11,17 +11,30 @@ function DatahubRequired({
     appConfiguration: { dopplerLegacyUrl },
   },
 }) {
+  const freeAccount = true;
+
   return (
     <section className="container-reports">
       <div className="wrapper-kpi">
-        <div>
-          <FormattedMessage tagName="h4" id="reports.datahub_not_active_title" />
-          <FormattedHTMLMessage
-            tagName="div"
-            id="reports.datahub_not_active_HTML"
-            values={{ dopplerBaseUrl: dopplerLegacyUrl }}
-          />
-        </div>
+        {freeAccount ? (
+          <div>
+            <FormattedMessage tagName="h3" id="reports.upgrade_account_free_title" />
+            <FormattedHTMLMessage
+              tagName="div"
+              id="reports.upgrade_account_free_HTML"
+              values={{ dopplerBaseUrl: dopplerLegacyUrl }}
+            />
+          </div>
+        ) : (
+          <div>
+            <FormattedMessage tagName="h3" id="reports.datahub_not_active_title" />
+            <FormattedHTMLMessage
+              tagName="div"
+              id="reports.datahub_not_active_HTML"
+              values={{ dopplerBaseUrl: dopplerLegacyUrl }}
+            />
+          </div>
+        )}
       </div>
     </section>
   );
