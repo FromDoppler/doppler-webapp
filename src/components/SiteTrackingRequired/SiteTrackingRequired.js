117660c0bb73ed945af3f09bdd996c42593e261b
Andrés Moschini <andresmoschini@gmail.com>
2019-05-02 17:13:41 -0300

    feat: call to backend to activate trial

--- src/components/SiteTrackingRequired/SiteTrackingRequired.js
+++ src/components/SiteTrackingRequired/SiteTrackingRequired.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { InjectAppServices } from '../../services/pure-di';
 import { FormattedHTMLMessage, FormattedMessage } from 'react-intl';
 
@@ -19,43 +19,65 @@ export const SiteTrackingRequired = InjectAppServices(
     reason,
     dependencies: {
       appConfiguration: { dopplerLegacyUrl },
+      dopplerLegacyClient,
+      sessionManager,
     },
-  }) => (
-    <section className="container-reports">
-      <div className="dp-wrapper-messages">
-        {reason === SiteTrackingNotAvailableReasons.freeAccount ? (
-          // Free accounts cannot enable trial, they should buy
-          <>
-            <FormattedMessage tagName="h2" id="reports.upgrade_account_free_title" />
+  }) => {
+    const [isActivatingTrial, setIsActivatingTrial] = useState(false);
+
+    const activateTrial = async () => {
+      setIsActivatingTrial(true);
+      try {
+        await dopplerLegacyClient.activateSiteTrackingTrial();
+        sessionManager.restart();
+      } finally {
+        setIsActivatingTrial(false);
+      }
+    };
+
+    return (
+      <section className="container-reports">
+        <div className="dp-wrapper-messages">
+          {reason === SiteTrackingNotAvailableReasons.freeAccount ? (
+            // Free accounts cannot enable trial, they should buy
+            <>
+              <FormattedMessage tagName="h2" id="reports.upgrade_account_free_title" />
+              <FormattedHTMLMessage
+                tagName="div"
+                id="reports.upgrade_account_free_HTML"
+                values={{ dopplerBaseUrl: dopplerLegacyUrl }}
+              />
+            </>
+          ) : reason === SiteTrackingNotAvailableReasons.trialNotAccepted ? (
+            // Any paid account can enable the trial
+            <>
+              <FormattedMessage tagName="h2" id="reports.allow_enable_trial_title" />
+              <FormattedHTMLMessage tagName="div" id="reports.allow_enable_trial_HTML" />
+              <div className="dp-messages-actions">
+                <button
+                  onClick={activateTrial}
+                  className={
+                    'dp-button button-medium primary-green' +
+                    ((isActivatingTrial && ' button--loading') || '')
+                  }
+                  disabled={isActivatingTrial}
+                >
+                  <FormattedMessage id="reports.allow_enable_trial_button" />
+                </button>
+              </div>
+            </>
+          ) : (
+            // SiteTrackingNotAvailableReasons.featureDisabled
+            // SiteTrackingNotAvailableReasons.thereAreNotDomains
+            // SiteTrackingNotAvailableReasons.noDatahubId
             <FormattedHTMLMessage
-              tagName="div"
-              id="reports.upgrade_account_free_HTML"
+              className="patch-no-domains"
+              id="reports.no_domains_HTML"
               values={{ dopplerBaseUrl: dopplerLegacyUrl }}
             />
-          </>
-        ) : reason === SiteTrackingNotAvailableReasons.trialNotAccepted ? (
-          // Any paid account can enable the trial
-          <>
-            <FormattedMessage tagName="h2" id="reports.allow_enable_trial_title" />
-            <FormattedHTMLMessage tagName="div" id="reports.allow_enable_trial_HTML" />
-            <div className="dp-messages-actions">
-              {/* TODO: implement this action DBR-227 */}
-              <button className="dp-button button-medium primary-green">
-                <FormattedMessage id="reports.allow_enable_trial_button" />
-              </button>
-            </div>
-          </>
-        ) : (
-          // SiteTrackingNotAvailableReasons.featureDisabled
-          // SiteTrackingNotAvailableReasons.thereAreNotDomains
-          // SiteTrackingNotAvailableReasons.noDatahubId
-          <FormattedHTMLMessage
-            className="patch-no-domains"
-            id="reports.no_domains_HTML"
-            values={{ dopplerBaseUrl: dopplerLegacyUrl }}
-          />
-        )}
-      </div>
-    </section>
-  ),
+          )}
+        </div>
+      </section>
+    );
+  },
 );
