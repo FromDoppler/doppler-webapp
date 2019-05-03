2864ae65e6809863c55bf12f10295f45918a77e4
Andrés Moschini <andresmoschini@gmail.com>
2019-05-03 13:06:04 -0300

    chore: add FormattedMessageMarkdown component

---
+++ src/i18n/FormattedMessageMarkdown.js
@@ -0,0 +1,16 @@
+// Based on https://github.com/yahoo/react-intl/issues/513#issuecomment-252083860-permalink
+import React from 'react';
+import Markdown from 'react-remarkable';
+import { injectIntl } from 'react-intl';
+
+// TODO: do something to build markdown files inside es.json and en.json
+// to edit it in a more friendly way, or at least use a different format
+// than JSON to allow line breaks.
+// TODO: also consider memoize it, for example https://medium.com/@planttheidea/memoize-react-components-33377d7ebb6c
+// in order to avoid multiple processing of the same markdown source (Take
+// into account the message in the memoization.
+export const FormattedMessageMarkdown = injectIntl(
+  ({ id, defaultMessage, values, description, intl: { formatMessage }, ...rest }) => (
+    <Markdown source={formatMessage({ id, defaultMessage, description }, values)} {...rest} />
+  ),
+);
