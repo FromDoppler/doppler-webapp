490e1c39a4486c447d593ca160e30519b1c7f578
Andrés Moschini <andresmoschini@gmail.com>
2019-05-06 09:44:38 -0300

    chore: fix comment length

--- src/i18n/FormattedMessageMarkdown.js
+++ src/i18n/FormattedMessageMarkdown.js
@@ -7,11 +7,12 @@ import './FormattedMessageMarkdown.css';
 // TODO: do something to build markdown files inside es.json and en.json
 // to edit it in a more friendly way, or at least use a different format
 // than JSON to allow line breaks.
-// TODO: also consider memoize it, for example https://medium.com/@planttheidea/memoize-react-components-33377d7ebb6c
+// TODO: also consider memoize it, for example:
+// https://medium.com/@planttheidea/memoize-react-components-33377d7ebb6c
 // in order to avoid multiple processing of the same markdown source (Take
 // into account the message in the memoization.
-// TODO: also consider using always a linkTarget option as function detecting not webapp nor
-// doppler legacy links and apply _blank target to them.
+// TODO: also consider using always a linkTarget option as function detecting
+// not webapp nor doppler legacy links and apply _blank target to them.
 export const FormattedMessageMarkdown = injectIntl(
   ({ id, defaultMessage, values, description, intl: { formatMessage }, ...rest }) => (
     <Markdown source={formatMessage({ id, defaultMessage, description }, values)} {...rest} />
