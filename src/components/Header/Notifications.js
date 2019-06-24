d0799d63b8490a8c64b5aefd3293321ba291de87
gbaamonde <gusvodesign@gmail.com>
2019-06-24 10:41:47 -0300

    chore: add indentation

--- src/components/Header/Notifications.js
+++ src/components/Header/Notifications.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Notifications = ({ plan/*, notifications*/ }) => {
-    const notifications= [
-        "<strong>NUEVO: Comportamiento en Sitio</strong><br/><p class='text--small'>Programa un flujo automatizado de Emails que se env&#237;e de acuerdo a las p&#225;ginas que los Suscriptores hayan visitado en tu sitio. Para probarlo <a target='_self' href='https://app2.fromdoppler.com/ControlPanel/AccountPreferences/UpgradeAccount/?Plan=monthly'><strong>CONTRATA UN PLAN</strong></a>.</p>"
-      ];
-      const emptyNotificationText= "No tienes notificaciones pendientes.";
+const Notifications = ({ plan /*, notifications*/ }) => {
+  const notifications = [
+    "<strong>NUEVO: Comportamiento en Sitio</strong><br/><p class='text--small'>Programa un flujo automatizado de Emails que se env&#237;e de acuerdo a las p&#225;ginas que los Suscriptores hayan visitado en tu sitio. Para probarlo <a target='_self' href='https://app2.fromdoppler.com/ControlPanel/AccountPreferences/UpgradeAccount/?Plan=monthly'><strong>CONTRATA UN PLAN</strong></a>.</p>",
+  ];
+  const emptyNotificationText = 'No tienes notificaciones pendientes.';
   console.log(plan, notifications);
   return (
     <>
@@ -12,11 +12,11 @@ const Notifications = ({ plan/*, notifications*/ }) => {
         <span className="ms-icon icon-notification" />
       </span>
       <div className="user-menu helper--right notifications">
-          {/* revisar si siempre vamos a mostrar las notificaciones */}
+        {/* revisar si siempre vamos a mostrar las notificaciones */}
         {!plan.isFreeAccount || !notifications.length ? (
           <i>{emptyNotificationText}</i>
         ) : (
-            <div dangerouslySetInnerHTML={{ __html: notifications[0] }} />
+          <div dangerouslySetInnerHTML={{ __html: notifications[0] }} />
         )}
       </div>
     </>
