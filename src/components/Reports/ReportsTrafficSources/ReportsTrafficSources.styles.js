7370f70c72b46f5aa83b23ab980b786aa94564c1
gbaamonde <gusvodesign@gmail.com>
2019-08-30 11:36:10 -0300

    chore: Add color variables in ReportsTrafficSources

--- src/components/Reports/ReportsTrafficSources/ReportsTrafficSources.styles.js
+++ src/components/Reports/ReportsTrafficSources/ReportsTrafficSources.styles.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
+import colors from '../../shared/Colors';
 
 export const Container = styled.div`
   display: flex;
   flex-flow: wrap;
   & > div:nth-child(odd) {
-    background: #f6f6f6;
+    background: ${colors.SmoothGrey};
   }
   & > div:nth-child(n + 4) {
     margin-top: 10px;
@@ -14,8 +15,8 @@ export const Container = styled.div`
 export const TrafficSourceContainer = styled.div`
   flex: calc(100% / 3);
   box-shadow: 2px 0 4px 0 rgba(0, 0, 0, 0.2);
-  border-bottom: 1px solid #ccc;
-  border-top: 1px solid #ccc;
+  border-bottom: 1px solid ${colors.SoftGrey};
+  border-top: 1px solid ${colors.SoftGrey};
   padding: 40px 30px;
 `;
 
@@ -23,7 +24,7 @@ export const TrafficSourceHeader = styled.header`
   display: flex;
   justify-content: space-between;
   font-weight: bold;
-  color: #333;
+  color: ${colors.DarkGrey};
 
   h6 {
     line-height: 1;
