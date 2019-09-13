4aa195cde69abcb9086091a21a62706ad946e3e4
cbernat <cbernat@makingsense.com>
2019-09-13 12:34:00 -0300

    feat(build): build using dockerfile

--- build-n-publish.sh
+++ build-n-publish.sh
@@ -3,64 +3,56 @@
 pkgName="doppler-webapp"
 pkgVersion=${1:-"v0.0.0-build0"}
 cdnBaseUrl=${2:-"//cdn.fromdoppler.com/$pkgName"}
-pkgVersionQa="qa-$pkgVersion"
-pkgVersionInt="int-$pkgVersion"
-pkgVersionDevelopment="dev-$pkgVersion"
 pkgBuild=${4:-0}
 pkgCommitId=${5:-$(git rev-parse HEAD)}
+environments="int qa development production"
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
+
+# Stop script if unbound variable found (use ${var:-} if intentional)
+set -u
 
 # Lines added to get the script running in the script path shell context
 # reference: http://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
 cd $(dirname $0)
 
-sh ./verify-w-docker.sh
+# To avoid issues with MINGW y Git Bash, see:
+# https://github.com/docker/toolbox/issues/673
+# https://gist.github.com/borekb/cb1536a3685ca6fc0ad9a028e6a959e3
+export MSYS_NO_PATHCONV=1
+export MSYS2_ARG_CONV_EXCL="*"
+
+echo Publishing to Docker and Akamai...
+echo pkgName: $pkgName
+echo cdnBaseUrl: $cdnBaseUrl
+echo pkgVersion: $pkgVersion
+echo pkgBuild: $pkgBuild
+echo pkgCommitId: $pkgCommitId
 
 # Force pull the latest image version due to the cache not always is pruned immediately after an update is uploaded to docker hub
 docker pull dopplerrelay/doppler-relay-akamai-publish
 
-sh ./build-w-docker.sh $pkgVersion $cdnBaseUrl production $pkgVersion $pkgBuild $pkgCommitId
-docker run --rm \
-    -e AKAMAI_CDN_HOSTNAME \
-    -e AKAMAI_CDN_USERNAME \
-    -e AKAMAI_CDN_PASSWORD \
-    -e AKAMAI_CDN_CPCODE \
-    -e "PROJECT_NAME=$pkgName" \
-    -e "VERSION_NAME=$pkgVersion" \
-    -v `pwd`/build:/source \
-    dopplerrelay/doppler-relay-akamai-publish
+for environment in ${environments}; do
+    echo Publishing ${environment}...
 
-sh ./build-w-docker.sh $pkgVersion $cdnBaseUrl qa $pkgVersion $pkgBuild $pkgCommitId
-docker run --rm \
-    -e AKAMAI_CDN_HOSTNAME \
-    -e AKAMAI_CDN_USERNAME \
-    -e AKAMAI_CDN_PASSWORD \
-    -e AKAMAI_CDN_CPCODE \
-    -e "PROJECT_NAME=$pkgName" \
-    -e "VERSION_NAME=$pkgVersionQa" \
-    -v `pwd`/build:/source \
-    dopplerrelay/doppler-relay-akamai-publish
+    if test "$environment" = "production";
+    then  
+        env_version=$pkgVersion;
+    else
+        env_version=$environment-$pkgVersion;
+    fi
 
-sh ./build-w-docker.sh $pkgVersion $cdnBaseUrl int $pkgVersion $pkgBuild $pkgCommitId
-docker run --rm \
-    -e AKAMAI_CDN_HOSTNAME \
-    -e AKAMAI_CDN_USERNAME \
-    -e AKAMAI_CDN_PASSWORD \
-    -e AKAMAI_CDN_CPCODE \
-    -e "PROJECT_NAME=$pkgName" \
-    -e "VERSION_NAME=$pkgVersionInt" \
-    -v `pwd`/build:/source \
-    dopplerrelay/doppler-relay-akamai-publish
+    echo deploying into $env_version folder ...
 
-sh ./build-w-docker.sh $pkgVersion $cdnBaseUrl development $pkgVersion $pkgBuild $pkgCommitId
-docker run --rm \
-    -e AKAMAI_CDN_HOSTNAME \
-    -e AKAMAI_CDN_USERNAME \
-    -e AKAMAI_CDN_PASSWORD \
-    -e AKAMAI_CDN_CPCODE \
-    -e "PROJECT_NAME=$pkgName" \
-    -e "VERSION_NAME=$pkgVersionDevelopment" \
-    -v `pwd`/build:/source \
-    dopplerrelay/doppler-relay-akamai-publish
+    docker build --pull \
+        --build-arg environment=$environment \
+        --build-arg env_version=$env_version \
+        --build-arg cdn_hostname=$AKAMAI_CDN_HOSTNAME \
+        --build-arg cdn_username=$AKAMAI_CDN_USERNAME \
+        --build-arg cdn_password=$AKAMAI_CDN_PASSWORD \
+        --build-arg cdn_cpcode=$AKAMAI_CDN_CPCODE \
+        -f Dockerfile.BUILDS_AND_CDN \
+        .
+
+done
