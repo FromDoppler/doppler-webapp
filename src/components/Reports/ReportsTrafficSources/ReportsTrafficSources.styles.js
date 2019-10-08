8c312ab6fa3525e03af1ae9ccb8e8931aa7740f2
gbaamonde <gusvodesign@gmail.com>
2019-10-08 10:54:33 -0300

    fix: remove box-shadow from traffic sources

--- src/components/Reports/ReportsTrafficSources/ReportsTrafficSources.styles.js
+++ src/components/Reports/ReportsTrafficSources/ReportsTrafficSources.styles.js
@@ -34,7 +34,6 @@ export const ListContainer = styled.div`
 
 export const ListItem = styled.div`
   flex: calc(100% / 3);
-  box-shadow: 2px 0 4px 0 rgba(0, 0, 0, 0.2);
   border-bottom: 1px solid ${colors.softGrey};
   border-top: 1px solid ${colors.softGrey};
   padding: ${spacings.spacesLvl6} ${spacings.spacesLvl5} ${spacings.spacesLvl7};
