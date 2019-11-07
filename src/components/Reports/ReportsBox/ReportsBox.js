465c65d2b856fb0cede017580b7daf80f8a56b56
cbernat <cbernat@makingsense.com>
2019-11-07 15:09:24 -0300

    refactor(reportBox): translate to funcitional component with hooks
    Issue: DW-93

--- src/components/Reports/ReportsBox/ReportsBox.js
+++ src/components/Reports/ReportsBox/ReportsBox.js
@@ -1,122 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { InjectAppServices } from '../../../services/pure-di';
 import { FormattedMessage } from 'react-intl';
 import { FormattedDateRangeText } from '../../shared/FormattedDateRangeText/FormattedDateRangeText';
 
-class ReportsBox extends React.Component {
-  /**
-   * @param { Object } props - props
-   * @param { import('../../../services/pure-di').AppServices } props.dependencies
-   */
-  constructor({ dependencies: { datahubClient } }) {
-    super();
+/**
+ * @param { Object } props - props
+ * @param { import('../../../services/pure-di').AppServices } props.dependencies
+ */
+const ReportsBox = ({
+  domainName,
+  dateFrom,
+  dateTo,
+  withEmail,
+  withoutEmail,
+  today,
+  dependencies: { datahubClient },
+}) => {
+  const [visits, setVisits] = useState(null);
 
-    this.datahubClient = datahubClient;
-
-    this.state = {
-      visits: null,
+  useEffect(() => {
+    const fetchVisitsByPeriod = async (domainName, dateFrom, dateTo) => {
+      let asyncRequest = datahubClient.getTotalVisitsOfPeriod({
+        domainName: domainName,
+        dateFrom: dateFrom,
+        dateTo: dateTo,
+        emailFilter: withEmail ? 'with_email' : withoutEmail ? 'without_email' : null,
+      });
+      const visits = await asyncRequest;
+      asyncRequest = null;
+      setVisits(visits);
     };
+    fetchVisitsByPeriod(domainName, dateFrom, dateTo);
+  }, [domainName, dateFrom, dateTo, datahubClient, withEmail, withoutEmail]);
 
-    this.fetchVisitsByPeriod = this.fetchVisitsByPeriod.bind(this);
-  }
-
-  async fetchVisitsByPeriod(domainName, dateFrom, dateTo) {
-    this.asyncRequest = this.datahubClient.getTotalVisitsOfPeriod({
-      domainName: domainName,
-      dateFrom: dateFrom,
-      dateTo: dateTo,
-      emailFilter: this.props.withEmail
-        ? 'with_email'
-        : this.props.withoutEmail
-        ? 'without_email'
-        : null,
-    });
-    const visits = await this.asyncRequest;
-    this.asyncRequest = null;
-    this.setState({
-      visits: visits,
-    });
-  }
-
-  componentWillUnmount() {
-    // TODO: abort request or at least side effects after finish
-    this.asyncRequest = null;
-  }
-
-  componentDidMount() {
-    this.fetchVisitsByPeriod(this.props.domainName, this.props.dateFrom, this.props.dateTo);
-  }
-
-  static getDerivedStateFromProps(props, state) {
-    if (
-      props.domainName !== state.prevDomainName ||
-      props.dateFrom !== state.prevDateFrom ||
-      props.dateTo !== state.prevDateTo
-    ) {
-      return {
-        visits: null,
-        prevDomainName: props.domainName,
-        prevDateFrom: props.dateFrom,
-        prevDateTo: props.dateTo,
-      };
-    }
-
-    // No state update necessary
-    return null;
-  }
-
-  componentDidUpdate() {
-    if (this.state.visits === null) {
-      this.fetchVisitsByPeriod(this.props.domainName, this.props.dateFrom, this.props.dateTo);
-    }
-  }
-
-  render() {
-    const {
-      state: { visits },
-      props: { dateFrom, dateTo, today },
-    } = this;
-
-    return (
-      <div className={visits === 0 ? 'dp-box-shadow warning--kpi' : 'dp-box-shadow'}>
-        {visits === null ? (
-          <div className="loading-box" />
-        ) : this.props.withEmail ? (
-          <>
-            <div className="box-border--bottom">
-              <h3 className="number-kpi">{visits}</h3>
-              <h6 className="subtitle-kpi">
-                <FormattedMessage id="reports_box.visits_with_email" />
-              </h6>
-              <small className="date-range">
-                <FormattedDateRangeText dateFrom={dateFrom} dateTo={dateTo} today={today} />
-              </small>
-            </div>
-            <p className="text-kpi">
-              <FormattedMessage id="reports_box.visits_description_with_email" />
-            </p>
-          </>
-        ) : this.props.withoutEmail ? (
-          <>
-            <div className="box-border--bottom">
-              <h3 className="number-kpi">{visits}</h3>
-              <h6 className="subtitle-kpi">
-                <FormattedMessage id="reports_box.visits_without_emails" />
-              </h6>
-              <small className="date-range">
-                <FormattedDateRangeText dateFrom={dateFrom} dateTo={dateTo} today={today} />
-              </small>
-            </div>
-            <p className="text-kpi">
-              <FormattedMessage id="reports_box.visits_description_without_emails" />
-            </p>
-          </>
-        ) : (
-          <span>Unexpected error</span>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={visits === 0 ? 'dp-box-shadow warning--kpi' : 'dp-box-shadow'}>
+      {visits === null ? (
+        <div className="loading-box" />
+      ) : withEmail ? (
+        <>
+          <div className="box-border--bottom">
+            <h3 className="number-kpi">{visits}</h3>
+            <h6 className="subtitle-kpi">
+              <FormattedMessage id="reports_box.visits_with_email" />
+            </h6>
+            <small className="date-range">
+              <FormattedDateRangeText dateFrom={dateFrom} dateTo={dateTo} today={today} />
+            </small>
+          </div>
+          <p className="text-kpi">
+            <FormattedMessage id="reports_box.visits_description_with_email" />
+          </p>
+        </>
+      ) : withoutEmail ? (
+        <>
+          <div className="box-border--bottom">
+            <h3 className="number-kpi">{visits}</h3>
+            <h6 className="subtitle-kpi">
+              <FormattedMessage id="reports_box.visits_without_emails" />
+            </h6>
+            <small className="date-range">
+              <FormattedDateRangeText dateFrom={dateFrom} dateTo={dateTo} today={today} />
+            </small>
+          </div>
+          <p className="text-kpi">
+            <FormattedMessage id="reports_box.visits_description_without_emails" />
+          </p>
+        </>
+      ) : (
+        <span>Unexpected error</span>
+      )}
+    </div>
+  );
+};
 
 export default InjectAppServices(ReportsBox);
