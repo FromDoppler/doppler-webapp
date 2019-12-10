2ec01d8f12b47461aedb60077385ab8e05d00e01
fcoronelmakingsense <fcoronel@makingsense.com>
2019-12-10 14:06:03 -0300

    refactor(App): change class to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import DopplerIntlProvider from './i18n/DopplerIntlProvider';
 import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
@@ -16,109 +16,95 @@ import Shopify from './components/Integrations/Shopify/Shopify';
 import SignupConfirmation from './components/Signup/SignupConfirmation';
 import SubscriberHistory from './components/Reports/SubscriberHistory/SubscriberHistory';
 
-class App extends Component {
-  /**
-   * @param { Object } props - props
-   * @param { string } props.locale - locale
-   * @param { import('./services/pure-di').AppServices } props.dependencies - dependencies
-   */
-  constructor({ locale, dependencies: { appSessionRef, sessionManager } }) {
-    super();
+/**
+ * @param { Object } props - props
+ * @param { string } props.locale - locale
+ * @param { import('./services/pure-di').AppServices } props.dependencies - dependencies
+ */
+const App = ({ locale, location, dependencies: { appSessionRef, sessionManager } }) => {
+  const [state, setState] = useState({
+    dopplerSession: appSessionRef.current,
+    i18nLocale: locale,
+  });
 
-    this.updateSession = this.updateSession.bind(this);
+  const langFromUrl = useRef(null);
 
-    this.sessionManager = sessionManager;
-
-    this.state = {
-      // TODO: consider removing dopplerSession from the state
-      dopplerSession: appSessionRef.current,
-      i18nLocale: locale,
-    };
-  }
-
-  componentDidMount() {
-    this.sessionManager.initialize(this.updateSession);
-  }
-
-  componentWillUnmount() {
-    this.sessionManager.finalize();
-  }
-
-  static getDerivedStateFromProps(props, state) {
-    const { lang: langFromUrl } =
-      props.location && props.location.search && queryString.parse(props.location.search);
-
-    const expectedLang = availableLanguageOrNull(langFromUrl);
-
-    if (state.langFromUrl !== expectedLang) {
-      return expectedLang
-        ? {
-            langFromUrl: expectedLang,
-            i18nLocale: expectedLang,
-          }
-        : { langFromUrl: null };
-    }
-
-    // No state update necessary
-    return null;
-  }
-
-  updateSession(dopplerSession) {
-    const stateChanges = { dopplerSession: dopplerSession };
-
-    if (
-      !this.state.langFromUrl &&
-      dopplerSession.userData &&
-      dopplerSession.userData.user &&
-      dopplerSession.userData.user.lang
-    ) {
-      stateChanges.i18nLocale = dopplerSession.userData.user.lang;
-    }
-
-    this.setState(stateChanges);
-  }
-
-  render() {
-    const { i18nLocale } = this.state;
-
+  const getI18nLocale = (dopplerSession, prevI18nLocale) => {
     return (
-      <DopplerIntlProvider locale={i18nLocale}>
-        <>
-          <Helmet>
-            <html lang={i18nLocale} />
-          </Helmet>
-          <OriginCatcher />
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={({ location }) =>
-                location.hash.length && process.env.REACT_APP_ROUTER !== 'hash' ? (
-                  <Redirect to={location.hash.replace('#', '')} />
-                ) : (
-                  <RedirectToLegacyUrl to="/Campaigns/Draft" />
-                )
-              }
-            />
-            <PrivateRoute path="/reports/" exact requireSiteTracking component={Reports} />
-            <PrivateRoute path="/integrations/shopify" exact component={Shopify} />
-            <PrivateRoute path="/reports/subscriber-history" exact component={SubscriberHistory} />
-            <PublicRouteWithLegacyFallback exact path="/login" />
-            <PublicRouteWithLegacyFallback exact path="/signup" />
-            <PublicRouteWithLegacyFallback exact path="/login/reset-password" />
-            <Route path="/signup/confirmation" exact component={SignupConfirmation} />
-            <RedirectWithQuery exact from="/ingresa" to="/login?lang=es" />
-            <RedirectWithQuery exact from="/registrate" to="/signup?lang=es" />
-            <RedirectWithQuery exact from="/ingresa/cambiar-clave" to="/forgot-password?lang=es" />
-            <RedirectWithQuery exact from="/forgot-password" to="/login/reset-password" />
-            {/* TODO: Implement NotFound page in place of redirect all to reports */}
-            {/* <Route component={NotFound} /> */}
-            <Route component={() => <Redirect to={{ pathname: '/reports' }} />} />
-          </Switch>
-        </>
-      </DopplerIntlProvider>
+      (!langFromUrl.current &&
+        dopplerSession.userData &&
+        dopplerSession.userData.user &&
+        dopplerSession.userData.user.lang) ||
+      prevI18nLocale
     );
-  }
-}
+  };
+
+  useEffect(() => {
+    const updateSession = (dopplerSession) => {
+      setState((prevState) => ({
+        i18nLocale: getI18nLocale(dopplerSession, prevState.i18nLocale),
+        dopplerSession: dopplerSession,
+      }));
+    };
+    sessionManager.initialize(updateSession);
+    return () => {
+      sessionManager.finalize();
+    };
+  }, [sessionManager]);
+
+  useEffect(() => {
+    const { lang: langFromUrlParameter } =
+      location && location.search && queryString.parse(location.search);
+
+    const expectedLang = availableLanguageOrNull(langFromUrlParameter);
+
+    if (!expectedLang) {
+      langFromUrl.current = null;
+    } else if (!langFromUrl.current || langFromUrl.current !== expectedLang) {
+      setState({
+        i18nLocale: expectedLang,
+      });
+      langFromUrl.current = expectedLang;
+    }
+  }, [location]);
+
+  return (
+    <DopplerIntlProvider locale={state.i18nLocale}>
+      <>
+        <Helmet>
+          <html lang={state.i18nLocale} />
+        </Helmet>
+        <OriginCatcher />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={({ location }) =>
+              location.hash.length && process.env.REACT_APP_ROUTER !== 'hash' ? (
+                <Redirect to={location.hash.replace('#', '')} />
+              ) : (
+                <RedirectToLegacyUrl to="/Campaigns/Draft" />
+              )
+            }
+          />
+          <PrivateRoute path="/reports/" exact requireSiteTracking component={Reports} />
+          <PrivateRoute path="/integrations/shopify" exact component={Shopify} />
+          <PrivateRoute path="/reports/subscriber-history" exact component={SubscriberHistory} />
+          <PublicRouteWithLegacyFallback exact path="/login" />
+          <PublicRouteWithLegacyFallback exact path="/signup" />
+          <PublicRouteWithLegacyFallback exact path="/login/reset-password" />
+          <Route path="/signup/confirmation" exact component={SignupConfirmation} />
+          <RedirectWithQuery exact from="/ingresa" to="/login?lang=es" />
+          <RedirectWithQuery exact from="/registrate" to="/signup?lang=es" />
+          <RedirectWithQuery exact from="/ingresa/cambiar-clave" to="/forgot-password?lang=es" />
+          <RedirectWithQuery exact from="/forgot-password" to="/login/reset-password" />
+          {/* TODO: Implement NotFound page in place of redirect all to reports */}
+          {/* <Route component={NotFound} /> */}
+          <Route component={() => <Redirect to={{ pathname: '/reports' }} />} />
+        </Switch>
+      </>
+    </DopplerIntlProvider>
+  );
+};
 
 export default withRouter(InjectAppServices(App));
