fc42d97138473b5db3a2382aa4139f91b3ee17f2
cbernat <cbernat@makingsense.com>
2019-12-26 10:47:45 -0300

    feat: add external url whitelisted

--- src/components/SafeRedirect.js
+++ src/components/SafeRedirect.js
@@ -8,6 +8,32 @@ import RedirectToExternalUrl from './RedirectToExternalUrl';
  * @param { import('../services/pure-di').AppServices } props.dependencies
  */
 function SafeRedirect({ to, dependencies }) {
-  return <RedirectToExternalUrl to={dependencies.appConfiguration.dopplerLegacyUrl + to} />;
+  const isPartialUrl = (url) => {
+    return /^\/[^/]/.test(url);
+  };
+
+  const isWhitelisted = (url) => {
+    const loginWhitelist = [
+      'https://academy.fromdoppler.com/',
+      'http://academy.fromdoppler.com/',
+      'http://prod.doppleracademy.com/',
+      'https://prod.doppleracademy.com/',
+      'https://doppleracademy.com/',
+      'http://doppleracademy.com/',
+    ];
+    return loginWhitelist.some((element) => url.startsWith(element));
+  };
+
+  if (isPartialUrl(to)) {
+    return <RedirectToExternalUrl to={dependencies.appConfiguration.dopplerLegacyUrl + to} />;
+  } else if (isWhitelisted(to)) {
+    return <RedirectToExternalUrl to={to} />;
+  } else {
+    return (
+      <RedirectToExternalUrl
+        to={dependencies.appConfiguration.dopplerLegacyUrl + '/Campaigns/Draft'}
+      />
+    );
+  }
 }
 export default InjectAppServices(SafeRedirect);
