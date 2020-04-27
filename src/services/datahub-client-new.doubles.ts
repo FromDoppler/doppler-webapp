11f4895f95b436fecbf32fe2dc5c6788e615810d
fcoronelmakingsense <fcoronel@makingsense>
2020-04-27 15:43:11 -0300

    chore(datahub-client-new): add new doubles service

---
+++ src/services/datahub-client-new.doubles.ts
@@ -0,0 +1,43 @@
+import {
+  DatahubClientNew,
+  DomainsResult,
+} from './datahub-client-new';
+import { timeout } from '../utils';
+
+const domains = [
+  {
+    name: 'www.fromdoppler.com',
+    verified_date: new Date('2010-12-17'),
+    pages: [],
+  },
+  {
+    name: 'www.makingsense.com',
+    verified_date: null,
+    pages: [],
+  },
+  {
+    name: 'www.google.com',
+    verified_date: new Date('2017-12-17'),
+    pages: [],
+  },
+];
+
+
+export class HardcodedDatahubClientNew implements DatahubClientNew {
+  public async getAccountDomains(): Promise<DomainsResult> {
+    console.log('getAccountDomains');
+    await timeout(1500);
+    const data = domains.map((x) => ({ name: x.name, verified_date: x.verified_date }));
+    return {
+      success: true,
+      value: data,
+    }
+
+    //return {
+    //  success: false,
+    //  error: new Error('Dummy error'),
+    //};
+  }
+
+
+}
