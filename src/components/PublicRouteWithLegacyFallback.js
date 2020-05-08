16cde23ea8cfea301d84bad87f398f95f700a711
gbaamonde <gusvodesign@gmail.com>
2020-05-08 11:41:43 -0300

    feat: add ancestor for library operation
    - component style adjustment
    - update build version

--- src/components/PublicRouteWithLegacyFallback.js
+++ src/components/PublicRouteWithLegacyFallback.js
@@ -80,7 +80,7 @@ function PublicRouteWithLegacyFallback({
   return (
     <>
       <Helmet>
-        <body className="showZohoTitleDiv" />
+        <body className="showZohoTitleDiv dp-library" />
       </Helmet>
       <Route
         {...rest}
