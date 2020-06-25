c61d719c69817ab2ca2659f89df1336c4e115b2d
lucianaDilizio <lgarcia@makingsense.com>
2020-06-25 11:19:13 -0300

    chore: create reusable subscriberPage element

--- src/components/shared/SubscriberPage/SubscriberPage.js
+++ src/components/shared/SubscriberPage/SubscriberPage.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { InjectAppServices } from '../../../services/pure-di';
 import { Loading } from '../../Loading/Loading';
-import SafeRedirect from '../../SafeRedirect';
+import queryString from 'query-string';
+import { extractParameter, replaceSpaceWithSigns } from '../../../utils';
 import SubscriberInfo from '../SubscriberInfo/SubscriberInfo';
+import SafeRedirect from '../../SafeRedirect';
 
-const SubscriberInfoContainer = ({ email, dependencies: { dopplerApiClient } }) => {
+const SubscriberPage = ({
+  location,
+  header: Header,
+  content: Content,
+  dependencies: { dopplerApiClient },
+}) => {
   const [stateSubscriber, setStateSubscriber] = useState({ loading: true });
+
+  const email = replaceSpaceWithSigns(extractParameter(location, queryString.parse, 'email'));
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,6 +23,7 @@ const SubscriberInfoContainer = ({ email, dependencies: { dopplerApiClient } }) 
       if (responseSubscriber.success) {
         const subscriber = {
           ...responseSubscriber.value,
+          // TODO: move this to api client
           firstName: responseSubscriber.value.fields.find((x) => x.name === 'FIRSTNAME'),
           lastName: responseSubscriber.value.fields.find((x) => x.name === 'LASTNAME'),
         };
@@ -35,13 +45,23 @@ const SubscriberInfoContainer = ({ email, dependencies: { dopplerApiClient } }) 
 
   return (
     <>
-      {stateSubscriber.loading ? (
-        <Loading />
-      ) : (
-        <SubscriberInfo subscriber={stateSubscriber.subscriber} />
-      )}
+      <Header />
+      <section className="dp-container">
+        <div className="dp-rowflex">
+          <div className="col-sm-12 m-b-36">
+            <div className="dp-block-wlp dp-box-shadow">
+              {stateSubscriber.loading ? (
+                <Loading />
+              ) : (
+                <SubscriberInfo subscriber={stateSubscriber.subscriber} />
+              )}
+              <Content subscriber={stateSubscriber.subscriber} />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
 
-export default InjectAppServices(SubscriberInfoContainer);
+export default InjectAppServices(SubscriberPage);
