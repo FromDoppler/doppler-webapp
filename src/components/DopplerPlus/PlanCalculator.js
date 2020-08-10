fd41302f8cd482e230552c1ee2edb0cc9d2b0299
lucianaDilizio <lgarcia@makingsense.com>
2020-08-10 11:18:01 -0300

    feat: add discounts for plans

--- src/components/DopplerPlus/PlanCalculator.js
+++ src/components/DopplerPlus/PlanCalculator.js
@@ -15,6 +15,13 @@ const PlanCalculator = () => {
     { idPlan: 9, price: 460, amount: 100000 },
   ];
 
+  const discountsList = [
+    { id: 1, percent: 0, monthsAmmount: 1, description: 'Mensual' },
+    { id: 2, percent: 5, monthsAmmount: 3, description: 'Trimestral' },
+    { id: 3, percent: 15, monthsAmmount: 6, description: 'Semestral' },
+    { id: 4, percent: 25, monthsAmmount: 12, description: 'Anual' },
+  ];
+
   const plansTooltipDescriptions = plansList.map((plan) => {
     return plan.amount + ' Suscriptores';
   });
@@ -24,6 +31,10 @@ const PlanCalculator = () => {
   const [currentPlan, updateSelectedPlan] = useReducer((currentPlan, index) => {
     return plansList[index] || currentPlan;
   }, initialPlan);
+
+  const [currentDiscount, applyDiscount] = useReducer((discount, index) => {
+    return discountsList[index] || discount;
+  }, discountsList[0]);
 
   return (
     <div className="p-t-54 p-b-54" style={{ backgroundColor: '#f6f6f6', flex: '1' }}>
@@ -36,9 +47,39 @@ const PlanCalculator = () => {
               {/* TODO: change this to intl elemnt */}
               ¿Cuántos contactos tienes? Utiliza el slider para calcular el costo final de tu Plan
             </p>
+            <div style={{ marginBottom: '40px' }}>
+              {discountsList.map((discount, index) => (
+                <button
+                  key={index}
+                  style={{
+                    padding: '10px',
+                    border: '1px solid #000',
+                    backgroundColor: discount.id === currentDiscount.id ? '#33ad73' : '#f6f6f6',
+                  }}
+                  onClick={() => {
+                    applyDiscount(index);
+                  }}
+                >
+                  {discount.description}
+                </button>
+              ))}
+            </div>
+            {currentDiscount.percent ? (
+              <p style={{ textDecoration: 'line-through' }}>
+                US${currentPlan.price * currentDiscount.monthsAmmount}
+              </p>
+            ) : (
+              <></>
+            )}
             <span>US$</span>
-            <span style={{ fontSize: '40px' }}>{currentPlan.price}</span>
-            <p>por mes</p>
+            <span style={{ fontSize: '40px' }}>
+              {Math.round(
+                currentPlan.price *
+                  (1 - currentDiscount.percent / 100) *
+                  currentDiscount.monthsAmmount,
+              )}
+            </span>
+            <p>{currentDiscount.description}</p>
             <Slider
               tooltipDescriptions={plansTooltipDescriptions}
               defaultValue={0}
