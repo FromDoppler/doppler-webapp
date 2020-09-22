ff9b84f3374053411df802da7c1e2cd8d18a795f
cbernat <cbernat@makingsense.com>
2020-09-22 15:25:25 -0300

    chore(PlanCalculator): remove tooltip from slider

--- src/components/shared/Slider/Slider.js
+++ src/components/shared/Slider/Slider.js
@@ -1,19 +1,12 @@
 import React, { useState } from 'react';
-import * as S from './Slider.styles';
 
 export const Slider = ({ tooltipDescriptions, defaultValue, handleChange }) => {
-  const [currentTooltipDescription, setCurrentTooltipDescription] = useState(
-    tooltipDescriptions[defaultValue],
-  );
   const [dataProgress, setDataProgress] = useState(
     (100 / tooltipDescriptions.length) * defaultValue,
   );
   // TODO: add styles
   return (
     <>
-      <S.SliderTooltip dataProgress={dataProgress}>
-        <span>{currentTooltipDescription}</span>
-      </S.SliderTooltip>
       <input
         style={{ marginTop: '100px', padding: '0' }}
         type="range"
@@ -22,7 +15,6 @@ export const Slider = ({ tooltipDescriptions, defaultValue, handleChange }) => {
         step={1}
         defaultValue={defaultValue}
         onChange={(e) => {
-          setCurrentTooltipDescription(tooltipDescriptions[e.target.value]);
           setDataProgress((100 / tooltipDescriptions.length) * e.target.value);
           handleChange(e.target.value);
         }}
