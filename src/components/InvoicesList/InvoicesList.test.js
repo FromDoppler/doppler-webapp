e7418a05c605f4e2b4cc65317cf7c57de3c9af42
jhoffman-ms <jmh248@gmail.com>
2020-10-07 16:03:41 -0300

    feat(invoices): add pagination into invoices list

--- src/components/InvoicesList/InvoicesList.test.js
+++ src/components/InvoicesList/InvoicesList.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render, cleanup, waitFor } from '@testing-library/react';
+import { render, cleanup, waitFor, getByTitle } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import IntlProvider from '../../i18n/DopplerIntlProvider.double-with-ids-as-values';
 import { AppServicesProvider } from '../../services/pure-di';
 import { BrowserRouter } from 'react-router-dom';
 import InvoicesList from './InvoicesList';
+import { notDeepEqual } from 'assert';
 
 describe('InvoiceList component', () => {
   afterEach(cleanup);
@@ -195,5 +196,117 @@ describe('InvoiceList component', () => {
 
     // Assert
     await waitFor(() => expect(getByText('invoices_list.no_download_msg')).toBeInTheDocument());
+  });
+
+  it('should show a pagination when there are invoices', async () => {
+    // Arrange
+    const invoicesCollection = {
+      items: [
+        {
+          accountId: 'CD0000000073689',
+          product: 'Prod 1',
+          date: new Date('2020-09-25T00:00:00'),
+          currency: 'ARS',
+          amount: 1500,
+          filename: 'invoice_2020-10-05_10.pdf',
+          downloadInvoiceUrl: '',
+        },
+      ],
+      totalItems: 1,
+    };
+
+    const dopplerBillingApiClientDouble = {
+      getInvoices: async () => {
+        return { success: true, value: invoicesCollection };
+      },
+    };
+
+    // Act
+    const { container } = render(
+      <AppServicesProvider
+        forcedServices={{
+          dopplerBillingApiClient: dopplerBillingApiClientDouble,
+        }}
+      >
+        <IntlProvider>
+          <BrowserRouter>
+            <InvoicesList />
+          </BrowserRouter>
+        </IntlProvider>
+      </AppServicesProvider>,
+    );
+
+    // Assert
+    await waitFor(() => {
+      expect(container.querySelector('.dp-pagination')).toBeInTheDocument();
+    });
+  });
+
+  it('should not show pagination when there are no invoices', async () => {
+    // Arrange
+    const invoicesCollection = {
+      items: [],
+      totalItems: 0,
+    };
+
+    const dopplerBillingApiClientDouble = {
+      getInvoices: async () => {
+        return { success: true, value: invoicesCollection };
+      },
+    };
+
+    // Act
+    const { container } = render(
+      <AppServicesProvider
+        forcedServices={{
+          dopplerBillingApiClient: dopplerBillingApiClientDouble,
+        }}
+      >
+        <IntlProvider>
+          <BrowserRouter>
+            <InvoicesList />
+          </BrowserRouter>
+        </IntlProvider>
+      </AppServicesProvider>,
+    );
+
+    // Assert
+    await waitFor(() => {
+      expect(container.querySelector('.dp-pagination')).not.toBeInTheDocument();
+    });
+  });
+
+  it('should not show pagination when the service throws an error', async () => {
+    // Arrange
+    const invoicesCollection = {
+      items: [],
+      totalItems: 0,
+    };
+
+    const dopplerBillingApiClientDouble = {
+      getInvoices: async () => {
+        return { success: false, value: invoicesCollection };
+      },
+    };
+
+    // Act
+    const { container } = render(
+      <AppServicesProvider
+        forcedServices={{
+          dopplerBillingApiClient: dopplerBillingApiClientDouble,
+        }}
+      >
+        <IntlProvider>
+          <BrowserRouter>
+            <InvoicesList />
+          </BrowserRouter>
+        </IntlProvider>
+      </AppServicesProvider>,
+    );
+
+    // Assert
+    await waitFor(() => {
+      expect(container.querySelector('.dp-pagination')).not.toBeInTheDocument();
+    });
   });
 });
