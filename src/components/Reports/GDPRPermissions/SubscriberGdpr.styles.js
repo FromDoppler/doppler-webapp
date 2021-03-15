7b98973b3518f67df8220d6af7984aec88d2c2aa
Yoe <bravoy93@gmail.com>
2021-03-15 15:16:31 -0400

    feat(SubscriberGDPR): Permissions expandable row

--- src/components/Reports/GDPRPermissions/SubscriberGdpr.styles.js
+++ src/components/Reports/GDPRPermissions/SubscriberGdpr.styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { fonts } from '../../styles/fonts';
 
-export const TextColumn = styled.div`
+export const TextColumn = styled.span`
   p {
     font-size: ${fonts.fontLvl3};
   }
