a13b4b796d1f33f9a00c655b7eb9e10b7de5e663
Ramniel Labaut <rlabaut@kreitech.io>
2021-06-23 10:17:06 -0400

    test: add contact policy api client dummy test

---
+++ src/services/doppler-contact-policy-api-client.test.ts
@@ -0,0 +1,15 @@
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('HttpDopplerContactPolicyApiClient', () => {
+  afterEach(cleanup);
+
+  //TODO: Implement real tests
+  it('dummy test', () => {
+    // Act
+    const n = null;
+
+    // Assert
+    expect(n).toBeNull();
+  });
+});
