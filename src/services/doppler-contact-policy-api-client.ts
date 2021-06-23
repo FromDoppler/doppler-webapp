683666e9a80ee6af5962f63dbc518845cc8eb643
Ramniel Labaut <rlabaut@kreitech.io>
2021-06-23 10:17:20 -0400

    feat: add contact policy api client

---
+++ src/services/doppler-contact-policy-api-client.ts
@@ -0,0 +1,78 @@
+import { ResultWithoutExpectedErrors } from '../doppler-types';
+import { AxiosInstance, AxiosStatic } from 'axios';
+import { RefObject } from 'react';
+import { AppSession } from './app-session';
+
+export interface DopplerContactPolicyApiClient {
+  getAccountSettings(email: string): Promise<ResultWithoutExpectedErrors<AccountSettings>>;
+}
+
+export interface AccountSettings {
+  accountName: string;
+  active: boolean;
+  emailsAmountByInterval: number;
+  intervalInDays: number;
+  excludedSubscribersLists: SubscriberList[];
+}
+
+export interface SubscriberList {
+  id: number;
+  name: string;
+}
+
+export class HttpDopplerContactPolicyApiClient implements DopplerContactPolicyApiClient {
+  private readonly axios: AxiosInstance;
+  private readonly baseUrl: string;
+  private readonly connectionDataRef: RefObject<AppSession>;
+
+  constructor({
+    axiosStatic,
+    baseUrl,
+    connectionDataRef,
+  }: {
+    axiosStatic: AxiosStatic;
+    baseUrl: string;
+    connectionDataRef: RefObject<AppSession>;
+  }) {
+    this.baseUrl = baseUrl;
+    this.axios = axiosStatic.create({
+      baseURL: this.baseUrl,
+    });
+    this.connectionDataRef = connectionDataRef;
+  }
+
+  private mapSubscriberList(data: any): SubscriberList[] {
+    return data.map((x: any) => ({
+      id: x.id,
+      name: x.name,
+    }));
+  }
+
+  async getAccountSettings(email: string): Promise<ResultWithoutExpectedErrors<AccountSettings>> {
+    try {
+      const response = await this.axios.request({
+        method: 'GET',
+        url: `/accounts/${email}/settings`,
+      });
+
+      if (response.status === 200 && response.data) {
+        const settings = {
+          accountName: response.data.accountName,
+          active: response.data.active,
+          emailsAmountByInterval: response.data.emailsAmountByInterval,
+          intervalInDays: response.data.intervalInDays,
+          excludedSubscribersLists: this.mapSubscriberList(response.data.excludedSubscribersLists),
+        };
+
+        return {
+          success: true,
+          value: settings,
+        };
+      } else {
+        return { success: false, error: response };
+      }
+    } catch (error) {
+      return { success: false, error: error };
+    }
+  }
+}
