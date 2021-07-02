b4a80a67b7407b4c063faf8e121e24c4be02b7e2
Ramniel Labaut <rlabaut@kreitech.io>
2021-07-02 10:40:37 -0400

    test: add tests to verify experimental feature status

--- src/components/ContactPolicy/ContactPolicy.test.js
+++ src/components/ContactPolicy/ContactPolicy.test.js
@@ -4,15 +4,31 @@ import { act } from 'react-dom/test-utils';
 import '@testing-library/jest-dom/extend-expect';
 import DopplerIntlProvider from '../../i18n/DopplerIntlProvider.double-with-ids-as-values';
 import { ContactPolicy } from './ContactPolicy';
+import { AppServicesProvider } from '../../services/pure-di';
 
 describe('ContactPolicy component', () => {
   afterEach(cleanup);
 
-  const ContactPolicyComponent = () => (
-    <DopplerIntlProvider>
-      <ContactPolicy />
-    </DopplerIntlProvider>
-  );
+  const experimentalFeatureDouble = () => {
+    return {
+      getFeature: (feature) => feature === 'ContactPolicy',
+    };
+  };
+
+  const dependencies = {
+    experimentalFeatures: experimentalFeatureDouble(),
+  };
+
+  const ContactPolicyComponent = ({ isEnabled = true }) => {
+    const services = isEnabled ? dependencies : {};
+    return (
+      <AppServicesProvider forcedServices={services}>
+        <DopplerIntlProvider>
+          <ContactPolicy />
+        </DopplerIntlProvider>
+      </AppServicesProvider>
+    );
+  };
 
   it('should disable the inputs if the switch is not active', async () => {
     // Act
@@ -45,5 +61,22 @@ describe('ContactPolicy component', () => {
       expect(inputAmount).not.toBeDisabled();
       expect(inputInterval).not.toBeDisabled();
     });
+  });
+
+  it('should show the contact policy configuration if the feature is enabled', () => {
+    //Act
+    const { container } = render(<ContactPolicyComponent />);
+    const form = container.querySelector('form');
+
+    //Assert
+    expect(form).toBeInTheDocument();
+  });
+
+  it("shouldn't show the contact policy configuration if the feature is disabled", () => {
+    //Act
+    const { getByText } = render(<ContactPolicyComponent isEnabled={false} />);
+
+    //Assert
+    expect(getByText('common.feature_no_available'));
   });
 });
