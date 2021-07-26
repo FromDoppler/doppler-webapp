7ba56800e30807973563e1eaa8be5ac858d2ff94
cbernat <cbernat@makingsense.com>
2021-07-26 16:30:59 -0300

    fix(chat): replace zoho chat with zendesk chat

--- public/zoho-chat.js
+++ public/zoho-chat.js
@@ -1,64 +1,42 @@
-var langRes = {
-  es: {
-    online: {
-      legend: '¡Hola! Estamos aquí para ayudarte',
-    },
-    offline: {
-      legend: '¡Hola! Estamos offline. Déjanos un mensaje:)',
-    },
-  },
-  en: {
-    online: {
-      legend: 'Hi! We’re here if you need some help :)',
-    },
-    offline: {
-      legend: 'Hi! We’re offline now. We’ll be back ASAP :)',
-    },
-  },
-};
-
-var $zoho = $zoho || {};
-$zoho.salesiq = $zoho.salesiq || {
-  widgetcode: '44b76224430b91326cb02039d609a5e008e7fe0266102a0ce5060b5c1ff1e0ee',
-  values: {},
-  ready: function() {
-    var lang = 'es'; /*change this in the future*/
-    var chatResources = lang === 'en' ? langRes.en : langRes.es;
-    $zoho.salesiq.language('es'); //we need this hardcoded, only the bubble changes languages
-    $zoho.salesiq.chatbutton.texts([
-      [chatResources.online.legend, 'Online'],
-      [chatResources.offline.legend, 'Offline'],
-    ]);
-    $zoho.salesiq.customfield.add({
-      name: '_default.name',
-      hint: '¿Cómo te llamas?',
-      type: 'text',
-      required: 'true',
-      visibility: 'both',
-    });
-    $zoho.salesiq.customfield.add({
-      name: '_default.email',
-      hint: '¿Cuál es tu Email?',
-      type: 'email',
-      required: 'true',
-      visibility: 'both',
-    });
-    $zoho.salesiq.floatwindow.onlinetitle('¿En qué podemos ayudarte?');
-    $zoho.salesiq.floatwindow.offlinetitle('¿En qué podemos ayudarte?');
-    $zoho.salesiq.domain('fromdoppler.com');
-    $zoho.salesiq.chatbutton.width('205');
-  },
-};
-
-var zohoScript = document.createElement('script');
-zohoScript.type = 'text/javascript';
-zohoScript.id = 'zsiqscript';
-zohoScript.defer = true;
-zohoScript.src = 'https://salesiq.zoho.com/widget';
-
-var firstScript = document.getElementsByTagName('script')[0];
-firstScript.parentNode.insertBefore(zohoScript, firstScript);
-document.write("<div id='zsiqwidget'></div>");
+zE('webWidget', 'setLocale', 'es');
+	window.zESettings = {
+		webWidget: {
+			offset: {
+				horizontal: '25px',
+				vertical: '55px',
+				mobile: {
+					horizontal: '-7px',
+					vertical: '56px'
+				}
+			},
+			launcher: {
+				chatLabel: {
+					'*': "Chat"
+				}
+			},
+			chat: {
+				title: {
+					'*': 'Customer Support Chat',
+					'en-US': 'Customer Support Chat',
+					es: 'Atención al Cliente'
+				},
+				offlineForm: {
+					greeting: {
+						'*': "How can we help you? Leave us a message and we’ll contact you soon.",
+						'en-US': "How can we help you? Leave us a message and we’ll contact you soon.",
+						es: "¿En qué podemos ayudarte? Déjanos un mensaje y nos contactaremos pronto."
+					}
+				},
+				prechatForm: {
+					greeting: {
+						'*': 'Hi! We’re here to help you. Tell us about your questions or request.',
+						'en-US': 'Hi! We’re here to help you. Tell us about your questions or request.',
+						es: '¡Hola! Estamos aquí para ayudarte. Cuéntanos sobre tu consulta o solicitud.'
+					}
+				}
+			},
+		}
+	};
 
 // hide bubble after certain time
 window.onload = function() {
