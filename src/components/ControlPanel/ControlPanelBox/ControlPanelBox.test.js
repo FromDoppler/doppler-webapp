c8dfba302003a50226d2f87df70f0cf5e8012357
fgonzalez <fgonzalez@makingsense.com>
2021-10-15 17:45:49 -0300

    feat(ControlPanel): Add images

--- src/components/ControlPanel/ControlPanelBox/ControlPanelBox.test.js
+++ src/components/ControlPanel/ControlPanelBox/ControlPanelBox.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { ControlPanelBox } from './ControlPanelBox';
-import ControlPanelIconImg from '../account_icon.png';
+import ControlPanelIconImg from '../images/account_information_icon.png';
 import IntlProvider from '../../../i18n/DopplerIntlProvider.double-with-ids-as-values';
 
 it('should render a control panel box', async () => {
