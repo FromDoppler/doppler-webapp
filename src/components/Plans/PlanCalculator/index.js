eb4141955f54d2cafcea81c88ea03bcf3759c8e4
Junior Campos <jcampos@makingsense.com>
2021-10-26 09:40:39 -0500

    feat: add PlanCalculator component

---
+++ src/components/Plans/PlanCalculator/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const PlanCalculator = () => {
+  return (
+    <section className="dp-gray-page p-t-54 p-b-54">
+      <section className="dp-container">
+        <div className="dp-rowflex">
+          <div className="col-sm-12">
+            <h1 className="dp-tit-plans">Calculadora de planes</h1>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
