ee87f173c329aab20fa5b50954f91f2fefe1c493
Junior Campos <jcampos@makingsense.com>
2021-10-28 10:26:38 -0500

    feat: apply plan service to get plan types

--- src/components/Plans/PlanCalculator/index.js
+++ src/components/Plans/PlanCalculator/index.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
+import { InjectAppServices } from '../../../services/pure-di';
+import { Loading } from '../../Loading/Loading';
+import {
+  INITIAL_STATE_PLAN_TYPES,
+  planTypesReducer,
+  PLAN_TYPES_ACTIONS,
+} from './reducers/planTypesReducer';
 
-export const PlanCalculator = () => {
+export const PlanCalculator = InjectAppServices(({ dependencies: { planService } }) => {
+  const [{ planTypes, loading }, dispatch] = useReducer(planTypesReducer, INITIAL_STATE_PLAN_TYPES);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: PLAN_TYPES_ACTIONS.FETCHING_STARTED });
+      await planService.getPlanList();
+      const _planTypes = planService.getPlanTypes();
+      dispatch({ type: PLAN_TYPES_ACTIONS.RECEIVE_PLAN_TYPES, payload: _planTypes });
+    };
+    fetchData();
+  }, [planService]);
+
+  if (loading) {
+    return <Loading page />;
+  }
+
   return (
     <section className="dp-gray-page p-t-54 p-b-54">
       <section className="dp-container">
@@ -12,4 +35,4 @@ export const PlanCalculator = () => {
       </section>
     </section>
   );
-};
+});
--- src/components/Plans/PlanCalculator/index.test.js
+++ src/components/Plans/PlanCalculator/index.test.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { PlanCalculator } from '.';
+import { AppServicesProvider } from '../../../services/pure-di';
+import IntlProvider from '../../../i18n/DopplerIntlProvider.double-with-ids-as-values';
+import { allPlans } from '../../../services/doppler-legacy-client.doubles';
+import { PLAN_TYPE } from '../../../doppler-types';
 
 describe('PlanCalculator component', () => {
-  it('should render PlanCalculator', () => {
-    render(<PlanCalculator />);
+  it('should render PlanCalculator', async () => {
+    // Arrange
+    const forcedServices = {
+      planService: {
+        getPlanList: async () => allPlans,
+        getPlanTypes: () => [PLAN_TYPE.byContact, PLAN_TYPE.byEmail, PLAN_TYPE.byCredit],
+      },
+    };
+
+    // Act
+    render(
+      <AppServicesProvider forcedServices={forcedServices}>
+        <IntlProvider>
+          <PlanCalculator />
+        </IntlProvider>
+      </AppServicesProvider>,
+    );
+
+    // Assert
+    const loader = screen.getByTestId('wrapper-loading');
+    await waitForElementToBeRemoved(loader);
   });
 });
