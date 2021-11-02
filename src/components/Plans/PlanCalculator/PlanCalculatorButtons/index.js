1e37f034d2e8566d7fc3e0b1aa034beb887b8e75
Junior Campos <jcampos@makingsense.com>
2021-11-02 11:54:52 -0500

    feat: create buttons to PlanCalculator component

---
+++ src/components/Plans/PlanCalculator/PlanCalculatorButtons/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useIntl } from 'react-intl';
+import { useQuery } from '../../../../hooks/useQuery';
+import { InjectAppServices } from '../../../../services/pure-di';
+import { TooltipContainer } from '../../../TooltipContainer/TooltipContainer';
+import * as S from './index.styles';
+
+export const PlanCalculatorButtons = InjectAppServices(
+  ({ selectedPlanId, selectedDiscountId, dependencies: { appSessionRef } }) => {
+    const intl = useIntl();
+    const _ = (id, values) => intl.formatMessage({ id: id }, values);
+    const query = useQuery();
+    const promoCode = query.get('promo-code');
+
+    const sessionPlan = appSessionRef.current.userData.user;
+    const isEqualPlan = sessionPlan.plan.idPlan === selectedPlanId;
+
+    return (
+      <div className="dp-container">
+        <div className="dp-rowflex">
+          <div className="dp-align-center dp-cta-plans">
+            <button
+              type="button"
+              className="dp-button button-medium primary-grey"
+              onClick={() => window.history.back()}
+            >
+              {_('plan_calculator.button_back')}
+            </button>
+            <TooltipContainer
+              visible={isEqualPlan}
+              content={_('plan_calculator.button_purchase_tooltip')}
+              orientation="top"
+            >
+              <S.PurchaseLink
+                className={`dp-button button-medium primary-green ${isEqualPlan ? 'disabled' : ''}`}
+                href={getBuyPurchaseUrl({
+                  controlPanelUrl: _('common.control_panel_section_url'),
+                  planId: selectedPlanId,
+                  discountId: selectedDiscountId,
+                  promoCode,
+                })}
+              >
+                {_('plan_calculator.button_purchase')}
+              </S.PurchaseLink>
+            </TooltipContainer>
+          </div>
+        </div>
+      </div>
+    );
+  },
+);
+
+const getBuyPurchaseUrl = ({ controlPanelUrl, planId, discountId, promoCode }) => {
+  return (
+    controlPanelUrl +
+    `/AccountPreferences/UpgradeAccountStep2?IdUserTypePlan=${planId}&fromStep1=True` +
+    `${discountId ? `&IdDiscountPlan=${discountId}` : ''}` +
+    `${promoCode ? `&PromoCode=${promoCode}` : ''}`
+  );
+};
