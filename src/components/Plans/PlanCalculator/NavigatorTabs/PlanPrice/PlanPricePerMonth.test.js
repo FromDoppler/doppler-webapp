cc00e7fc36a6bc21faf0e856219bb910c10b79c0
Sil Sanchez <silsanchez8@gmail.com>
2021-11-04 00:51:10 -0300

    feat(PlanPricePerMonth): Add PlanPricePerMonth component and its tests

---
+++ src/components/Plans/PlanCalculator/NavigatorTabs/PlanPrice/PlanPricePerMonth.test.js
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { PlanPricePerMonth } from './PlanPricePerMonth';
+import { getPlanFee } from '../../../../../utils';
+import { PLAN_TYPE } from '../../../../../doppler-types';
+import { allPlans } from '../../../../../services/doppler-legacy-client.doubles';
+import IntlProvider from '../../../../../i18n/DopplerIntlProvider.double-with-ids-as-values';
+
+describe('PlanPricePerMonth component', () => {
+  test(`Show correct price when plan type is ${PLAN_TYPE.byContact} and subscription is half-yearly`, () => {
+    //Arrange
+    const plansByType = allPlans.filter((plan) => plan.type === PLAN_TYPE.byContact);
+    const myFakePlan = plansByType[0];
+    const myFakePlanDetails = myFakePlan.billingCycleDetails.find(
+      (details) => details.billingCycle === 'half-yearly',
+    );
+    //Act
+    const fee = getPlanFee(myFakePlan);
+    const planFeeWithDiscount = fee * (1 - myFakePlanDetails.discountPercentage / 100);
+
+    render(
+      <IntlProvider>
+        <PlanPricePerMonth
+          selectedPlan={myFakePlan}
+          discountPercentage={myFakePlanDetails.discountPercentage}
+        />
+      </IntlProvider>,
+    );
+    //Assert
+    expect(screen.getByText(planFeeWithDiscount.toString())).toBeInTheDocument();
+  });
+});
