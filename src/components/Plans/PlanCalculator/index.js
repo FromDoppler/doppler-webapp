80c4709fa4b3e58d5d728b69c407b5bcd6359ed8
Junior Campos <jcampos@makingsense.com>
2021-11-03 20:52:08 -0500

    feat: build isVisible in plan calculator

--- src/components/Plans/PlanCalculator/index.js
+++ src/components/Plans/PlanCalculator/index.js
@@ -24,112 +24,119 @@ import { Slider } from './Slider';
 import { UnexpectedError } from './UnexpectedError';
 
 const INITIAL_VALUE_OF_SLIDER = 0;
-export const PlanCalculator = InjectAppServices(({ dependencies: { planService } }) => {
-  const [{ planTypes, loading, hasError }, dispatch] = useReducer(
-    planTypesReducer,
-    INITIAL_STATE_PLAN_TYPES,
-  );
-  const [{ plansByType, sliderValuesRange, hasError: hasErrorPlansByType }, dispatchPlansByType] =
-    useReducer(plansByTypeReducer, INITIAL_STATE_PLANS_BY_TYPE);
-  const [activeClass, setActiveClass] = useState('active');
-  const [selectedPlanIndex, setSelectedPlanIndex] = useState(INITIAL_VALUE_OF_SLIDER);
-  const createTimeout = useTimeout();
-  const intl = useIntl();
-  const _ = (id, values) => intl.formatMessage({ id: id }, values);
-  const { planType: planTypeUrlSegment } = useParams();
-  const selectedPlanType = getPlanTypeFromUrlSegment(planTypeUrlSegment);
+export const PlanCalculator = InjectAppServices(
+  ({ dependencies: { planService, appSessionRef } }) => {
+    const [{ planTypes, loading, hasError }, dispatch] = useReducer(
+      planTypesReducer,
+      INITIAL_STATE_PLAN_TYPES,
+    );
+    const [{ plansByType, sliderValuesRange, hasError: hasErrorPlansByType }, dispatchPlansByType] =
+      useReducer(plansByTypeReducer, INITIAL_STATE_PLANS_BY_TYPE);
+    const [activeClass, setActiveClass] = useState('active');
+    const [selectedPlanIndex, setSelectedPlanIndex] = useState(INITIAL_VALUE_OF_SLIDER);
+    const createTimeout = useTimeout();
+    const intl = useIntl();
+    const _ = (id, values) => intl.formatMessage({ id: id }, values);
+    const { planType: planTypeUrlSegment } = useParams();
+    const selectedPlanType = getPlanTypeFromUrlSegment(planTypeUrlSegment);
+    const sessionPlan = appSessionRef.current.userData.user;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        dispatch({ type: PLAN_TYPES_ACTIONS.FETCHING_STARTED });
-        const _planTypes = await planService.getPlanTypes();
-        dispatch({ type: PLAN_TYPES_ACTIONS.RECEIVE_PLAN_TYPES, payload: _planTypes });
-      } catch (error) {
-        dispatch({ type: PLAN_TYPES_ACTIONS.FETCH_FAILED });
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          dispatch({ type: PLAN_TYPES_ACTIONS.FETCHING_STARTED });
+          const _planTypes = await planService.getPlanTypes();
+          dispatch({ type: PLAN_TYPES_ACTIONS.RECEIVE_PLAN_TYPES, payload: _planTypes });
+        } catch (error) {
+          dispatch({ type: PLAN_TYPES_ACTIONS.FETCH_FAILED });
+        }
+      };
+      fetchData();
+    }, [planService]);
+
+    useEffect(() => {
+      const fetchPlansByType = async () => {
+        try {
+          dispatchPlansByType({ type: PLANS_BY_TYPE_ACTIONS.FETCHING_STARTED });
+          const _plansByType = await planService.getPlansByType(
+            getPlanTypeFromUrlSegment(planTypeUrlSegment),
+          );
+          dispatchPlansByType({
+            type: PLANS_BY_TYPE_ACTIONS.RECEIVE_PLANS_BY_TYPE,
+            payload: _plansByType,
+          });
+        } catch (error) {
+          dispatchPlansByType({ type: PLANS_BY_TYPE_ACTIONS.FETCH_FAILED });
+        }
+      };
+
+      if (planTypes.length > 0) {
+        setSelectedPlanIndex(INITIAL_VALUE_OF_SLIDER);
+        fetchPlansByType();
       }
-    };
-    fetchData();
-  }, [planService]);
+    }, [planService, planTypeUrlSegment, planTypes]);
 
-  useEffect(() => {
-    const fetchPlansByType = async () => {
-      try {
-        dispatchPlansByType({ type: PLANS_BY_TYPE_ACTIONS.FETCHING_STARTED });
-        const _plansByType = await planService.getPlansByType(
-          getPlanTypeFromUrlSegment(planTypeUrlSegment),
-        );
-        dispatchPlansByType({
-          type: PLANS_BY_TYPE_ACTIONS.RECEIVE_PLANS_BY_TYPE,
-          payload: _plansByType,
-        });
-      } catch (error) {
-        dispatchPlansByType({ type: PLANS_BY_TYPE_ACTIONS.FETCH_FAILED });
-      }
+    useEffect(() => {
+      setActiveClass('');
+      createTimeout(() => setActiveClass('active'), 0);
+    }, [planTypeUrlSegment, createTimeout]);
+
+    const handleSliderChange = (e) => {
+      const { value } = e.target;
+      const _selectedPlanIndex = parseInt(value);
+      setSelectedPlanIndex(_selectedPlanIndex);
     };
 
-    if (planTypes.length > 0) {
-      setSelectedPlanIndex(INITIAL_VALUE_OF_SLIDER);
-      fetchPlansByType();
+    if (loading) {
+      return <Loading page />;
     }
-  }, [planService, planTypeUrlSegment, planTypes]);
 
-  useEffect(() => {
-    setActiveClass('');
-    createTimeout(() => setActiveClass('active'), 0);
-  }, [planTypeUrlSegment, createTimeout]);
+    if (hasError || hasErrorPlansByType || selectedPlanType === 'unknown') {
+      return <UnexpectedError />;
+    }
 
-  const handleSliderChange = (e) => {
-    const { value } = e.target;
-    const _selectedPlanIndex = parseInt(value);
-    setSelectedPlanIndex(_selectedPlanIndex);
-  };
+    const isEqualPlan = sessionPlan.plan.idPlan === plansByType[selectedPlanIndex]?.id;
+    const hightestPlan = plansByType.length === 1 && isEqualPlan;
 
-  if (loading) {
-    return <Loading page />;
-  }
-
-  if (hasError || hasErrorPlansByType || selectedPlanType === 'unknown') {
-    return <UnexpectedError />;
-  }
-
-  return (
-    <>
-      <section className="dp-gray-page p-t-54 p-b-54">
-        <section className="dp-container">
-          <div className="dp-rowflex">
-            <div className="col-sm-12 text-align--center">
-              <h1 className="dp-tit-plans">{_(`plan_calculator.plan_premium_title`)}</h1>
-              <p>{_(`plan_calculator.plan_premium_subtitle`)}</p>
-              <div className="dp-align-center dp-tabs-plans col-sm-9">
-                <NavigatorTabs tabs={planTypes} selectedPlanType={selectedPlanType} />
-              </div>
-              <S.PlanTabContainer className="col-sm-12">
-                <article className={`tab--content ${activeClass}`}>
-                  <div className="dp-container">
-                    <div className="dp-rowflex">
-                      <div className="dp-calc-box">
-                        <div className="col-md-6 col-sm-12">
-                          <article className="dp-box-shadow dp-bgplan">
-                            <Slider
-                              planType={selectedPlanType}
-                              values={sliderValuesRange}
-                              selectedPlanIndex={selectedPlanIndex}
-                              handleChange={handleSliderChange}
-                            />
-                          </article>
+    return (
+      <>
+        <section className="dp-gray-page p-t-54 p-b-54">
+          <section className="dp-container">
+            <div className="dp-rowflex">
+              <div className="col-sm-12 text-align--center">
+                <h1 className="dp-tit-plans">{_(`plan_calculator.plan_premium_title`)}</h1>
+                <p>{_(`plan_calculator.plan_premium_subtitle`)}</p>
+                <div className="dp-align-center dp-tabs-plans col-sm-9">
+                  <NavigatorTabs tabs={planTypes} selectedPlanType={selectedPlanType} />
+                </div>
+                <S.PlanTabContainer className="col-sm-12">
+                  <article className={`tab--content ${activeClass}`}>
+                    <div className="dp-container">
+                      <div className="dp-rowflex">
+                        <div className="dp-calc-box">
+                          <div className="col-md-6 col-sm-12">
+                            <article className="dp-box-shadow dp-bgplan">
+                              <Slider
+                                planType={selectedPlanType}
+                                values={sliderValuesRange}
+                                selectedPlanIndex={selectedPlanIndex}
+                                handleChange={handleSliderChange}
+                                isVisible={!hightestPlan}
+                              />
+                            </article>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </article>
-              </S.PlanTabContainer>
-              <PlanCalculatorButtons selectedPlanId={plansByType[selectedPlanIndex]?.id} />
+                  </article>
+                </S.PlanTabContainer>
+                <PlanCalculatorButtons selectedPlanId={plansByType[selectedPlanIndex]?.id} />
+              </div>
             </div>
-          </div>
+          </section>
         </section>
-      </section>
-      <FAQ topics={topics} />
-    </>
-  );
-});
+        <FAQ topics={topics} />
+      </>
+    );
+  },
+);
