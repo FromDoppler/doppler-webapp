cce43af968f2b2bff47922c71b8f4209ade22f08
Junior Campos <jcampos@makingsense.com>
2021-11-08 13:15:54 -0500

    feat: create component of price with discount

---
+++ src/components/Plans/PlanCalculator/PlanPrice/PriceWithDiscount/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useIntl } from 'react-intl';
+import { getPlanFee, thousandSeparatorNumber } from '../../../../../utils';
+import PropTypes from 'prop-types';
+
+export const PriceWithDiscount = ({ selectedPlan, selectedDiscount }) => {
+  const intl = useIntl();
+  const _ = (id, values) => intl.formatMessage({ id: id }, values);
+  const discountPercentage = selectedDiscount?.discountPercentage;
+  const monthsAmmount = selectedDiscount?.monthsAmmount;
+  const planFee = Math.round(getPlanFee(selectedPlan));
+  const planFeeWithDiscount = thousandSeparatorNumber(
+    intl.defaultLocale,
+    planFee * (1 - discountPercentage / 100) * monthsAmmount,
+  );
+
+  return (
+    <>
+      {discountPercentage > 0 ? (
+        <p>
+          {_(
+            'plan_calculator.with_' +
+              selectedDiscount.subscriptionType.replace('-', '_') +
+              '_discount',
+          )}
+          <strong>
+            {' '}
+            US$
+            {planFeeWithDiscount}
+          </strong>
+        </p>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+PriceWithDiscount.propTypes = {
+  selectedPlan: PropTypes.object,
+  selectedDiscount: PropTypes.object,
+};
