3616e29b9d3e29410b05be56d3a1cd93bca6e625
Junior Campos <jcampos@makingsense.com>
2021-11-09 10:55:47 -0500

    feat: add price components to PlanPrice component

--- src/components/Plans/PlanCalculator/PlanPrice/index.js
+++ src/components/Plans/PlanCalculator/PlanPrice/index.js
@@ -3,14 +3,25 @@ import React from 'react';
 import { useIntl } from 'react-intl';
 import { PLAN_TYPE } from '../../../../doppler-types';
 import { PricePerExtraEmail } from './PricePerExtraEmail';
+import { PricePerMonth } from './PricePerMonth';
 import { PriceWithDiscount } from './PriceWithDiscount';
+import { PriceWithoutDiscount } from './PriceWithoutDiscount';
 
 export const PlanPrice = ({ selectedPlan, selectedDiscount }) => {
   const intl = useIntl();
   const _ = (id, values) => intl.formatMessage({ id: id }, values);
 
+  if (!selectedPlan) {
+    return <></>;
+  }
+
   return (
     <div className="dp-price--wrapper">
+      <PriceWithoutDiscount selectedPlan={selectedPlan} selectedDiscount={selectedDiscount} />
+      <PricePerMonth
+        selectedPlan={selectedPlan}
+        discountPercentage={selectedDiscount?.discountPercentage}
+      />
       <div className="dp-agreement">
         <PriceWithDiscount selectedPlan={selectedPlan} selectedDiscount={selectedDiscount} />
         <PricePerExtraEmail selectedPlan={selectedPlan} />
