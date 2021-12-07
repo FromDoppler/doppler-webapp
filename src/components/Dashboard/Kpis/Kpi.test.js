7e064cb2b24f35cde665b7124c169988a41996fe
Junior Campos <jcampos@makingsense.com>
2021-12-06 19:55:44 -0500

    refactor(kpi): refactor kpi component

--- src/components/Dashboard/Kpis/Kpi.test.js
+++ src/components/Dashboard/Kpis/Kpi.test.js
@@ -1,13 +1,49 @@
-import React from 'react';
-import { act, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
 import { Kpi } from './Kpi';
+import IntlProvider from '../../../i18n/DopplerIntlProvider.double-with-ids-as-values';
 
 describe('Kpi component', () => {
   it('should render Kpi Component', async () => {
-    //act
-    render(<Kpi {...Kpi} />);
-    //assert
+    // Arrange
+    const fakeKpi = {
+      kpiTitleId: 'dashboard.campaigns.totalCampaigns',
+      kpiValue: 21.478,
+      iconClass: 'deliveries',
+    };
+
+    // Act
+    render(
+      <IntlProvider>
+        <Kpi {...fakeKpi} />
+      </IntlProvider>,
+    );
+
+    // Assert
     expect(screen.getByRole('figure')).toBeInTheDocument();
+    expect(screen.getByText(fakeKpi.kpiTitleId)).toBeInTheDocument();
+    expect(screen.getByText(fakeKpi.kpiValue)).toBeInTheDocument();
+  });
+
+  it('should render Kpi Component with preiod custom', async () => {
+    // Arrange
+    const fakeKpi = {
+      kpiTitleId: 'dashboard.campaigns.totalOpen',
+      kpiValue: '57%',
+      iconClass: 'open-rate',
+      kpiPeriodId: 'dashboard.total',
+    };
+
+    // Act
+    render(
+      <IntlProvider>
+        <Kpi {...fakeKpi} />
+      </IntlProvider>,
+    );
+
+    // Assert
+    expect(screen.getByRole('figure')).toBeInTheDocument();
+    expect(screen.getByText(fakeKpi.kpiPeriodId)).toBeInTheDocument();
   });
 });
