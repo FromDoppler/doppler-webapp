57918daf5dbc19e07e70fc265b521ca70c09e369
Junior Campos <jcampos@makingsense.com>
2021-12-09 16:06:46 -0500

    feat: add contacts summary service

---
+++ src/services/contactSummary.ts
@@ -0,0 +1,61 @@
+import { ResultWithoutExpectedErrors } from '../doppler-types';
+import { addDays, getStartOfDate } from '../utils';
+import { ContactSummary, ReportClient } from './reports/index';
+
+export interface ContactKpi {
+  id: Number;
+  kpiTitleId: String;
+  kpiValue: Number | String;
+  iconClass: String;
+  kpiPeriodId?: String;
+}
+
+export interface ContactSummaryInterface {
+  getContactsSummary(): Promise<any>;
+}
+
+export class ContactSummaryService implements ContactSummaryInterface {
+  private readonly reportClient: ReportClient;
+
+  constructor({ reportClient }: { reportClient: ReportClient }) {
+    this.reportClient = reportClient;
+  }
+
+  async getContactsSummary(): Promise<ResultWithoutExpectedErrors<ContactKpi[]>> {
+    const dateTo = getStartOfDate(new Date()) ?? new Date();
+    const dateFrom = addDays(dateTo, -30);
+    const response = await this.reportClient.getContactsSummary({
+      dateFrom,
+      dateTo,
+    });
+
+    return response.success
+      ? {
+          ...response,
+          value: mapContactSummary(response.value),
+        }
+      : response;
+  }
+}
+
+export const mapContactSummary = (contactSummary: ContactSummary): ContactKpi[] => [
+  {
+    id: 1,
+    kpiTitleId: 'dashboard.contacts.totalContacts',
+    kpiValue: contactSummary.totalSubscribers,
+    iconClass: 'book',
+    kpiPeriodId: 'dashboard.total',
+  },
+  {
+    id: 2,
+    kpiTitleId: 'dashboard.contacts.totalNewContacts',
+    kpiValue: contactSummary.newSubscribers,
+    iconClass: 'user-new',
+  },
+  {
+    id: 3,
+    kpiTitleId: 'dashboard.contacts.totalRemovedContacts',
+    kpiValue: contactSummary.removedSubscribers,
+    iconClass: 'user-removed',
+  },
+];
