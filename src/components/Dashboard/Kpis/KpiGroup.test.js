23e3609ca2aa1ef18344579fa2e65c19faa167b2
Junior Campos <jcampos@makingsense.com>
2021-12-09 21:28:57 -0500

    feat: add overlay to KpiGroup

--- src/components/Dashboard/Kpis/KpiGroup.test.js
+++ src/components/Dashboard/Kpis/KpiGroup.test.js
@@ -1,23 +1,53 @@
-import React from 'react';
-import { act, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
 import { KpiGroup } from './KpiGroup';
-import { Kpi } from './Kpi';
 
 describe('KpiGroup component', () => {
-  it('should render KpiGroup Component', async () => {
-    //act
-    render(<KpiGroup {...KpiGroup} />);
-    //assert
-    expect(screen.getByTestId('dp-dashboard-panel')).toBeInTheDocument();
+  it('should render KpiGroup Component when is loading', async () => {
+    // Act
+    render(<KpiGroup loading={true} />);
+
+    // Assert
+    expect(screen.getByTestId('loading-box')).toBeInTheDocument();
   });
 
-  it('should show loading when flag is set to loading', async () => {
-    //Arrange
-    const loading = true;
-    //act
-    render(<KpiGroup {...KpiGroup} loading={loading} />);
-    //assert
-    expect(screen.getByTestId('loading-box')).toBeInTheDocument();
+  it('should render KpiGroup Component when is not loading', async () => {
+    // Act
+    render(<KpiGroup loading={false} />);
+
+    // Assert
+    expect(screen.queryByTestId('loading-box')).not.toBeInTheDocument();
+  });
+
+  it('should render KpiGroup when is disabled', async () => {
+    // Arrange
+    const props = {
+      disabled: true,
+      loading: false,
+      overlay: <p>lorem ipsum dolor sit amet...</p>,
+    };
+
+    // Act
+    const { container } = render(<KpiGroup {...props} />);
+
+    // Assert
+    expect(container.querySelector('.dp-overlay.show')).toBeInTheDocument();
+    expect(screen.getByText('lorem ipsum dolor sit amet...')).toBeInTheDocument();
+  });
+
+  it('should render KpiGroup when is not disabled', async () => {
+    // Arrange
+    const props = {
+      disabled: false,
+      loading: false,
+      overlay: <p>lorem ipsum dolor sit amet...</p>,
+    };
+
+    // Act
+    const { container } = render(<KpiGroup {...props} />);
+
+    // Assert
+    expect(container.querySelector('.dp-overlay.show')).not.toBeInTheDocument();
   });
 });
