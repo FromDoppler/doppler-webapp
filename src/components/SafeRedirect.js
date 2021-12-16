f1663754928b7337512bb4a0c5d08fea4c0a9090
Sil Sanchez <silsanchez8@gmail.com>
2021-12-16 10:41:39 -0300

    fix: create environment vars for default redirect

--- src/components/SafeRedirect.js
+++ src/components/SafeRedirect.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { InjectAppServices } from '../services/pure-di';
 import RedirectToExternalUrl from './RedirectToExternalUrl';
 import { isWhitelisted } from './../utils';
-import { getDefaultView } from '../App';
+import { Redirect } from 'react-router-dom';
 
 /**
  * @param { Object } props
@@ -10,6 +10,8 @@ import { getDefaultView } from '../App';
  * @param { import('../services/pure-di').AppServices } props.dependencies
  */
 function SafeRedirect({ to, dependencies }) {
+  const newDashboard = process.env.NEW_DASHBOARD === 'true';
+
   const isPartialUrl = (url) => {
     return /^\/[^/]/.test(url);
   };
@@ -18,10 +20,12 @@ function SafeRedirect({ to, dependencies }) {
     return <RedirectToExternalUrl to={dependencies.appConfiguration.dopplerLegacyUrl + to} />;
   } else if (isWhitelisted(to)) {
     return <RedirectToExternalUrl to={to} />;
+  } else if (newDashboard) {
+    <Redirect to="/" />;
   } else {
     return (
       <RedirectToExternalUrl
-        to={dependencies.appConfiguration.dopplerLegacyUrl + getDefaultView()}
+        to={dependencies.appConfiguration.dopplerLegacyUrl + '/Campaigns/Draft'}
       />
     );
   }
