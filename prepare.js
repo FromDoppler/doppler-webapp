388aa5df656eba649c84002155d46dfe4d2b4869
Junior Campos <jcampos@makingsense.com>
2022-02-04 08:07:14 -0500

    chore: conventional commits

--- prepare.js
+++ prepare.js
@@ -3,4 +3,5 @@ if (!disableHusky) {
   const husky = require('husky');
   husky.install();
   husky.set('.husky/pre-commit', 'yarn lint-staged');
-}
+  husky.set('.husky/commit-msg', 'yarn commitlint --edit $1');
+} 
