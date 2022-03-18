be4c3ebe4431463981600eb28831ae76db914ed5
Junior Campos <jcampos@makingsense.com>
2022-03-18 09:57:45 -0500

    feat: create TypeformSurvey component

---
+++ src/components/TypeformSurvey/index.js
@@ -0,0 +1,55 @@
+import { PopupButton } from '@typeform/embed-react';
+import React, { useEffect, useReducer } from 'react';
+import { InjectAppServices } from '../../services/pure-di';
+import { INITIAL_STATE_SURVEY, surveyReducer, SURVEY_ACTIONS } from './reducers/surveyReducer';
+
+export const TypeformSurvey = InjectAppServices(
+  ({ dependencies: { appSessionRef, surveyClient, experimentalFeatures } }) => {
+    const [{ surveyFormCompleted, loading }, dispatch] = useReducer(
+      surveyReducer,
+      INITIAL_STATE_SURVEY,
+    );
+
+    useEffect(() => {
+      const fetchData = async () => {
+        dispatch({ type: SURVEY_ACTIONS.START_FETCH });
+        const response = await surveyClient.getSurveyFormStatus();
+        console.log('response', response);
+        if (response.success) {
+          dispatch({ type: SURVEY_ACTIONS.FINISH_FETCH, payload: response.value });
+        } else {
+          dispatch({ type: SURVEY_ACTIONS.FAIL_FETCH });
+        }
+      };
+
+      const typeformEnabled = experimentalFeatures.getFeature('typeformEnabled');
+      if (typeformEnabled) {
+        fetchData();
+      }
+    }, [surveyClient, experimentalFeatures]);
+
+    if (loading || surveyFormCompleted) {
+      return <div data-testid="empty-fragment" />;
+    }
+
+    const { REACT_APP_TYPEFORMID_ES, REACT_APP_TYPEFORMID_EN } = process.env;
+    const { lang, email, fullname } = appSessionRef.current.userData.user;
+    const typeformId = lang === 'es' ? REACT_APP_TYPEFORMID_ES : REACT_APP_TYPEFORMID_EN;
+
+    return (
+      <PopupButton
+        id={typeformId}
+        open="load"
+        size={100}
+        hidden={{
+          email: email,
+          lastName: fullname,
+        }}
+        onReady={async () => {
+          console.log('form ready');
+          await surveyClient.setSurveyToCompleted();
+        }}
+      />
+    );
+  },
+);
