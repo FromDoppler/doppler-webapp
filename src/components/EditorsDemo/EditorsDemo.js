024d4b7593866ac4f2074644b7a8a94510e80ae6
mcappato <mcappato@makingsense.com>
2022-04-08 16:29:32 -0300

    chore: add editors demo & menu demo components

---
+++ src/components/EditorsDemo/EditorsDemo.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const EditorsDemo = () => {
+  useEffect(() => {
+    window['doppler-session-mfe-configuration'] = {
+      useDummies: true,
+    };
+    new window.AssetServices().load(process.env.REACT_APP_SESSION_MFE_MANIFIEST_FILE);
+    window['editors-webapp-configuration'] = {
+      basename: 'editors-demo',
+      appElementId: 'doppler-editors-webapp',
+      useDummies: true,
+    };
+    new window.AssetServices().load(process.env.REACT_APP_EDITORS_MFE_MANIFIEST_FILE);
+  }, []);
+  return (
+    <div>
+      <Link to={'/menu-demo'}>ir a Menu Demo</Link>
+      <div id="root-header"></div>
+      <div id="doppler-editors-webapp"></div>
+      <div id="root-footer"></div>
+    </div>
+  );
+};
+
+export default EditorsDemo;
