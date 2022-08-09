7796f64efb3d3c1e85d80fea0f323145e319b099
Andres Moschini <andresmoschini@gmail.com>
2022-08-09 15:36:13 -0300

    chore: create a Session MFE session manager

---
+++ src/services/sessionmfe-session-manager.ts
@@ -0,0 +1,96 @@
+import { AppSession } from './app-session';
+import { MutableRefObject } from 'react';
+import { SessionManager } from './session-manager';
+import { mapHeaderDataJson } from './doppler-legacy-client';
+
+// Doppler Session MFE conventions
+// from https://github.com/FromDoppler/doppler-menu-mfe/blob/main/src/session/doppler-session-mfe-conventions.ts
+export const DOPPLER_SESSION_STATE_UPDATE_EVENT_TYPE = 'doppler-session-state-update';
+
+export type DopplerSessionState =
+  | undefined
+  | { status: 'non-authenticated' }
+  | {
+      status: 'authenticated';
+      jwtToken: string;
+      dopplerAccountName: string;
+      lang: string;
+      rawDopplerUserData: any;
+    };
+
+declare global {
+  interface Window {
+    dopplerSessionState: DopplerSessionState;
+    restartDopplerSessionMonitor: () => void;
+  }
+}
+// End Doppler Session MFE conventions
+
+const noop = () => {};
+
+function mapDopplerSessionState(dopplerSessionState: DopplerSessionState): AppSession {
+  if (!dopplerSessionState) {
+    return { status: 'unknown' };
+  }
+
+  if (dopplerSessionState.status !== 'authenticated') {
+    return { status: dopplerSessionState.status };
+  }
+
+  const userData = mapHeaderDataJson(dopplerSessionState.rawDopplerUserData);
+  return {
+    status: 'authenticated',
+    jwtToken: userData.jwtToken,
+    datahubCustomerId: userData.datahubCustomerId,
+    userData,
+  };
+}
+
+export class SessionMfeSessionManager implements SessionManager {
+  private readonly appSessionRef: MutableRefObject<AppSession>;
+  private readonly window: Window;
+
+  private handler: (s: AppSession) => void = noop;
+
+  constructor({
+    appSessionRef,
+    window,
+  }: {
+    appSessionRef: MutableRefObject<AppSession>;
+    window: Window;
+  }) {
+    this.appSessionRef = appSessionRef;
+    this.window = window;
+    this.update = this.update.bind(this);
+  }
+
+  public initialize(handler: (s: AppSession) => void) {
+    this.handler = handler;
+    this.window.addEventListener(DOPPLER_SESSION_STATE_UPDATE_EVENT_TYPE, this.update);
+    this.update();
+  }
+
+  public finalize() {
+    this.window.removeEventListener(DOPPLER_SESSION_STATE_UPDATE_EVENT_TYPE, this.update);
+  }
+
+  public restart() {
+    window.restartDopplerSessionMonitor();
+  }
+
+  private updateSession(session: AppSession) {
+    this.appSessionRef.current = session;
+    this.handler(session);
+  }
+
+  private async update() {
+    const dopplerUserData = mapDopplerSessionState(this.window.dopplerSessionState);
+
+    if (dopplerUserData.status === 'authenticated') {
+      //This will be used by GTM and HotJar
+      (window as any).mainMenuData = dopplerUserData.userData;
+    }
+
+    this.updateSession(dopplerUserData);
+  }
+}
