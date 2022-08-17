ad186f02a32226b0fcfd04bf57d680773c09d726
jhoffman-ms <jmh248@gmail.com>
2022-08-17 09:19:55 -0300

    feat: add enable upgrade between plans by credits

--- src/components/Plans/PlanCalculator/GoToUpgrade/index.js
+++ src/components/Plans/PlanCalculator/GoToUpgrade/index.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
 import { PlanCalculator } from '..';
-import { PLAN_TYPE } from '../../../../doppler-types';
 import { InjectAppServices } from '../../../../services/pure-di';
-import { Loading } from '../../../Loading/Loading';
-import SafeRedirect from '../../../SafeRedirect';
 
-export const GoToUpgrade = InjectAppServices(({ dependencies: { appSessionRef } }) => {
-  const { isFreeAccount: isTrial, planType } = appSessionRef.current.userData.user.plan;
-  const location = useLocation();
-
-  if (!isTrial && planType === PLAN_TYPE.byCredit) {
-    // Redirect to buy credits with all query parameters
-    return (
-      <>
-        <Loading />
-        <SafeRedirect to={`/ControlPanel/AccountPreferences/BuyCreditsStep1${location.search}`} />
-      </>
-    );
-  }
-
+//TODO: In the future, this component should be replaced to PlanCalculator component.
+export const GoToUpgrade = InjectAppServices(() => {
   return <PlanCalculator />;
 });
