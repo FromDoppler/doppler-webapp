9dd129062c3967c303c29a09dc572e4856f080f4
Junior Campos <jcampos@makingsense.com>
2022-09-02 11:16:00 -0400

    chore: update image generation

--- Jenkinsfile
+++ Jenkinsfile
@@ -16,84 +16,71 @@ pipeline {
                 sh 'docker build --target test -f Dockerfile.swarm .'
             }
         }
-        stage('Build') {
-            steps {
-                sh '''docker build \\
-                    --target build \\
-                    --build-arg environment=production \\
-                    --build-arg react_app_router=browser \\
-                    --build-arg public_url="/" \\
-                    -f Dockerfile.swarm \\
-                    .'''
-            }
-        }
-        stage('Release') {
-            when { 
-                branch 'master'
-            }
-            steps {
-                sh 'sh generate-release-w-docker.sh'
-            }
-        }
-        stage('Build final version images') {
+        stage('Docker PR Build') {
             when {
-                expression {
-                    return isVersionTag(readCurrentTag())
-                }
+                changeRequest target: 'master'
             }
             steps {
-                // TODO: remove build differences based on environments to allow reducing the
-                // number of different images. Maybe `demo` and `production` cold be enought
                 sh '''docker build \
-                    -t "local-doppler-webapp:production-commit-${GIT_COMMIT}" \\
-                    --build-arg environment=production \\
-                    --build-arg react_app_router=browser \\
-                    --build-arg public_url="/" \\
-                    --build-arg version=production-${TAG_NAME}+${GIT_COMMIT} \\
-                    -f Dockerfile.swarm \\
-                    .'''
-                sh '''docker build \
-                    -t "local-doppler-webapp:demo-commit-${GIT_COMMIT}" \\
-                    --build-arg environment=demo \\
-                    --build-arg react_app_router=browser \\
-                    --build-arg public_url="/" \\
-                    --build-arg version=demo-${TAG_NAME}+${GIT_COMMIT} \\
-                    -f Dockerfile.swarm \\
-                    .'''
-                 sh '''docker build \
-                    -t "local-doppler-webapp:int-commit-${GIT_COMMIT}" \\
-                    --build-arg environment=int \\
-                    --build-arg react_app_router=browser \\
-                    --build-arg public_url="/" \\
-                    --build-arg version=int-${TAG_NAME}+${GIT_COMMIT} \\
-                    -f Dockerfile.swarm \\
-                    .'''
-                    sh '''docker build \
-                    -t "local-doppler-webapp:qa-commit-${GIT_COMMIT}" \\
-                    --build-arg environment=qa \\
-                    --build-arg react_app_router=browser \\
-                    --build-arg public_url="/" \\
-                    --build-arg version=qa-${TAG_NAME}+${GIT_COMMIT} \\
-                    -f Dockerfile.swarm \\
+                    -t "local-doppler-webapp:production-commit-${GIT_COMMIT}" \
+                    --build-arg environment=production \
+                    --build-arg version="local-doppler-webapp:production-commit-${GIT_COMMIT}" \
+                    -f Dockerfile.swarm \
                     .'''
             }
         }
-        stage('Publish final version images in dopplerdock') {
+        stage('Publish images to Docker Hub') {
             environment {
                 DOCKER_CREDENTIALS_ID = "dockerhub_dopplerdock"
                 DOCKER_IMAGE_NAME = "dopplerdock/doppler-webapp"
             }
-            when {
-                expression {
-                    return isVersionTag(readCurrentTag())
+            stages {
+                stage('Publish pre-release images from master') {
+                    when {
+                        branch 'master'
+                    }
+                    steps {
+                        withDockerRegistry(credentialsId: "${DOCKER_CREDENTIALS_ID}", url: "") {
+                            sh '''sh build-n-publish.sh \
+                                --image=${DOCKER_IMAGE_NAME} \
+                                --commit=${GIT_COMMIT} \
+                                --name=master \
+                                --environment=qa \
+                                '''
+                        }
+                    }
                 }
-            }
-            steps {
-                withDockerRegistry(credentialsId: "${DOCKER_CREDENTIALS_ID}", url: "") {
-                    sh 'sh publish-commit-image-to-dockerhub.sh production ${DOCKER_IMAGE_NAME} ${GIT_COMMIT} ${TAG_NAME}'
-                    sh 'sh publish-commit-image-to-dockerhub.sh demo ${DOCKER_IMAGE_NAME} ${GIT_COMMIT} ${TAG_NAME}'
-                    sh 'sh publish-commit-image-to-dockerhub.sh int ${DOCKER_IMAGE_NAME} ${GIT_COMMIT} ${TAG_NAME}'
-                    sh 'sh publish-commit-image-to-dockerhub.sh qa ${DOCKER_IMAGE_NAME} ${GIT_COMMIT} ${TAG_NAME}'
+                stage('Publish pre-release images from INT') {
+                    when {
+                        branch 'INT'
+                    }
+                    steps {
+                        withDockerRegistry(credentialsId: "${DOCKER_CREDENTIALS_ID}", url: "") {
+                            sh '''sh build-n-publish.sh \
+                                --image=${DOCKER_IMAGE_NAME} \
+                                --commit=${GIT_COMMIT} \
+                                --name=INT \
+                                --environment=int \
+                                '''
+                        }
+                    }
+                }
+                stage('Publish final version images') {
+                    when {
+                        expression {
+                            return isVersionTag(readCurrentTag())
+                        }
+                    }
+                    steps {
+                        withDockerRegistry(credentialsId: "${DOCKER_CREDENTIALS_ID}", url: "") {
+                            sh '''sh build-n-publish.sh \
+                                --image=${DOCKER_IMAGE_NAME} \
+                                --commit=${GIT_COMMIT} \
+                                --version=${TAG_NAME} \
+                                --environment=production \
+                                '''
+                        }
+                    }
                 }
             }
         }
--- build-n-publish.sh
+++ build-n-publish.sh
@@ -1,58 +1,221 @@
 #!/bin/sh
 
-pkgName="doppler-webapp"
-pkgVersion=${1:-"v0.0.0-build0"}
-cdnBaseUrl=${2:-"//cdn.fromdoppler.com/$pkgName"}
-pkgBuild=${4:-0}
-pkgCommitId=${5:-$(git rev-parse HEAD)}
-environments="int qa development production demo"
+commit=""
+environment=""
+name=""
+version=""
+versionPre=""
+imageName=""
 
-# Exit immediately if a command exits with a non-zero status.
+print_help () {
+    echo ""
+    echo "Usage: sh build-n-publish.sh [OPTIONS]"
+    echo ""
+    echo "Build project's docker images and publish them to DockerHub"
+    echo ""
+    echo "Options:"
+    echo "  -i, --image, image name (mandatory)"
+    echo "  -c, --commit (mandatory)"
+    echo "  -e, --environment, build with this environment configuration (mandatory)"
+    echo "  -n, --name, version name"
+    echo "  -v, --version, version number"
+    echo "  -s, --pre-version-suffix (optional, only with version)"
+    echo "  -h, --help"
+    echo "Only one of name or version parameters is required, and cannot be included together."
+    echo
+    echo "Examples:"
+    echo "  sh build-n-publish.sh --image=dopplerdock/doppler-billing-user --commit=aee25c286a7c8265e2b32ccc293f5ab0bd7a9d57 --version=v1.2.11 --environment=production"
+    echo "  sh build-n-publish.sh --image=dopplerdock/doppler-billing-user --commit=e247ba0527665eb9dd7ffbff00bb42e5073cd457 --version=v0.0.0 --pre-version-suffix=commit-e247ba0527665eb9dd7ffbff00bb42e5073cd457 --environment=production"
+    echo "  sh build-n-publish.sh -i=dopplerdock/doppler-billing-user -c=94f85efb9c3689f409104ef7cde6813652ca59fb -v=v12.34.5 --environment=production"
+    echo "  sh build-n-publish.sh -i=dopplerdock/doppler-billing-user -c=94f85efb9c3689f409104ef7cde6813652ca59fb -v=v12.34.5 -s=beta1 --environment=production"
+    echo "  sh build-n-publish.sh -i=dopplerdock/doppler-billing-user -c=94f85efb9c3689f409104ef7cde6813652ca59fb -v=v12.34.5 -s=pr123 --environment=production"
+}
+
+for i in "$@" ; do
+case $i in
+    -i=*|--image=*)
+    imageName="${i#*=}"
+    ;;
+    -c=*|--commit=*)
+    commit="${i#*=}"
+    ;;
+    -e=*|--environment=*)
+    environment="${i#*=}"
+    ;;
+    -n=*|--name=*)
+    name="${i#*=}"
+    ;;
+    -v=*|--version=*)
+    version="${i#*=}"
+    ;;
+    -s=*|--pre-version-suffix=*)
+    versionPre="${i#*=}"
+    ;;
+    -h|--help)
+    print_help
+    exit 0
+    ;;
+esac
+done
+
+if [ -z "${imageName}" ]
+then
+  echo "Error: image parameter is mandatory"
+  print_help
+  exit 1
+fi
+
+if [ -z "${commit}" ]
+then
+  echo "Error: commit parameter is mandatory"
+  print_help
+  exit 1
+fi
+
+if [ -z "${environment}" ]
+then
+  echo "Error: environment parameter is mandatory"
+  print_help
+  exit 1
+fi
+
+if [ -n "${version}" ] && [ -n "${name}" ]
+then
+  echo "Error: name and version parameters cannot be included together"
+  print_help
+  exit 1
+fi
+
+if [ -z "${version}" ] && [ -z "${name}" ]
+then
+  echo "Error: one of name or version parameters is required"
+  print_help
+  exit 1
+fi
+
+if [ -z "${version}" ] && [ -n "${versionPre}" ]
+then
+  echo "Error: pre-version-suffix parameter is only accepted along with version parameter"
+  print_help
+  exit 1
+fi
+
+# TODO: validate commit format
+# TODO: validate version format (if it is included)
+
+# Stop script on NZEC
 set -e
-
 # Stop script if unbound variable found (use ${var:-} if intentional)
 set -u
 
 # Lines added to get the script running in the script path shell context
 # reference: http://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
-cd $(dirname $0)
+cd "$(dirname "$0")"
 
-# To avoid issues with MINGW y Git Bash, see:
+# To avoid issues with MINGW and Git Bash, see:
 # https://github.com/docker/toolbox/issues/673
 # https://gist.github.com/borekb/cb1536a3685ca6fc0ad9a028e6a959e3
 export MSYS_NO_PATHCONV=1
 export MSYS2_ARG_CONV_EXCL="*"
 
-echo Publishing to Docker
-echo pkgName: $pkgName
-echo cdnBaseUrl: $cdnBaseUrl
-echo pkgVersion: $pkgVersion
-echo pkgBuild: $pkgBuild
-echo pkgCommitId: $pkgCommitId
+if [ -n "${version}" ]
+then
+  versionBuild=${commit}
+  # Ugly code to deal with versions
+  # Input:
+  #   version=v12.34.5
+  #   versionBuild=94f85efb9c
+  #   versionPre=0pr
+  # Output:
+  #   versionMayor=pre-v12
+  #   versionMayorMinor=pre-v12.34
+  #   versionMayorMinorPatch=pre-v12.34.5
+  #   versionMayorMinorPatchPre=pre-v12.34.5-0pr
+  #   versionFull=pre-v12.34.5-0pr+94f85efb9c
+  #   versionFullForTag=pre-v12.34.5-0pr_94f85efb9c
+  # region Ugly code to deal with versions
 
+  versionFull=${version}
 
-for environment in ${environments}; do
-    echo Publishing ${environment}...
+  if [ -n "${versionPre}" ]
+  then
+    versionFull=${versionFull}-${versionPre}
+  fi
 
-    if test "$environment" = "production";
-    then
-        env_version=$pkgVersion;
-    else
-        env_version=$environment-$pkgVersion;
-    fi
+  if [ -n "${versionBuild}" ]
+  then
+    versionFull=${versionFull}+${versionBuild}
+  fi
 
-    echo deploying into $env_version folder ...
+  # https://semver.org/spec/v2.0.0.html#backusnaur-form-grammar-for-valid-semver-versions
+  # <valid semver> ::= <version core>
+  #                  | <version core> "-" <pre-release>
+  #                  | <version core> "+" <build>
+  #                  | <version core> "-" <pre-release> "+" <build>
+  #
+  # <version core> ::= <major> "." <minor> "." <patch>
+  versionBuild="$(echo "${versionFull}"+ | cut -d'+' -f2)"
+  versionMayorMinorPatchPre="$(echo "${versionFull}" | cut -d'+' -f1)" # v0.0.0-xxx (ignoring `+` if exists)
+  versionPre="$(echo "${versionMayorMinorPatchPre}"- | cut -d'-' -f2)"
+  versionMayorMinorPatch="$(echo "${versionMayorMinorPatchPre}" | cut -d'-' -f1)" # v0.0.0 (ignoring `-` if exists)
+  versionMayor="$(echo "${versionMayorMinorPatch}" | cut -d'.' -f1)" # v0
+  versionMinor="$(echo "${versionMayorMinorPatch}" | cut -d'.' -f2)"
+  versionMayorMinor="${versionMayor}.${versionMinor}" # v0.0
+  # by the moment we do not need it, versionPatch only for demo purposes
+  # versionPatch="$(echo "${versionMayorMinorPatch}" | cut -d'.' -f3)"
 
-    docker build --pull \
-        --build-arg environment=$environment \
-        --build-arg env_version=$env_version \
-        --build-arg CDN_SFTP_PORT=$CDN_SFTP_PORT \
-        --build-arg CDN_SFTP_USERNAME=$CDN_SFTP_USERNAME \
-        --build-arg CDN_SFTP_HOSTNAME=$CDN_SFTP_HOSTNAME \
-        --build-arg CDN_SFTP_BASE=$CDN_SFTP_BASE \
-        --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" \
-        --build-arg SSH_KNOWN_HOSTS="$(cat ~/.ssh/known_hosts)" \
-        -f Dockerfile.BUILDS_AND_CDN \
-        .
+  if [ -z "${versionBuild}" ]
+  then
+    canonicalTag=${versionMayorMinorPatchPre}
+  else
+    # because `+` is not accepted in tag names
+    canonicalTag=${versionMayorMinorPatchPre}_${versionBuild}
+  fi
 
-done
+  if [ -n "${versionPre}" ]
+  then
+    preReleasePrefix="pre-"
+    versionMayor=${preReleasePrefix}${versionMayor}
+    versionMayorMinor=${preReleasePrefix}${versionMayorMinor}
+    versionMayorMinorPatch=${preReleasePrefix}${versionMayorMinorPatch}
+    versionMayorMinorPatchPre=${preReleasePrefix}${versionMayorMinorPatchPre}
+    versionFull=${preReleasePrefix}${versionFull}
+    canonicalTag=${preReleasePrefix}${canonicalTag}
+  fi
+  # endregion Ugly code to deal with versions
+fi
+
+if [ -n "${name}" ]
+then
+  versionFull=${name}-${commit}
+  canonicalTag=${versionFull}
+fi
+
+docker build \
+    -t "${imageName}:${environment}-${canonicalTag}" \
+    --build-arg version="${imageName}:${environment}-${canonicalTag}" \
+    --build-arg environment="${environment}" \
+    -f Dockerfile.swarm \
+    .
+
+if [ -n "${version}" ]
+then
+    docker tag "${imageName}:${environment}-${canonicalTag}" "${imageName}:${environment}-${versionMayor}"
+    docker tag "${imageName}:${environment}-${canonicalTag}" "${imageName}:${environment}-${versionMayorMinor}"
+    docker tag "${imageName}:${environment}-${canonicalTag}" "${imageName}:${environment}-${versionMayorMinorPatch}"
+    docker tag "${imageName}:${environment}-${canonicalTag}" "${imageName}:${environment}-${versionMayorMinorPatchPre}"
+
+    docker push "${imageName}:${environment}-${canonicalTag}"
+    docker push "${imageName}:${environment}-${versionMayorMinorPatchPre}"
+    docker push "${imageName}:${environment}-${versionMayorMinorPatch}"
+    docker push "${imageName}:${environment}-${versionMayorMinor}"
+    docker push "${imageName}:${environment}-${versionMayor}"
+fi
+
+if [ -n "${name}" ]
+then
+    docker tag "${imageName}:${environment}-${canonicalTag}" "${imageName}:${environment}-${name}"
+
+    docker push "${imageName}:${environment}-${canonicalTag}"
+    docker push "${imageName}:${environment}-${name}"
+fi
