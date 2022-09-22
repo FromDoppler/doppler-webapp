9d5c267547535352ad60f6c45b37be734cd33b34
Andres Moschini <andresmoschini@gmail.com>
2022-09-22 14:40:56 -0300

    fix: show page only when menu is loaded

--- src/components/MenuDemo/MenuDemo.js
+++ src/components/MenuDemo/MenuDemo.js
@@ -2,10 +2,18 @@ import React, { useEffect } from 'react';
 
 // TODO: rename it to Menu
 const MenuDemo = () => {
+  // Ugly patch: it is required because in some pages we do not have the menu
+  // and we should show them without waiting.
+  document.getElementById('root')?.classList.remove('dp-show-page');
   useEffect(() => {
     window['doppler-menu-mfe-configuration'] = {
       dopplerMenuElementId: 'doppler-menu-mfe',
       useDummies: false,
+      onStatusUpdate: (status) => {
+        if (status === 'authenticated') {
+          document.getElementById('root')?.classList.add('dp-show-page');
+        }
+      },
     };
     window.assetServices &&
       window.assetServices.load({
