4f6bcf00b3281bb36bac1749c584a3e50da47d7b
Junior Campos <jcampos@makingsense.com>
2022-10-25 01:00:11 -0500

    feat: convert Promotions in a component without logic

--- src/components/shared/Promotions/Promotions.js
+++ src/components/shared/Promotions/Promotions.js
@@ -1,64 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { useIntl } from 'react-intl';
-import { InjectAppServices } from '../../../services/pure-di';
+import React from 'react';
 import { Loading } from '../../Loading/Loading';
-
-const getDefaultBannerData = (intl) => {
-  const _ = (id, values) => intl.formatMessage({ id: id }, values);
-
-  return {
-    title: _('default_banner_data.title'),
-    description: _('default_banner_data.description'),
-    backgroundUrl: _('default_banner_data.background_url'),
-    imageUrl: _('default_banner_data.image_url'),
-    functionality: _('default_banner_data.functionality'),
-    fontColor: '#FFF',
-  };
-};
 
 /**
  * Promotions
  * @param { Object } props
- * @param { import('react-intl').InjectedIntl } props.intl
- * @param { import('../../services/pure-di').AppServices } props.dependencies
  */
-const Promotions = function ({ type, page, dependencies: { dopplerSitesClient } }) {
-  const intl = useIntl();
-  const [state, setState] = useState({ loading: true });
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setState({ loading: true });
-      const bannerData = await dopplerSitesClient.getBannerData(intl.locale, type, page || '');
-      if (!bannerData || !bannerData.success) {
-        setState({ loading: false, bannerData: getDefaultBannerData(intl) });
-      } else {
-        setState({ loading: false, bannerData: bannerData.value });
-      }
-    };
-
-    fetchData();
-  }, [dopplerSitesClient, page, intl, type]);
-
+const Promotions = function ({ loading, bannerData }) {
   return (
     <section className="feature-panel" style={{ position: 'relative' }}>
-      {state.loading ? (
+      {loading ? (
         <Loading />
       ) : (
         <div
           className="feature-panel--bg"
           style={{
-            backgroundImage: `url(${state.bannerData.backgroundUrl})`,
-            color: state.bannerData.fontColor,
+            backgroundImage: `url(${bannerData.backgroundUrl})`,
+            color: bannerData.fontColor,
           }}
         >
           <article className="feature-content">
-            <h6>{state.bannerData.functionality}</h6>
-            <h1>{state.bannerData.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: state.bannerData.description }} />
+            <h6>{bannerData.functionality}</h6>
+            <h1>{bannerData.title}</h1>
+            <div dangerouslySetInnerHTML={{ __html: bannerData.description }} />
           </article>
           <figure className="content-img">
-            <img src={state.bannerData.imageUrl} alt="" />
+            <img src={bannerData.imageUrl} alt="" />
           </figure>
         </div>
       )}
@@ -66,4 +32,4 @@ const Promotions = function ({ type, page, dependencies: { dopplerSitesClient } 
   );
 };
 
-export default InjectAppServices(Promotions);
+export default Promotions;
--- src/components/shared/Promotions/Promotions.test.js
+++ src/components/shared/Promotions/Promotions.test.js
@@ -1,82 +1,43 @@
 import React from 'react';
-import { render, cleanup, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Promotions from './Promotions';
-import DopplerIntlProvider from '../../../i18n/DopplerIntlProvider.double-with-ids-as-values';
-import { AppServicesProvider } from '../../../services/pure-di';
-
-const emptyResponse = { success: false, error: new Error('Dummy error') };
-const fullResponse = {
-  success: true,
-  value: {
-    title: 'default_banner_data.title',
-    description: 'default_banner_data.description',
-    backgroundUrl: 'default_banner_data.background_url',
-    imageUrl: 'default_banner_data.image_url',
-    functionality: 'default_banner_data.functionality',
-    color: '#fff',
-  },
-};
 
 describe('Promotions Component', () => {
-  afterEach(cleanup);
-  it('should sites response fail', async () => {
-    const dopplerSitesClientDouble = {
-      getBannerData: async () => emptyResponse,
+  it('should render loading component', async () => {
+    // arrange
+    const props = {
+      loading: true,
+      bannerData: null,
     };
 
-    const { container, getByText } = render(
-      <AppServicesProvider
-        forcedServices={{
-          dopplerSitesClient: dopplerSitesClientDouble,
-        }}
-      >
-        <DopplerIntlProvider locale="es">
-          <Promotions type="signup" page="example" />
-        </DopplerIntlProvider>
-      </AppServicesProvider>,
-    );
-    expect(container.querySelector('.loading-box')).toBeInTheDocument();
-    await waitFor(() => expect(getByText('default_banner_data.title')));
+    // act
+    render(<Promotions {...props} />);
+
+    // assert
+    screen.getByTestId('loading-box');
+    expect(screen.queryByRole('heading', { lavel: 1 })).not.toBeInTheDocument();
   });
 
-  it('should has full data from service', async () => {
-    const dopplerSitesClientDouble = {
-      getBannerData: async () => fullResponse,
+  it('should render Promotion component when has bannerData', async () => {
+    // arrange
+    const props = {
+      loading: false,
+      bannerData: {
+        title: 'default_banner_data.title',
+        description: 'default_banner_data.description',
+        backgroundUrl: 'default_banner_data.background_url',
+        imageUrl: 'default_banner_data.image_url',
+        functionality: 'default_banner_data.functionality',
+        fontColor: '#FFF',
+      },
     };
 
-    const { container, getByText } = render(
-      <AppServicesProvider
-        forcedServices={{
-          dopplerSitesClient: dopplerSitesClientDouble,
-        }}
-      >
-        <DopplerIntlProvider locale="es">
-          <Promotions type="signup" page="example" />
-        </DopplerIntlProvider>
-      </AppServicesProvider>,
-    );
-    expect(container.querySelector('.loading-box')).toBeInTheDocument();
-    await waitFor(() => expect(getByText(fullResponse.value.title)));
-  });
+    // act
+    render(<Promotions {...props} />);
 
-  it('should has full default data from service when dont have page in parameters', async () => {
-    const dopplerSitesClientDouble = {
-      getBannerData: async () => fullResponse,
-    };
-
-    const { container, getByText } = render(
-      <AppServicesProvider
-        forcedServices={{
-          dopplerSitesClient: dopplerSitesClientDouble,
-        }}
-      >
-        <DopplerIntlProvider locale="es">
-          <Promotions type="signup" />
-        </DopplerIntlProvider>
-      </AppServicesProvider>,
-    );
-    expect(container.querySelector('.loading-box')).toBeInTheDocument();
-    await waitFor(() => expect(getByText(fullResponse.value.title)));
+    // assert
+    expect(screen.queryByTestId('loading-box')).not.toBeInTheDocument();
+    screen.getByRole('heading', { level: 1, name: props.bannerData.title });
   });
 });
