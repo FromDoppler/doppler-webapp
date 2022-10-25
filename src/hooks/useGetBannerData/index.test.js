915d37630ef1ac2856cf9aafc1498264cf3637ce
Junior Campos <jcampos@makingsense.com>
2022-10-25 08:20:03 -0500

    test: add test to useGetBannerData hook

--- src/hooks/useGetBannerData/index.test.js
+++ src/hooks/useGetBannerData/index.test.js
@@ -33,4 +33,36 @@ describe('useGetBannerData', () => {
     expect(result.current.loading).toBe(false);
     expect(result.current.bannerData).toBe(bannerData);
   });
+
+  it('should complete getBannerData with banner data by default', async () => {
+    // Arrange
+    const dopplerSitesClientFake = {
+      getBannerData: jest.fn().mockResolvedValue({ success: false, value: null }),
+    };
+    const props = {
+      dopplerSitesClient: dopplerSitesClientFake,
+      intl: {
+        formatMessage: jest.fn(({ id }) => id),
+      },
+      type: 'signup',
+      page: 'dts',
+    };
+
+    // Act
+    const { result, waitForNextUpdate } = renderHook(() => useGetBannerData(props));
+
+    // Assert
+    expect(result.current.loading).toBe(true);
+
+    await waitForNextUpdate();
+    expect(result.current.loading).toBe(false);
+    expect(result.current.bannerData).toEqual({
+      title: 'default_banner_data.title',
+      description: 'default_banner_data.description',
+      backgroundUrl: 'default_banner_data.background_url',
+      imageUrl: 'default_banner_data.image_url',
+      functionality: 'default_banner_data.functionality',
+      fontColor: '#FFF',
+    });
+  });
 });
