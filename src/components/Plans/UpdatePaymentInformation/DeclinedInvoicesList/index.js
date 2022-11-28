0f9443d66e79e36561900de7ea180b5aa296b128
jhoffman-ms <jmh248@gmail.com>
2022-11-28 09:06:12 -0300

    feat: Create the DecliendInvoiceList compoment to use in the summary page

---
+++ src/components/Plans/UpdatePaymentInformation/DeclinedInvoicesList/index.js
@@ -0,0 +1,42 @@
+import { useIntl, FormattedDate, FormattedNumber } from 'react-intl';
+
+const dollarSymbol = 'US$';
+const numberFormatOptions = {
+  style: 'decimal',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+};
+
+export const DeclinedInvoicesList = ({ declinedInvoices }) => {
+  const intl = useIntl();
+  const _ = (id, values) => intl.formatMessage({ id: id }, values);
+
+  return (
+    <>
+      <table className="dp-c-table">
+        <thead>
+          <tr>
+            <th>{_('updatePaymentMethod.reprocess.invoices_declined_table.date_column')}</th>
+            <th>{_('updatePaymentMethod.reprocess.invoices_declined_table.invoice_column')}</th>
+            <th>{_('updatePaymentMethod.reprocess.invoices_declined_table.amount_column')}</th>
+            <th>{_('updatePaymentMethod.reprocess.invoices_declined_table.error_column')}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {declinedInvoices.map((invoice) => (
+            <tr role="row" aria-label="invoice" key={invoice.invoiceNumber}>
+              <td>
+                <FormattedDate value={invoice.date} />
+              </td>
+              <td>{invoice.invoiceNumber}</td>
+              <td>
+                {dollarSymbol} <FormattedNumber value={invoice.amount} {...numberFormatOptions} />
+              </td>
+              <td>{invoice.error}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
