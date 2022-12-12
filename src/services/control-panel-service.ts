5f61fea46bcf14de5d830f4b1e825e3823c53791
fgonzalez <fgonzalez@makingsense.com>
2022-12-12 16:21:38 -0300

    chore: remove old content

--- src/services/control-panel-service.ts
+++ src/services/control-panel-service.ts
@@ -15,44 +15,44 @@ import social_network_icon from '../components/ControlPanel/images/social_networ
 import rss_preferences_icon from '../components/ControlPanel/images/rss_preferences_icon.png';
 import viralization_icon from '../components/ControlPanel/images/viralization_icon.png';
 import auto_publish_icon from '../components/ControlPanel/images/auto_publish_icon.png';
-import bigquery_icon from '../components/ControlPanel/images/bigquery_icon.png';
-import bmw_crm_icon from '../components/ControlPanel/images/bmw_crm_icon.png';
-import easycommerce_icon from '../components/ControlPanel/images/easycommerce_icon.svg';
-import google_analitics_icon from '../components/ControlPanel/images/google_analitics_icon.png';
-import adobe_commerce_icon from '../components/ControlPanel/images/adobe_commerce_icon.png';
-import prestashop_icon from '../components/ControlPanel/images/prestashop_icon.png';
-import shopify_icon from '../components/ControlPanel/images/shopify_icon.png';
-import Tiendanube_icon from '../components/ControlPanel/images/Tiendanube_icon.svg';
-import tokko_icon from '../components/ControlPanel/images/tokko_icon.png';
-import Typeform_icon from '../components/ControlPanel/images/Typeform_icon.png';
-import Unbounce_icon from '../components/ControlPanel/images/Unbounce_icon.svg';
-import vtex_icon from '../components/ControlPanel/images/vtex_icon.svg';
-import woocommerce_icon from '../components/ControlPanel/images/woocommerce_icon.png';
-import zoho_icon from '../components/ControlPanel/images/zoho_icon.png';
+import bigquery_icon from '../components/Integrations/images/bigquery_icon.png';
+import bmw_crm_icon from '../components/Integrations/images/bmw_crm_icon.png';
+import easycommerce_icon from '../components/Integrations/images/easycommerce_icon.svg';
+import google_analitics_icon from '../components/Integrations/images/google_analitics_icon.png';
+import adobe_commerce_icon from '../components/Integrations/images/adobe_commerce_icon.png';
+import prestashop_icon from '../components/Integrations/images/prestashop_icon.png';
+import shopify_icon from '../components/Integrations/images/shopify_icon.png';
+import Tiendanube_icon from '../components/Integrations/images/Tiendanube_icon.svg';
+import tokko_icon from '../components/Integrations/images/tokko_icon.png';
+import Typeform_icon from '../components/Integrations/images/Typeform_icon.png';
+import Unbounce_icon from '../components/Integrations/images/Unbounce_icon.svg';
+import vtex_icon from '../components/Integrations/images/vtex_icon.svg';
+import woocommerce_icon from '../components/Integrations/images/woocommerce_icon.png';
+import zoho_icon from '../components/Integrations/images/zoho_icon.png';
 import custom_domain_icon from '../components/ControlPanel/images/custom_domain_icon.png';
 import domain_key_icon from '../components/ControlPanel/images/domain_key_icon.png';
 import doppler_api_icon from '../components/ControlPanel/images/doppler_api_icon.png';
-import eventbrite_icon from '../components/ControlPanel/images/eventbrite_icon.png';
-import facebook_lead_ads_icon from '../components/ControlPanel/images/facebook_lead_ads_icon.png';
-import go_to_webinar_icon from '../components/ControlPanel/images/go_to_webinar_icon.png';
-import hubspot_icon from '../components/ControlPanel/images/hubspot_icon.png';
-import optin_monster_icon from '../components/ControlPanel/images/optin_monster_icon.png';
-import salesforce_icon from '../components/ControlPanel/images/salesforce_icon.png';
-import zapier_icon from '../components/ControlPanel/images/zapier_icon.png';
-import facebook_icon from '../components/ControlPanel/images/facebook_icon.png';
-import infor_icon from '../components/ControlPanel/images/infor_icon.png';
-import lander__icon from '../components/ControlPanel/images/lander__icon.png';
-import learnpress_icon from '../components/ControlPanel/images/learnpress_icon.png';
-import payu_icon from '../components/ControlPanel/images/payu_icon.png';
-import viewed_icon from '../components/ControlPanel/images/viewed_icon.png';
-import wix_icon from '../components/ControlPanel/images/wix_icon.png';
-import wizell_icon from '../components/ControlPanel/images/wizell_icon.png';
-import xintel_icon from '../components/ControlPanel/images/xintel_icon.png';
-import mercadolibre_icon from '../components/ControlPanel/images/mercadolibre_icon.png';
-import mercado_shops_icon from '../components/ControlPanel/images/mercadoshops_icon.png';
-import dynamics_icon from '../components/ControlPanel/images/dynamics_icon.png';
-import mitienda_icon from '../components/ControlPanel/images/mitienda-icon.png';
-import tiendup_icon from '../components/ControlPanel/images/tiendup_icon.png';
+import eventbrite_icon from '../components/Integrations/images/eventbrite_icon.png';
+import facebook_lead_ads_icon from '../components/Integrations/images/facebook_lead_ads_icon.png';
+import go_to_webinar_icon from '../components/Integrations/images/go_to_webinar_icon.png';
+import hubspot_icon from '../components/Integrations/images/hubspot_icon.png';
+import optin_monster_icon from '../components/Integrations/images/optin_monster_icon.png';
+import salesforce_icon from '../components/Integrations/images/salesforce_icon.png';
+import zapier_icon from '../components/Integrations/images/zapier_icon.png';
+import facebook_icon from '../components/Integrations/images/facebook_icon.png';
+import infor_icon from '../components/Integrations/images/infor_icon.png';
+import lander__icon from '../components/Integrations/images/lander__icon.png';
+import learnpress_icon from '../components/Integrations/images/learnpress_icon.png';
+import payu_icon from '../components/Integrations/images/payu_icon.png';
+import viewed_icon from '../components/Integrations/images/viewed_icon.png';
+import wix_icon from '../components/Integrations/images/wix_icon.png';
+import wizell_icon from '../components/Integrations/images/wizell_icon.png';
+import xintel_icon from '../components/Integrations/images/xintel_icon.png';
+import mercadolibre_icon from '../components/Integrations/images/mercadolibre_icon.png';
+import mercado_shops_icon from '../components/Integrations/images/mercadoshops_icon.png';
+import dynamics_icon from '../components/Integrations/images/dynamics_icon.png';
+import mitienda_icon from '../components/Integrations/images/mitienda-icon.png';
+import tiendup_icon from '../components/Integrations/images/tiendup_icon.png';
 
 import { AppSession } from './app-session';
 import { RefObject } from 'react';
@@ -177,7 +177,7 @@ export class ControlPanelService implements ControlPanelService {
             {
               name: 'Magento',
               linkUrl: `${urlIntegrations}/MagentoSection`,
-              imgSrc: magento_icon,
+              imgSrc: adobe_commerce_icon,
               imgAlt: _('integrations.native_integrations.magento_title'),
               iconName: _('integrations.native_integrations.magento_title'),
             },
@@ -541,278 +541,6 @@ export class ControlPanelService implements ControlPanelService {
             imgSrc: custom_domain_icon,
             imgAlt: _('control_panel.advanced_preferences.custom_domain'),
             iconName: _('control_panel.advanced_preferences.custom_domain'),
-          },
-        ],
-      },
-      {
-        title: _('control_panel.native_integrations.title'),
-        showStatus: true,
-        anchorLink: 'native-integrations',
-        boxes: [
-          {
-            name: 'GoogleAnaliytic',
-            linkUrl: `${urlAdvancedPreferences}/GetGoogleAnaliyticPreferences`,
-            imgSrc: google_analitics_icon,
-            imgAlt: _('control_panel.native_integrations.google_Analityc_title'),
-            iconName: _('control_panel.native_integrations.google_Analityc_title'),
-          },
-          {
-            name: 'Zoho',
-            linkUrl: `${urlIntegrations}/ZohoSection`,
-            imgSrc: zoho_icon,
-            imgAlt: _('control_panel.native_integrations.zoho_title'),
-            iconName: _('control_panel.native_integrations.zoho_title'),
-          },
-          {
-            name: 'TokkoBroker',
-            linkUrl: `${urlIntegrations}/TokkoSection`,
-            imgSrc: tokko_icon,
-            imgAlt: _('control_panel.native_integrations.tokko_title'),
-            iconName: _('control_panel.native_integrations.tokko_title'),
-          },
-          {
-            name: 'Tiendanube',
-            linkUrl: `${urlIntegrations}/TiendaNubeSection`,
-            imgSrc: Tiendanube_icon,
-            imgAlt: _('control_panel.native_integrations.tiendanube_title'),
-            iconName: _('control_panel.native_integrations.tiendanube_title'),
-          },
-          {
-            name: 'Vtex',
-            linkUrl: `${urlIntegrations}/VtexSection`,
-            imgSrc: vtex_icon,
-            imgAlt: _('control_panel.native_integrations.vtex_title'),
-            iconName: _('control_panel.native_integrations.vtex_title'),
-          },
-          {
-            name: 'Prestashop',
-            linkUrl: `${urlIntegrations}/PrestashopSection`,
-            imgSrc: prestashop_icon,
-            imgAlt: _('control_panel.native_integrations.prestashop_title'),
-            iconName: _('control_panel.native_integrations.prestashop_title'),
-          },
-          {
-            name: 'Shopify',
-            linkUrl: '/integrations/shopify',
-            imgSrc: shopify_icon,
-            imgAlt: _('control_panel.native_integrations.shopify_title'),
-            iconName: _('control_panel.native_integrations.shopify_title'),
-          },
-          {
-            linkUrl: `${urlIntegrations}/WebHookIntegration?integrationType=Unbounce`,
-            imgSrc: Unbounce_icon,
-            imgAlt: _('control_panel.native_integrations.unbounce_title'),
-            iconName: _('control_panel.native_integrations.unbounce_title'),
-          },
-          {
-            name: 'Magento',
-            linkUrl: `${urlIntegrations}/MagentoSection`,
-            imgSrc: adobe_commerce_icon,
-            imgAlt: _('control_panel.native_integrations.magento_title'),
-            iconName: _('control_panel.native_integrations.magento_title'),
-          },
-          {
-            linkUrl: `${urlIntegrations}/WebHookIntegration?integrationType=Typeform`,
-            imgSrc: Typeform_icon,
-            imgAlt: _('control_panel.native_integrations.typeform_title'),
-            iconName: _('control_panel.native_integrations.typeform_title'),
-          },
-          {
-            name: 'Woocommerce',
-            linkUrl: `${urlIntegrations}/WooCommerceSection`,
-            imgSrc: woocommerce_icon,
-            imgAlt: _('control_panel.native_integrations.woocommerce_title'),
-            iconName: _('control_panel.native_integrations.woocommerce_title'),
-          },
-          {
-            name: 'Easycommerce',
-            linkUrl: `${urlIntegrations}/EasycommerceSection`,
-            imgSrc: easycommerce_icon,
-            imgAlt: _('control_panel.native_integrations.easycommerce_title'),
-            iconName: _('control_panel.native_integrations.easycommerce_title'),
-          },
-          {
-            name: 'BmwRspCrm',
-            linkUrl: `${urlIntegrations}/BmwCrmSection`,
-            imgSrc: bmw_crm_icon,
-            imgAlt: _('control_panel.native_integrations.bmw_rsp_crm_title'),
-            iconName: _('control_panel.native_integrations.bmw_rsp_crm_title'),
-          },
-          {
-            name: 'BigQuery',
-            linkUrl: '/integrations/big-query',
-            imgSrc: bigquery_icon,
-            imgAlt: _('control_panel.native_integrations.big_query_title'),
-            iconName: _('control_panel.native_integrations.big_query_title'),
-          },
-          {
-            name: 'MercadoLibre',
-            linkUrl: `${urlIntegrations}/MercadoLibreSection`,
-            imgSrc: mercadolibre_icon,
-            imgAlt: _('control_panel.native_integrations.mercadolibre_title'),
-            iconName: _('control_panel.native_integrations.mercadolibre_title'),
-            ribbonColor: 'violet',
-            ribbonText: _('promotional_ribbons.new'),
-          },
-          {
-            name: 'MercadoShops',
-            linkUrl: `${urlIntegrations}/MercadoShopsSection`,
-            imgSrc: mercado_shops_icon,
-            imgAlt: _('control_panel.native_integrations.mercadoshops_title'),
-            iconName: _('control_panel.native_integrations.mercadoshops_title'),
-            ribbonColor: 'violet',
-            ribbonText: _('promotional_ribbons.new'),
-          },
-          {
-            name: 'MiTienda',
-            linkUrl: `${urlIntegrations}/MiTiendaSection`,
-            imgSrc: mitienda_icon,
-            imgAlt: _('control_panel.native_integrations.mitienda_title'),
-            iconName: _('control_panel.native_integrations.mitienda_title'),
-            ribbonColor: 'violet',
-            ribbonText: _('promotional_ribbons.new'),
-          },
-          {
-            linkUrl: `${urlSitesHelp}/es/como-publicar-un-formulario-en-una-facebook-tab`,
-            imgSrc: facebook_icon,
-            imgAlt: _('control_panel.external_integrations.facebook_title'),
-            iconName: _('control_panel.external_integrations.facebook_title'),
-            targetBlank: true,
-          },
-        ],
-      },
-      {
-        title: _('control_panel.zapier_integrations.title'),
-        anchorLink: 'zapier-integrations',
-        boxes: [
-          {
-            linkUrl: `${urlSitesHelp}/es/integracion-con-zapier-glosario`,
-            imgSrc: zapier_icon,
-            imgAlt: _('control_panel.zapier_integrations.zapier'),
-            iconName: _('control_panel.zapier_integrations.zapier'),
-            targetBlank: true,
-          },
-          {
-            linkUrl: `${urlSitesHelp}/es/como-integrar-doppler-con-facebookleadads-utilizando-zapier`,
-            imgSrc: facebook_lead_ads_icon,
-            imgAlt: _('control_panel.zapier_integrations.lead_ads'),
-            iconName: _('control_panel.zapier_integrations.lead_ads'),
-            targetBlank: true,
-          },
-          {
-            linkUrl: `${urlSitesHelp}/es/como-integrar-doppler-con-optinmonster-utilizando-zapier`,
-            imgSrc: optin_monster_icon,
-            imgAlt: _('control_panel.zapier_integrations.optin_monster'),
-            iconName: _('control_panel.zapier_integrations.optin_monster'),
-            targetBlank: true,
-          },
-          {
-            linkUrl: `${urlSitesHelp}/es/como-integrar-doppler-con-hubspot-a-traves-de-zapier`,
-            imgSrc: hubspot_icon,
-            imgAlt: _('control_panel.zapier_integrations.hubspot'),
-            iconName: _('control_panel.zapier_integrations.hubspot'),
-            targetBlank: true,
-          },
-          {
-            linkUrl: `${urlSitesHelp}/es/como-integrar-doppler-con-salesforce-a-traves-de-zapier`,
-            imgSrc: salesforce_icon,
-            imgAlt: _('control_panel.zapier_integrations.salesforce'),
-            iconName: _('control_panel.zapier_integrations.salesforce'),
-            targetBlank: true,
-          },
-          {
-            linkUrl: `${urlSitesHelp}/es/como-integrar-doppler-con-eventbrite-a-traves-de-zapier`,
-            imgSrc: eventbrite_icon,
-            imgAlt: _('control_panel.zapier_integrations.eventbrite'),
-            iconName: _('control_panel.zapier_integrations.eventbrite'),
-            targetBlank: true,
-          },
-          {
-            linkUrl: `${urlSitesHelp}/es/integrar-doppler-con-gotowebinar-via-zapier`,
-            imgSrc: go_to_webinar_icon,
-            imgAlt: _('control_panel.zapier_integrations.go_to_webinar'),
-            iconName: _('control_panel.zapier_integrations.go_to_webinar'),
-            targetBlank: true,
-          },
-        ],
-      },
-      {
-        title: _('control_panel.external_integrations.title'),
-        anchorLink: 'external-integrations',
-        boxes: [
-          {
-            linkUrl: `${urlSitesHelp}/es/como-integrar-doppler-con-payu`,
-            imgSrc: payu_icon,
-            imgAlt: _('control_panel.external_integrations.payU_title'),
-            iconName: _('control_panel.external_integrations.payU_title'),
-            targetBlank: true,
-          },
-          {
-            linkUrl: `${urlSitesHelp}/es/como-integrar-doppler-con-infor-crm`,
-            imgSrc: infor_icon,
-            imgAlt: _('control_panel.external_integrations.infor_title'),
-            iconName: _('control_panel.external_integrations.infor_title'),
-            targetBlank: true,
-          },
-          {
-            linkUrl: `${urlSitesHelp}/es/como-integrar-wizell-con-doppler`,
-            imgSrc: wizell_icon,
-            imgAlt: _('control_panel.external_integrations.wizell_title'),
-            iconName: _('control_panel.external_integrations.wizell_title'),
-            targetBlank: true,
-          },
-          {
-            linkUrl: `${urlSitesHelp}/es/como-integrar-doppler-con-xintel`,
-            imgSrc: xintel_icon,
-            imgAlt: _('control_panel.external_integrations.xintel_title'),
-            iconName: _('control_panel.external_integrations.xintel_title'),
-            targetBlank: true,
-          },
-          {
-            linkUrl: `${urlSitesHelp}/es/insertar-formulario-en-wix`,
-            imgSrc: wix_icon,
-            imgAlt: _('control_panel.external_integrations.wix_title'),
-            iconName: _('control_panel.external_integrations.wix_title'),
-            targetBlank: true,
-          },
-          {
-            linkUrl: `${urlSitesHelp}/en/como-integrar-doppler-y-lander`,
-            imgSrc: lander__icon,
-            imgAlt: _('control_panel.external_integrations.lander_title'),
-            iconName: _('control_panel.external_integrations.lander_title'),
-            targetBlank: true,
-          },
-          {
-            linkUrl: `${urlSitesHelp}/es/como-integrar-formularios-de-wordpress-con-doppler`,
-            imgSrc: learnpress_icon,
-            imgAlt: _('control_panel.external_integrations.leanrpress_title'),
-            iconName: _('control_panel.external_integrations.leanrpress_title'),
-            targetBlank: true,
-          },
-          {
-            linkUrl: `${urlSitesHelp}/es/como-integrar-videos-en-tus-campanas`,
-            imgSrc: viewed_icon,
-            imgAlt: _('control_panel.external_integrations.viewed_title'),
-            iconName: _('control_panel.external_integrations.viewed_title'),
-            targetBlank: true,
-          },
-          {
-            linkUrl: _('control_panel.external_integrations.dynamics_link_url'),
-            imgSrc: dynamics_icon,
-            imgAlt: _('control_panel.external_integrations.dynamics_title'),
-            iconName: _('control_panel.external_integrations.dynamics_title'),
-            targetBlank: true,
-            ribbonColor: 'violet',
-            ribbonText: _('promotional_ribbons.new'),
-          },
-          {
-            linkUrl: `${urlSitesHelp}/es/como-integrar-doppler-con-tiendup`,
-            imgSrc: tiendup_icon,
-            imgAlt: _('control_panel.external_integrations.tiendup_title'),
-            iconName: _('control_panel.external_integrations.tiendup_title'),
-            targetBlank: true,
-            ribbonColor: 'violet',
-            ribbonText: _('promotional_ribbons.new'),
           },
         ],
       },
