853d6e8b16bec867ff580f83b47e81ecffa5ffcf
Junior Campos <jcampos@makingsense.com>
2023-01-05 10:36:25 -0500

    fix: fix redirect to webapp pages after login

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -3,7 +3,7 @@ import { render, cleanup, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import IntlProvider from '../../i18n/DopplerIntlProvider.double-with-ids-as-values';
 import { MemoryRouter as Router } from 'react-router-dom';
-import Login from './Login';
+import Login, { getPathFromLocation } from './Login';
 import { AppServicesProvider } from '../../services/pure-di';
 
 describe('Login component', () => {
@@ -35,5 +35,40 @@ describe('Login component', () => {
     );
 
     await waitFor(() => expect(getByText('login.button_login')).toBeInTheDocument());
+  });
+
+  describe('getPathFromLocation', () => {
+    it('should return a path when location has state', async () => {
+      // Arrange
+      const location = {
+        pathname: '/login', // current pathname
+        state: {
+          from: {
+            pathname: '/plan-selection/premium/by-contacts', // previous pathname
+            search: '?param1=test1&param2=test2',
+          },
+        },
+      };
+
+      // Act
+      const path = getPathFromLocation(location);
+
+      // Assert
+      expect(path).toBe('/plan-selection/premium/by-contacts?param1=test1&param2=test2');
+    });
+
+    it('should return "/" when has not previous page', async () => {
+      // Arrange
+      const location = {
+        pathname: '/login', // current pathname
+        state: null,
+      };
+
+      // Act
+      const path = getPathFromLocation(location);
+
+      // Assert
+      expect(path).toBe('/');
+    });
   });
 });
