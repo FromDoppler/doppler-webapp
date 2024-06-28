f913d1a87fa8a2a4dda1398a9f432a25e027657a
fgonzalez <fgonzalez@makingsense.com>
2024-06-28 16:49:25 -0300

    refactor: remove form structure from modal

--- src/components/ControlPanel/CollaboratorsSections/Modal/index.js
+++ src/components/ControlPanel/CollaboratorsSections/Modal/index.js
@@ -1,9 +1,4 @@
-import { useIntl } from 'react-intl';
-
-export const Modal = ({ title, subtitle, items, onClose }) => {
-  const intl = useIntl();
-  const _ = (id, values) => intl.formatMessage({ id: id }, values);
-
+export const Modal = ({ children, title, subtitle, onClose }) => {
   return (
     <>
       <div className="modal" id="modal-new-collaborator">
@@ -11,40 +6,7 @@ export const Modal = ({ title, subtitle, items, onClose }) => {
           <span className="close" onClick={() => onClose(false)}></span>
           <h2 className="modal-title">{title}</h2>
           <p>{subtitle}</p>
-          <form action="#" className="awa-form">
-            <legend>{title}</legend>
-            <fieldset>
-              <ul className="field-group">
-                {items.map((item, index) => (
-                  <li className="field-item" key={`item-key-${index}`}>
-                    <label
-                      for={item.name}
-                      className="labelcontrol"
-                      aria-disabled="false"
-                      data-required="true"
-                    >
-                      {item.name}
-                      <input
-                        type={item.type}
-                        name={item.name}
-                        placeholder={item.placeholder}
-                        aria-required="true"
-                        aria-invalid="false"
-                        aria-placeholder={item.placeholder}
-                      />
-                    </label>
-                  </li>
-                ))}
-              </ul>
-              <ul className="dp-group-buttons">
-                <li>
-                  <button type="button" className="dp-button button-medium primary-green">
-                    {_('common.next')}
-                  </button>
-                </li>
-              </ul>
-            </fieldset>
-          </form>
+          {children}
         </div>
       </div>
     </>
