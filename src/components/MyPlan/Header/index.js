ab1a05c33d9279d49b38513f9ef9b74e47bdc034
jhoffman-ms <jmh248@gmail.com>
2025-03-14 11:36:59 -0300

    feat: add tabs component

---
+++ src/components/MyPlan/Header/index.js
@@ -0,0 +1,22 @@
+export const Header = () => {
+  return (
+    <header className="hero-banner">
+      <div className="dp-container">
+        <div className="dp-rowflex">
+          <div className="col-sm-12 col-md-12 col-lg-12">
+            <h2 className="dp-first-order-title">
+              Mi Plan <span className="dpicon iconapp-checklist"></span>
+            </h2>
+          </div>
+          <div className="col-sm-7">
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Ac eleifend diam lobortis montes eget proin
+              purus. Faucibus viverra suspendisse molestie viverra.
+            </p>
+          </div>
+        </div>
+        <span className="arrow"></span>
+      </div>
+    </header>
+  );
+};
---
+++ src/components/MyPlan/PaymentMethod/index.js
@@ -0,0 +1,18 @@
+export const PaymentMethod = () => {
+  return (
+    <div className="dp-container col-p-l-0 col-p-r-0">
+      <div className="dp-rowflex">
+        <div className="col-lg-8 col-md-12 m-b-24">
+          <div className="dp-box-shadow">
+            <p>Payment Method - Content</p>
+          </div>
+        </div>
+        <div className="col-lg-4 col-sm-12">
+          <div className="dp-box-shadow">
+            <p>Payment Method - Columna</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
