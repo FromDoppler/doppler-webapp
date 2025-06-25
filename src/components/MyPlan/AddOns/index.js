33243c23cdc3de366c59813edd13301c42b10b68
jhoffman-ms <jmh248@gmail.com>
2025-06-25 09:32:45 -0300

    feat: add addons page

--- src/components/MyPlan/AddOns/index.js
+++ src/components/MyPlan/AddOns/index.js
@@ -2,12 +2,12 @@ export const AddOns = () => {
   return (
     <div className="dp-container col-p-l-0 col-p-r-0">
       <div className="dp-rowflex">
-        <div className="col-lg-8 col-md-12 m-b-24">
+        <div className="col-lg-9 col-md-12 m-b-24">
           <div className="dp-box-shadow">
             <p>Add-ons - Content</p>
           </div>
         </div>
-        <div className="col-lg-4 col-sm-12">
+        <div className="col-lg-3 col-sm-12">
           <div className="dp-box-shadow">
             <p>Add-ons - Columna</p>
           </div>
