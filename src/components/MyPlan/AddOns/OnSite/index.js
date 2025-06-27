43f401dc0a49e7991b00f56cc33faa0584a8e0e4
jhoffman-ms <jmh248@gmail.com>
2025-06-27 15:35:18 -0300

    feat: add onsite component in the addons page

---
+++ src/components/MyPlan/AddOns/OnSite/index.js
@@ -0,0 +1,47 @@
+import { FormattedNumber, useIntl } from 'react-intl';
+import { Card } from '../Card';
+
+const numberFormatOptions = {
+  style: 'decimal',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+};
+
+const PriceSection = () => {
+  const intl = useIntl();
+  const _ = (id, values) => intl.formatMessage({ id: id }, values);
+
+  const price = 15;
+
+  return (
+    <>
+      <div className="col-sm-6">
+        <div className="m-l-18">
+          <span className="dp-legend-price">{_(`my_plan.addons.onsite.plans_from_legend`)}</span>
+          <h2>
+            US$ <FormattedNumber value={price} {...numberFormatOptions} />*
+          </h2>
+          <span className="dp-disclaimer">{_(`my_plan.addons.onsite.month_legend`)}</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export const OnSite = ({ onSite }) => {
+  const intl = useIntl();
+  const _ = (id, values) => intl.formatMessage({ id: id }, values);
+
+  return (
+    <Card
+      title={_(`my_plan.addons.onsite.title`)}
+      icon={'iconapp-online-clothing'}
+      description={_(`my_plan.addons.onsite.description`)}
+      priceSection={<PriceSection></PriceSection>}
+      moreInformationText={_(`my_plan.addons.more_information_link`)}
+      moreInformationLink={'#'}
+      buyButtonText={onSite.buttonText}
+      buyButtonUrl={onSite.buttonUrl}
+    ></Card>
+  );
+};
--- src/components/MyPlan/AddOns/OnSite/index.test.js
+++ src/components/MyPlan/AddOns/OnSite/index.test.js
@@ -1,18 +1,16 @@
 import { BrowserRouter } from 'react-router-dom';
-import { Conversations } from '.';
+import { OnSite } from '.';
 import { AppServicesProvider } from '../../../../services/pure-di';
 import IntlProvider from '../../../../i18n/DopplerIntlProvider.double-with-ids-as-values';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-describe('Conversations component', () => {
+describe('OnSite component', () => {
   it('should render component', () => {
     // Assert
-    const conversation = {
-      plan: {
-        buttonText: 'COMENZAR',
-        buttonUrl: '',
-      },
+    const onsite = {
+      buttonText: 'COMENZAR',
+      buttonUrl: '',
     };
 
     // Act
@@ -20,14 +18,14 @@ describe('Conversations component', () => {
       <AppServicesProvider>
         <BrowserRouter>
           <IntlProvider>
-            <Conversations conversation={conversation} />
+            <OnSite onSite={onsite} />
           </IntlProvider>
         </BrowserRouter>
       </AppServicesProvider>,
     );
 
     // Assert
-    expect(screen.getByText('my_plan.addons.conversations.title')).toBeInTheDocument();
-    expect(screen.getByText('my_plan.addons.conversations.description')).toBeInTheDocument();
+    expect(screen.getByText('my_plan.addons.onsite.title')).toBeInTheDocument();
+    expect(screen.getByText('my_plan.addons.onsite.description')).toBeInTheDocument();
   });
 });
