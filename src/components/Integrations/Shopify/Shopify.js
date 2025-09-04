644d942d6f5e52dadb83d654e27a670d3e96c8e8
emoralesms <emorales@makingsense.com>
2025-09-04 09:57:50 -0300

    chore: test iframe 9

--- src/components/Integrations/Shopify/Shopify.js
+++ src/components/Integrations/Shopify/Shopify.js
@@ -157,6 +157,7 @@ const Shopify = ({ dependencies: { shopifyClient, dopplerApiClient } }) => {
   });
   const shopifyRef = useRef(shopifyState);
   const getShopifyDataRef = useRef(null);
+  const [isIframeLoaded, setIsIframeLoaded] = useState(false);
   const iframeRef = useRef(null);
   shopifyRef.current = shopifyState;
 
@@ -346,12 +347,18 @@ const Shopify = ({ dependencies: { shopifyClient, dopplerApiClient } }) => {
           </div>
         </div>
         <div className="dp-box-shadow m-b-24">
+          {!isIframeLoaded && (
+            <div style={{ minHeight: 200 }}> 
+              <Loading page />
+            </div>
+          )}
           <iframe
             ref={iframeRef}
             src="/integration/shopify/rfm"
-            style={{ border: "none" }}
+            style={{ border: "none", display: isIframeLoaded ? "block" : "none" }}
             className="col-sm-12"
             title="rfm"
+            onLoad={() => setIsIframeLoaded(true)}
           />
         </div>
       </section>
