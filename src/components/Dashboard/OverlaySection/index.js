d98411cd867ea67f2cded368cb32d2e6e5fd42d1
emoralesms <emorales@makingsense.com>
2026-03-11 11:25:01 -0300

    chore: hide create button for managed clients without campaign access

--- src/components/Dashboard/OverlaySection/index.js
+++ src/components/Dashboard/OverlaySection/index.js
@@ -2,23 +2,25 @@ import React from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { FormattedMessageMarkdown } from '../../../i18n/FormattedMessageMarkdown';
 
-export const OverlaySection = ({ messageKey, textLinkKey, urlKey }) => {
+export const OverlaySection = ({ messageKey, textLinkKey, urlKey, showLink = true }) => {
   const intl = useIntl();
   const _ = (id, values) => intl.formatMessage({ id: id }, values);
 
   return (
     <>
       <FormattedMessageMarkdown id={messageKey} />
-      <FormattedMessage
-        id={textLinkKey}
-        values={{
-          Link: (chunk) => (
-            <a href={_(urlKey)} className="dp-button button-medium primary-green m-t-18">
-              <strong>{chunk}</strong>
-            </a>
-          ),
-        }}
-      />
+      {showLink && textLinkKey && urlKey ? (
+        <FormattedMessage
+          id={textLinkKey}
+          values={{
+            Link: (chunk) => (
+              <a href={_(urlKey)} className="dp-button button-medium primary-green m-t-18">
+                <strong>{chunk}</strong>
+              </a>
+            ),
+          }}
+        />
+      ) : null}
     </>
   );
 };
