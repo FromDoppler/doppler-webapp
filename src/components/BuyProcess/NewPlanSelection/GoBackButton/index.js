210b2e38c308eee99e90b43f55519b969d6fbcd1
jhoffman-ms <jmh248@gmail.com>
2026-05-18 09:04:56 -0300

    fix(new-plan-selection): avoid router dependency in go back button

--- src/components/BuyProcess/NewPlanSelection/GoBackButton/index.js
+++ src/components/BuyProcess/NewPlanSelection/GoBackButton/index.js
@@ -1,16 +1,14 @@
 import { useIntl } from 'react-intl';
-import { useNavigate } from 'react-router-dom';
 
 export const GoBackButton = ({ goBackUrl }) => {
   const intl = useIntl();
-  const navigate = useNavigate();
   const _ = (id, values) => intl.formatMessage({ id }, values);
 
   return (
     <button
       type="button"
       className="dp-button button-medium ctaTertiary dp-new-plan-selection-back-button"
-      onClick={() => (goBackUrl ? navigate(goBackUrl) : window.history.back())}
+      onClick={() => (goBackUrl ? (window.location.href = goBackUrl) : window.history.back())}
     >
       <span className="dp-new-plan-selection-back-text">{_('plan_calculator.button_back')}</span>
     </button>
