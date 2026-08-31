3ffa5a27c7a6b3ec49a38e92f159931cba394bf3
homecavs <homecavs@gmail.com>
2026-08-31 14:37:53 -0300

    chore: add user email as param to the link

--- src/components/Dashboard/LearnWithDoppler/index.js
+++ src/components/Dashboard/LearnWithDoppler/index.js
@@ -63,6 +63,7 @@ export const learnWithDopplerPosts = [
     linkDescription: `dashboard.learn_with_doppler_posts.post_1.link_description`,
     trackingId: `dashboard-learnWithDoppler-card1`,
     newTab: true,
+    params: '&email=<email>',
   },
   {
     id: `2`,
